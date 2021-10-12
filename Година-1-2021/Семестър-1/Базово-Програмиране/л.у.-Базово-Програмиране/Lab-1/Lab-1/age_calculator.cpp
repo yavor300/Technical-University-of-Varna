29d6e114a526583a9cc052cd20017bc520bb0a21
yavor300 <yavor300@gmail.com>
2021-10-12 23:57:42 +0300

    First exerise task

---
+++ Година-1-2021/Семестър-1/Базово-Програмиране/л.у.-Базово-Програмиране/Lab-1/Lab-1/age_calculator.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+Съставете програма, която при въведена от клавиатурата година на раждане
+да извежда възрастта на съответното лице през 2021 г.
+Например при въведена година 1990 да извежда съобщение
+„You are born in 1990, so you are 31 years old”.
+*/
+int main()
+{
+    short bornYear;
+    cin >> bornYear;
+    short currentYear = 2021;
+
+    cout << "You are born in " << bornYear << ", so you are " << currentYear - bornYear << " years old.";
+}
