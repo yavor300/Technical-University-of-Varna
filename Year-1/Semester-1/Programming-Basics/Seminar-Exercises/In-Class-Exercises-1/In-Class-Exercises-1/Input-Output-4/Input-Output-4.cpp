598a0632411816149266a52699371e37b8e3382f
Yavor Chamov <yavor300@gmail.com>
2021-10-22 15:51:13 +0300

    I/O 4th exercise

---
+++ Year-1/Semester-1/Programming-Basics/Seminar-Exercises/In-Class-Exercises-1/In-Class-Exercises-1/Input-Output-4/Input-Output-4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main ()
+{
+    cout << setw(10) << "Levski" << setw(10) << "CSKA" << setw(10) << "Slavia" << setw(10)
+        << "Spartak" << setw(10) << "Botev" << endl;
+    cout << setw(10) << 2 << setw(10) << 5 << setw(10) << 3 << setw(10) << 0 << setw(10)
+        << 4 << endl;
+    cout << setw(10) << 6 << setw(10) << 3 << setw(10) << 1 << setw(10) << 0 << setw(10)
+        << 2 << endl;
+        cout << setw(10) << 1 << setw(10) << 1 << setw(10) << 2 << setw(10) << 2 << setw(10)
+        << 1 << endl;
+    return 0;
+}
