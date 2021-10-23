df165472ddc341e2cc58241a6101468bb1854944
yavor300 <yavor300@gmail.com>
2021-10-23 17:52:14 +0300

    Added Input-Output 13th exercise

---
+++ Year-1/Semester-1/Programming-Basics/Seminar-Exercises/In-Class-Exercises-1/In-Class-Exercises-1/Input-Output-13/Input-Output-13.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+* Задача 8
+* Какво е предимството на cin.getline() пред cin >> при въвеждане на низове?
+*/
+
+void main()
+{
+	char fullNameWithGetLine[25];
+	cin.getline(fullNameWithGetLine, 25); // Yavor Yordanov Chamov
+	cout << fullNameWithGetLine << endl; // Yavor
+
+	char fullNameWithCin[25];
+	cin >> fullNameWithCin; // Yavor Yordanov Chamov
+	cout << fullNameWithCin; // Yavor
+}
