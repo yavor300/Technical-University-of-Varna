fcf5dcd92c54a6e55836856d0fe1117a4f7cc335
yavor300 <yavor300@gmail.com>
2021-10-23 14:32:27 +0300

    Added Input-Output 7th exercise

---
+++ Year-1/Semester-1/Programming-Basics/Seminar-Exercises/In-Class-Exercises-1/In-Class-Exercises-1/Input-Output-7/Input-Output-7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/*
+* Задача 3.8
+* Съставете С/C++ програма, която дава възможност за въвеждане на ред от символи и повтарянето им като „ехо” докато се натисне Enter.
+*/
+void main()
+{
+    cout << "Enter symbols to be repeated." << endl;
+    char symbol;
+    do
+    {
+        cin.get(symbol);
+        cout << symbol;
+    } while (symbol != '\n');
+
+    /*
+    * Програмата демонстрира "ехо" на буферираните символи.
+    */
+}
