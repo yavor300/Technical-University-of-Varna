9b919cd564ca9e132e522db943d6f1355ae16db8
yavor300 <yavor300@gmail.com>
2021-10-28 23:59:27 +0300

    [PB-Lecture] Added solution of the 1st in-class sample

--- Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-1/Sample-1/Sample-1.cpp
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-1/Sample-1/Sample-1.cpp
@@ -1,20 +1,18 @@
-// Sample-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
+void main()
 {
-    std::cout << "Hello World!\n";
+    int broj, ed_cena, total;
+    
+    cout << "Въведи брой закупени продукти\n";
+    cin >> broj;
+    
+    cout << endl << "Въведи единична цена";
+    cin >> ed_cena;
+
+    total = broj * ed_cena;
+    
+    cout << endl << "Ако вие сте закупили " << broj << " продукти и те струват " << ed_cena << "лева" << endl
+        << "то вие сте платили " << total << " лева";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
