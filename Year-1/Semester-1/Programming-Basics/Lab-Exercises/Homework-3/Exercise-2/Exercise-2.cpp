4fcd66738a66af2b82b7dadb64e9ee39e8a89119
yavor300 <yavor300@gmail.com>
2021-10-29 19:43:22 +0300

    [PB-Lab] Added the solution of the 2nd exercise

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-3/Exercise-2/Exercise-2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+/*
+* Задача 2
+* 
+* Съставете програма с функции за:
+*	Извеждане на съобщение „Start?“. Функцията не приема параметри и не връща стойност.
+*	Извеждане на съобщение „Final!“. Функцията не приема стойност и не връща резултат.
+* 
+* Във функция main се извиква изписването на старт и се изчаква натискане на бутон от клавиатурата, след което се изписва финал.
+*/
+void printStart();
+void printFinal();
+
+int main()
+{
+	printStart();
+	cout << endl;
+
+	_getch();
+
+	printFinal();
+}
+
+void printStart()
+{
+	cout << "Start?";
+}
+
+void printFinal()
+{
+	cout << "Final!";
+}
