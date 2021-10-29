ab26a67d9c130f4948f66fad4f94979cd3b40f4e
yavor300 <yavor300@gmail.com>
2021-10-29 22:32:53 +0300

    [PB-Lab] Added the solution of the 4th exercise

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-3/Exercise-4/fn21621577_prob4.cpp
@@ -0,0 +1,32 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+/*
+* Задача 4
+*
+* Съставете програма с функции, която да намира обема и лицето на повърхнината
+* на цилиндър при въведени от клавиатурата радиус и височина.
+*/
+
+double calculateVolume(double radius, double height);
+double calculateArea(double radius);
+int main()
+{
+	double radius, height;
+	cin >> radius >> height;
+
+	cout << "Volume: " << calculateVolume(radius, height) << endl;
+	cout << "Area: " << calculateArea(radius);
+}
+
+double calculateVolume(double radius, double height)
+{
+	return calculateArea(radius) * height;
+}
+
+double calculateArea(double radius)
+{
+	return M_PI * pow(radius, 2);
+}
