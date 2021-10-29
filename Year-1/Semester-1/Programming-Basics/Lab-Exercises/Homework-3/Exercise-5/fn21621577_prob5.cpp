475803dbb90df0822ecf51442c455cba3613a22b
yavor300 <yavor300@gmail.com>
2021-10-29 22:39:51 +0300

    [PB-Lab] Added the solution of the 5th exercise

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-3/Exercise-5/fn21621577_prob5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 5
+*
+* Съставете програма с функция, която да установява дали точка с дадени координати
+* (въведени от клавиатурата) попада в първи квадрант на координатната система.
+*/
+
+bool isPointInFirstQuadrant(double x, double y);
+
+int main()
+{
+	double x, y;
+	cin >> x >> y;
+
+	if (isPointInFirstQuadrant(x, y)) cout << "The point lies in the first quadrant";
+	else cout << "The point does not lie in the first quadrant";
+}
+
+bool isPointInFirstQuadrant(double x, double y)
+{
+	return x > 0 && y > 0;
+}
