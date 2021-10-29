3f5b0742b3e5052c9cad9889f22bb3fe3fe5b525
yavor300 <yavor300@gmail.com>
2021-10-29 21:21:53 +0300

    [PB-Lab] Added the solution of the 3rd exercise

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-3/Exercise-3/fn21621577_prob3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 3
+*
+* Съставете програма функции за:
+* 
+*	Въвеждане  на  реално  число.
+*		Приема  като  входни  параметри  символа  на променливата,  която  ще  се  въвежда.
+*		Връща  като  резултат  стойността  на въведеното число.
+* 
+*	Изчисление на средната стойност на четири числа.
+*		Във функция main въведете четири цели числа, изведете средната им стойност.
+*/
+double getNumber(double number);
+double calculateAverage(double num1, double num2, double num3, double num4);
+int main()
+{
+	double a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	cout << "Average value: " << calculateAverage(a, b, c, d);
+}
+
+double getNumber(double number)
+{
+	return number;
+}
+
+double calculateAverage(double num1, double num2, double num3, double num4)
+{
+	return (getNumber(num1) + getNumber(num2) + getNumber(num3) + getNumber(4)) / 4;
+}
