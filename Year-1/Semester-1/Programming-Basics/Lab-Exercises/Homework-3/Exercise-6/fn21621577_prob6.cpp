d8605ee5d527f13482dbfa15abf4910da4bc0ec1
yavor300 <yavor300@gmail.com>
2021-10-30 22:21:41 +0300

    [PB-Lab] Added the solution of the 6th exercise

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-3/Exercise-6/fn21621577_prob6.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 6
+*
+* Съставете програма с функции за:
+*	Въвеждане  на  реално  число.
+*		Приема  като  входни  параметри  символа  на променливата,  която  ще  се  въвежда.
+*		Връща  като  резултат  стойността  на въведеното число.
+*
+*	Изчисление на x = 2((a−b)/(b−a)).
+*		Приема като входни параметри променливите a и b и връща като резултат отговора на уравнението.
+*		Във функция main въведете две променливи и изведете резултата от изчислението на формулата.
+*/
+
+double inputNumber();
+double calculateExpression(double a, double b);
+
+void main()
+{
+	cout << "Enter two numbers:" << endl;
+
+	double a = inputNumber();
+	double b = inputNumber();
+
+	cout << "2((a-b)/(b-a)) = " << calculateExpression(a, b);
+}
+
+double inputNumber()
+{
+	double number;
+	cin >> number;
+	return number;
+}
+
+double calculateExpression(double a, double b)
+{
+	return 2 * ((a - b) / (b - a));
+}
+
