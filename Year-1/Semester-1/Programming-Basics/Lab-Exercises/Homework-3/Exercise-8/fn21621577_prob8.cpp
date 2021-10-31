ab2c9e4c4ed1036973a93c9129f75097a3004df4
yavor300 <yavor300@gmail.com>
2021-10-31 23:39:36 +0200

    [PB-Lab] Added the solution of the 8th exercise

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-3/Exercise-8/fn21621577_prob8.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 8
+*
+* Влак се движи от гара А до гара В, който са на разстояние 987 километра.
+* Съставете програма с функции за:
+*	Изчисление на необходимата скорост на влака.
+*		Функцията приема като входен параметър времето за което влака трябва да измине
+*		разстоянието от гара А до гара В.
+*		Връща като резултат скоростта с която трябва да се движи влака.
+*	
+*	Изчисления на времето за пътуване.
+*		Функцията приема като входен параметър скоростта с която се движи влака и връща като резултат
+*		времето за което ще измине разстоянието от гара А до гара В.
+*
+* Във функция main въведете време за пътуване в цели часове и изведете скоростта на влака
+* и въведете скорост в цяло число и изведете времето за пътуване.
+*/
+
+#define distance 987.0
+
+double inputNumber();
+double calculateVelocity(int hours);
+double calculateTime(int velocity);
+
+void main()
+{
+	cout << "Hours: ";;
+	int hours = inputNumber();
+	cout << "Velocity: " << calculateVelocity(hours) << " km/h" << endl;
+
+	cout << "Velocity : ";
+	int velocity = inputNumber();
+	cout << "Hours: " << calculateTime(velocity) << " h";
+}
+
+double inputNumber()
+{
+	double number;
+	cin >> number;
+	return number;
+}
+
+double calculateVelocity(int hours)
+{
+	return distance / hours;
+}
+
+double calculateTime(int velocity)
+{
+	return distance / velocity;
+}
+
