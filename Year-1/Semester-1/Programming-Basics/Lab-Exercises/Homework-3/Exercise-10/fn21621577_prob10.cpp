3df5a777180a3fe198a5caf22bf30c90121d9e74
yavor300 <yavor300@gmail.com>
2021-11-01 22:22:42 +0200

    [PB-Lab] Added the solution of the 10th exercise

--- Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-3/Exercise-10/fn21621577_prob10.cpp
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-3/Exercise-10/fn21621577_prob10.cpp
@@ -9,10 +9,62 @@ using namespace std;
 */
 
 double inputNumber();
+void solveQuadraticEquation(double a, double b, double c);
 
 void main()
 {
+	setlocale(LC_ALL, "BG");
+
+	double a = inputNumber();
+	double b = inputNumber();
+	double c = inputNumber();
 	
+	solveQuadraticEquation(a, b, c);
+}
+
+void solveQuadraticEquation(double a, double b, double c)
+{
+	cout.setf(ios::fixed);
+	cout.precision(2);
+
+	if (a == 0)
+	{
+		if (b == 0)
+		{
+			if (c == 0)
+			{
+				cout << "Всяко реално число е решение на уравнението.";
+			}
+			else
+			{
+				cout << "Уравнението няма реални корени.";
+			}
+		}
+		else
+		{
+			cout << "Коренът на уравнението е " << -c / b;
+		}
+	}
+	else
+	{
+		double discriminant = pow(b, 2) - 4 * a * c;
+
+		if (discriminant < 0)
+		{
+			cout << "Уравнението няма реални корени.";
+		}
+		else if (discriminant > 0)
+		{
+			double firstAnswer = (-b + sqrt(discriminant)) / (2 * a);
+			double secondAnswer = (-b - sqrt(discriminant)) / (2 * a);
+
+			cout << "Корените на уравнението са: " << firstAnswer << " и " << secondAnswer;
+		}
+		else
+		{
+			cout << "Коренът на уравнението е " << -b / (2 * a);
+		}
+	}
 }
 
 double inputNumber()
