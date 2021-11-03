b45b32dc3a9c97ed2311fd0934b729bbc24c8f56
yavor300 <yavor300@gmail.com>
2021-11-03 21:40:39 +0200

    [PB-Seminar] Added solutions for two in-class exercises

---
+++ Year-1/Semester-1/Programming-Basics/Seminar-Exercises/In-Class-Exercises-3/Exercise-1/Exercise-1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int getBiggerValue(int firstNumber, int secondNumber);
+
+int main()
+{
+	int a, b, c, d, max1, max2;
+	cin >> a >> b >> c >> d;
+
+	max1 = getBiggerValue(a, b);
+	max2 = getBiggerValue(c, d);
+
+	cout << "Max1: " << max1 << endl;
+	cout << "Max2: " << max2;
+}
+
+int getBiggerValue(int firstNumber, int secondNumber)
+{
+	if (firstNumber > secondNumber) return firstNumber;
+	else return secondNumber;
+}
---
+++ Year-1/Semester-1/Programming-Basics/Seminar-Exercises/In-Class-Exercises-3/Exercise-2/Exercise-2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+double calculateAreaOnEquilateralTriangle(double sideSize);
+double calculateAreaOnIsosclesTriangle(double sideSize, double height);
+
+
+int main()
+{
+	double a, areaOnEquilateralTriangle;
+	cin >> a;
+
+	areaOnEquilateralTriangle = calculateAreaOnEquilateralTriangle(a);
+	cout << "Equilateral Triangle Area: " << areaOnEquilateralTriangle << endl;
+
+	double b, hb, areaOnIsosclesTriangle;
+	cin >> b >> hb;
+
+	areaOnIsosclesTriangle = calculateAreaOnIsosclesTriangle(b, hb);
+	cout << "Isoscles Triangle Area: " << areaOnIsosclesTriangle;
+}
+
+double calculateAreaOnEquilateralTriangle(double sideSize) {
+	return (sqrt(3) / 4) * pow(sideSize, 2);
+}
+
+double calculateAreaOnIsosclesTriangle(double sideSize, double height)
+{
+	return (sideSize * height) / 2;
+}
