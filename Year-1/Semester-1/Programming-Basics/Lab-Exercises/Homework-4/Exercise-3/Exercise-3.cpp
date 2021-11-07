c74472e7af7187da87eb52b762c7d95a09c63e01
yavor300 <yavor300@gmail.com>
2021-11-07 13:43:44 +0200

    [PB-Lab] Added the solution of the 3rd problem from Homework-4

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-4/Exercise-3/Exercise-3.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 3
+*
+* Съставете програма с функция за намиране обиколката и лицето на ромб,
+* която приема параметри по адрес за запазване на обиколката и лицето,
+* и параметри за основата и височината по стойност, които са въведени от клавиатурата. 
+*/
+
+const int rhombusSidesCount = 4;
+
+void calculateRhombusPerimeter(double sideSize, double &perimeter);
+void calculateRhombusArea(double sideSize, double height, double &area);
+void readDoubleInput(string message, double &value);
+
+void main()
+{
+	double sideSize, height, perimeter, area;
+	readDoubleInput("Enter the size of the rhombus side: ", sideSize);
+	readDoubleInput("Enter the size of the rhombus height: ", height);
+
+	calculateRhombusPerimeter(sideSize, perimeter);
+	cout << "Rhombus perimeter: " << perimeter << endl;
+
+	calculateRhombusArea(sideSize, height, area);
+	cout << "Rhombus area: " << area;
+}
+
+void calculateRhombusPerimeter(double sideSize, double &perimeter)
+{
+	perimeter = sideSize * rhombusSidesCount;
+}
+
+void calculateRhombusArea(double sideSize, double height, double &area)
+{
+	area = sideSize * height;
+}
+
+void readDoubleInput(string message, double &value)
+{
+	cout << message;
+	
+	double readValue;
+	cin >> readValue;
+	value = readValue;
+}
