0cd812f15de5a2793e7059883fc66a0da799678e
yavor300 <yavor300@gmail.com>
2021-11-13 19:50:15 +0200

    [PB-Lab] Added the solution of the 1st problem from Homework-5

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-5/Exercise-1/fn21621577_prob1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 1
+*
+* Съставете програма с функция, която да извежда числата, кратни на 8, в интервала от 1 до N.
+* N е естествено число, което се въвежда от клавиатурата и се подава като параметър на функцията.
+* Ако числото е по-малко от 8, да се изведе съобщение „Няма числа в интервала, кратни на 8“.
+*/
+
+bool isNumberDivisibleBy(int number, int divisor);
+bool hasQuotientInRange(int number, int divisor);
+
+int main()
+{
+    const int divisor = 8;
+
+    int n;
+    cin >> n;
+
+    if (!hasQuotientInRange(n, divisor))
+    {
+        printf("There aren't any numbers divisible by %d", divisor);
+        return  0;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (isNumberDivisibleBy(i, divisor)) printf("%d ", i);
+    }
+
+    return 0;
+}
+
+bool isNumberDivisibleBy(int number, int divisor)
+{
+    return number % divisor == 0;
+}
+
+bool hasQuotientInRange(int number, int divisor)
+{
+    return number >= divisor;
+}
