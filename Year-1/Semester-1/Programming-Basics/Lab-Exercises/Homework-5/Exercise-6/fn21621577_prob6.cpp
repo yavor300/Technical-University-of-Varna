1887ee6d41e024240ac218714e04c1743f4930f7
yavor300 <yavor300@gmail.com>
2021-11-13 22:13:18 +0200

    [PB-Lab] Begin the solution of the 6th problem from Homework-5

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-5/Exercise-6/fn21621577_prob6.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+/*
+* Задача 6
+*
+* Съставете програма, която да изведе резултатите по-долу.
+* За всяка подточка съставете функция: -използвайте
+*	for за а) подточка
+*	-while за б) подточка
+*	-do while за в) подточка.
+*/
+
+void main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 4 - i; j > -1; j--)
+		{
+			cout << "%";
+		}
+		cout << endl;
+	}
+	
+	cout << endl;
+
+	int beginRowCounter = 0;
+	while (beginRowCounter < 3)
+	{
+		int beginSymbolCounter = 0;
+		while (beginSymbolCounter <= beginRowCounter)
+		{
+			cout << "%";
+			beginSymbolCounter++;
+		}
+		cout << endl;
+		beginRowCounter++;
+	}
+
+	int reverseRowCounter = 2;
+	while (reverseRowCounter > 0)
+	{
+		int beginSymbolCounter = reverseRowCounter;
+		while (beginSymbolCounter > 0)
+		{
+			cout << "%";
+			beginSymbolCounter--;
+		}
+		cout << endl;
+		reverseRowCounter--;
+	}
+
+	cout << endl;
+}
