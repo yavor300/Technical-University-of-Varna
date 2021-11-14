0bfcaf079b5bb6d77fa3ee7fb3606761d674adb7
yavor300 <yavor300@gmail.com>
2021-11-14 11:36:02 +0200

    [PB-Lab] Added the solution of the 9th problem from Homework-5

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-5/Exercise-9/fn21621577_prob9.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 9
+*
+* Съставете програма,която да извежда цифрата на единиците, десетиците и стотиците на
+* положителни трицифрени числа. Ако се въведе число по голямо от 999 или по малко от 100,
+* да се подкани за повторно въвеждане: 
+*/
+
+int edinitci(int number);
+int desetitci(int number);
+int stotitci(int number);
+bool isNumberValid(int number);
+
+void main()
+{
+	setlocale(LC_ALL, "BG");
+
+	int number;
+	cin >> number;
+	while (!isNumberValid(number))
+	{
+		system("cls");
+		cout << "Невалидно число! Въведи трицифрено число: ";
+		cin >> number;
+	}
+
+	printf("Единици: %d\nДесетици: %d\nСтотици: %d", edinitci(number), desetitci(number), stotitci(number));
+}
+
+int edinitci(int number)
+{
+	return number % 10;
+}
+
+int desetitci(int number)
+{
+	return number / 10 % 10;
+}
+
+int stotitci(int number)
+{
+	return number / 100 % 10;
+}
+
+bool isNumberValid(int number)
+{
+	return number >= 100 && number <= 999;
+}
