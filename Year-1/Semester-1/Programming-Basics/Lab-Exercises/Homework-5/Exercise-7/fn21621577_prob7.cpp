866d752375c0448a832c87a8b381abf7aee902ea
yavor300 <yavor300@gmail.com>
2021-11-14 10:51:53 +0200

    [PB-Lab] Added the solution of the 7th problem from Homework-5

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-5/Exercise-7/fn21621577_prob7.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 7
+*
+* Съставете програма, в която последователно да се въвеждат 10 числа.
+* Да се преброят положителните и да се изведе техният брой.
+*/
+
+const int numbersCount = 10;
+
+int countPositiveNumbers(int numbersCount);
+
+void main()
+{
+	setlocale(LC_ALL, "BG");
+
+	int result = countPositiveNumbers(numbersCount);
+	printf("Положителните числа са: %d", result);
+}
+
+int countPositiveNumbers(int numbersCount)
+{
+	int positiveCounter = 0;
+
+	for (int i = 1; i <= numbersCount; i++)
+	{
+		int number;
+		cin >> number;
+		if (number > 0) positiveCounter++;
+	}
+
+	return positiveCounter;
+}
