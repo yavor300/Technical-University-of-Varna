3723632a2490c8d4606de1cec64d596c2ae844a0
yavor300 <yavor300@gmail.com>
2021-11-14 11:23:58 +0200

    [PB-Lab] Added the solution of the 10th problem from Homework-5

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-5/Exercise-10/fn21621577_prob10.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 9
+*
+* Съставете  програма,  вкоято  последователно  да  се  въвеждат  числа.
+* Да се  преброят положителните и при въвеждане на 0 да се изведе техния брой.
+*/
+
+int sumDigits(int number);
+bool isNumberValid(int number);
+
+void main()
+{
+	setlocale(LC_ALL, "BG");
+
+	int number;
+	cin >> number;
+	while (!isNumberValid(number))
+	{
+		system("cls");
+		cout << "Невалидно число! Въведи повторно число: ";
+		cin >> number;
+	}
+
+	int result = sumDigits(number);
+	cout << result;
+}
+
+int sumDigits(int number)
+{
+	int sum = 0;
+	while (number > 0)
+	{
+		sum += number % 10;
+		number /= 10;
+	}
+	return sum;
+}
+
+bool isNumberValid(int number)
+{
+	return number >= -1234 && number <= 1234;
+}
