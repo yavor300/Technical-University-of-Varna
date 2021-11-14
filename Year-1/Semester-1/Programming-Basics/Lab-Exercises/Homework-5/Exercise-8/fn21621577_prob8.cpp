615d5b6e426388ce67985152487ab2a1b1104a49
yavor300 <yavor300@gmail.com>
2021-11-14 11:01:16 +0200

    [PB-Lab] Added the solution of the 8th problem from Homework-5

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-5/Exercise-8/fn21621577_prob8.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 8
+*
+* Съставете  програма,  вкоято  последователно  да  се  въвеждат  числа.
+* Да се  преброят положителните и при въвеждане на 0 да се изведе техния брой.
+*/
+
+const int numbersCount = 10;
+
+int countPositiveNumbers();
+
+void main()
+{
+	setlocale(LC_ALL, "BG");
+
+	int result = countPositiveNumbers();
+	printf("Положителните числа са: %d", result);
+}
+
+int countPositiveNumbers()
+{
+	int positiveCounter = 0;
+	
+	int number;
+	cin >> number;
+	
+	while (number)
+	{
+		if (number > 0) positiveCounter++;
+		cin >> number;
+	}
+
+	return positiveCounter;
+}
