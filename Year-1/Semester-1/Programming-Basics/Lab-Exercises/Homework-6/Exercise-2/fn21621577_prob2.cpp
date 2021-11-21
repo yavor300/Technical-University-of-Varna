0423f1f6e4bd34b993c62975c9a633c0bb3c10f8
yavor300 <yavor300@gmail.com>
2021-11-21 21:46:54 +0200

    [PB-Lab] Added the solution of the 2nd exercise from Homework-6

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-6/Exercise-2/fn21621577_prob2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 2
+* Съставете програма с функция за въвеждане на масив от реални числа и функция за извеждане
+* на  масива от  реални  числа.  Функциите да приемат  параметри  масив  и  броя на елементите в масива.
+* В main да се въвежда желан брой елементи на масива (от 1 до 10), като програмата  да  продължи,
+* след  като  се  въведе  валиден  размер  на  масива.  След  това  да  се въвеждат и изведат стойностите на масивите, както следва: 
+*/
+
+void fillArray(double arr[], int length);
+void printArray(double arr[], int length);
+int main()
+{
+	setlocale(LC_ALL, "BG");
+
+	cout << "Въведете размер на масива от 1 до 10: ";
+	double lenght;
+	cin >> lenght;
+	cout << "Въвеждане на масив от: " << lenght << " елемента:" << endl;
+
+	double numbers[10];
+	fillArray(numbers, lenght);
+	printArray(numbers, lenght);
+}
+
+
+void fillArray(double arr[], int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		cout << "Елемент " << i + 1 << ": ";
+		cin >> arr[i];
+	}
+}
+
+void printArray(double arr[], int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		cout << "Елемент " << i + 1 << " -> " << arr[i] << endl;
+	}
+}
