d808ff1f2b44dd355ef7692fe4c58c672e8fa43e
yavor300 <yavor300@gmail.com>
2021-11-22 23:16:38 +0200

    [PB-Lab] Added the solution of the 3rd exercise from Homework-6

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-6/Exercise-3/fn21621577_prob3.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 2
+* 
+* Съставете програма с функция за въвеждане на масив от реални числа и функция за извеждане на масива
+* от реални числа в обратен ред на въвеждането. Функциите да приемат параметри масив и броя на елементите
+* в масива. В main се въвежда брой елементи на масива (от 1 до 10), като програмата да продължи, след
+* като се въведе валиден размер на масива. След това да се въвеждат и изведат стойностите на масивите.
+*/
+
+void fillArray(double arr[], int length);
+void printArray(double arr[], int length);
+bool isLengthValid(int lenght);
+
+int main()
+{
+	setlocale(LC_ALL, "BG");
+
+	cout << "Въведете размер на масива от 1 до 10: ";
+	double lenght;
+	cin >> lenght;
+	if (!isLengthValid(lenght))
+	{
+		cout << "Невалиден размер на масива.";
+		return 0;
+	}
+	cout << "Въвеждане на масив от: " << lenght << " елемента:" << endl;
+
+	double numbers[10];
+	fillArray(numbers, lenght);
+	printArray(numbers, lenght);
+}
+
+
+void fillArray(double arr[], int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		cout << "Елемент " << i + 1 << ": ";
+		cin >> arr[i];
+	}
+}
+
+void printArray(double arr[], int length)
+{
+	for (int i = length - 1; i >= 0; i--)
+	{
+		cout << "Елемент " << length - i << " -> " << arr[i] << endl;
+	}
+}
+
+bool isLengthValid(int lenght)
+{
+	return lenght >= 1 && lenght <= 10;
+}
