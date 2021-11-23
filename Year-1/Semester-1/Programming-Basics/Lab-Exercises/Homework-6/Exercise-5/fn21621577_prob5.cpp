c358c1bd92b674b9ded02dd9b1a75c6f4c6ed7d7
yavor300 <yavor300@gmail.com>
2021-11-23 14:23:35 +0200

    [PB-Lab] Added the solution of the 5th exercise from Homework-6

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-6/Exercise-5/fn21621577_prob5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+* Задача 5
+*
+* Съставете програма с функция за извеждане на ASCII кодовете на масив от символи.
+* Функцията да приема параметри масив и броя на елементите в масива.
+* В main се въвеждат и извеждат стойностите на масива.
+*/
+
+void printAsciiValues(string input);
+
+int main()
+{
+	string input;
+	getline(cin, input);
+	printAsciiValues(input);
+}
+
+void printAsciiValues(string input)
+{
+	for (int i = 0; i < input.length(); i++)
+	{
+		cout << input[i] << " -> " << (int) input[i] << endl;
+	}
+}
