a7972e0bd653f1289f799e3d21c74920d200541b
yavor300 <yavor300@gmail.com>
2021-11-23 14:09:03 +0200

    [PB-Lab] Added the solution of the 4th exercise from Homework-6

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-6/Exercise-4/fn21621577_prob4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 4
+*
+* Съставете програма с функция за извеждане на масива от символи в обратен ред на въвеждането.
+* Функцията да приема параметри масив и броя на елементите в масива.
+* В main се въвеждат и извеждат стойностите на масива.
+*/
+
+const int initialArrayLength = 10;
+
+void printArray(char arr[], int symbolsCount);
+
+int main()
+{
+	int symbolsCount;
+	cin >> symbolsCount;
+
+	char symbols[initialArrayLength];
+	cin >> symbols;
+
+	printArray(symbols, symbolsCount + 1);
+}
+
+void printArray(char arr[], int symbolsCount)
+{
+	for (int i = symbolsCount - 1; i >= 0; i--)
+	{
+		cout << arr[i];
+	}
+}
