3ce6aa52f858fd5a80d11f330f89e0906918229c
yavor300 <yavor300@gmail.com>
2021-11-28 14:24:10 +0200

    [PB-Lab] Added the solution of the 1st exercise from Homework-7

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-7/Exercise-1/fn21621577_prob1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 1
+*
+* Съставете програма, в която се декларира масив от 5 числа тип int, които се въвеждат от клавиатурата с функция.
+* Въвежда се още едно допълнително число. Да се състави функция която да изведе индекса (индексите) на съответния елемент,
+* ако допълнителното число се среща в масива, в противен случай - „Не се среща“. 
+*/
+
+const int NUMBERS_COUNT = 5;
+
+void inputArray(int numbers[]);
+int findIndex(int numbers[], int numberSearch);
+
+int main()
+{
+	setlocale(LC_ALL, "BG");
+
+	int numbers[NUMBERS_COUNT];
+	inputArray(numbers);
+
+	int numberSearch;
+	cin >> numberSearch;
+
+	int foundIndex = findIndex(numbers, numberSearch);
+	if (foundIndex != -1) cout << foundIndex;
+	else cout << "Не се среща";
+
+}
+
+void inputArray(int numbers[])
+{
+	for (int i = 0; i < NUMBERS_COUNT; i++)
+	{
+		cin >> numbers[i];
+	}
+}
+
+int findIndex(int numbers[], int numberSearch)
+{
+	for (int i = 0; i < NUMBERS_COUNT; i++)
+	{
+		if (numbers[i] == numberSearch) return i;
+	}
+	return -1;
+}
