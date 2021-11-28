94dd10bb3f203d5ffb84e233f9c16a1b042d2965
yavor300 <yavor300@gmail.com>
2021-11-28 16:58:42 +0200

    [PB-Lab] Added the solution of the 4th exercise from Homework-7

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-7/Exercise-4/fn21621577_prob4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+/*
+* Задача 4
+*
+* В клуба по литература спорят коя от двете гласни се среща най-често в речта – а или о.
+* Напише програма с масив, която приема през клавиатурата символен низ (string) и с помощта
+* на функция да извежда коя от двете букви се среща по-често в него. При равенство да изписва нужното съобщение.
+*/
+
+const int SYMBOLS_COUNT = 59;
+
+int countLetter(string input, char letter);
+
+int main()
+{
+	setlocale(LC_ALL, "BG");
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	string input;
+	getline(cin, input);
+
+	int occuranceO = countLetter(input, 'о');
+	int occuranceA = countLetter(input, 'а');
+
+	if (occuranceO > occuranceA) cout << "О се среща по-често";
+	else if (occuranceA > occuranceO) cout << "А се среща по-често";
+	else cout << "Равенство";
+
+}
+
+int countLetter(string input, char letter)
+{
+	int count = 0;
+	for (int i = 0; i < input.length(); i++)
+	{
+		if (tolower(input[i]) == tolower(letter)) count++;
+	}
+	return count;
+}
