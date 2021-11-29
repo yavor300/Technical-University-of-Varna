b73861f64cb481b0e0c755aaa97b0a8b45aec7af
yavor300 <yavor300@gmail.com>
2021-11-29 21:30:14 +0200

    [PB-Lab] Added the solution of the 8th exercise from Homework-7

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-7/Exercise-8/fn21621577_prob8.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+* Задача 8
+*
+* В училище  „Алберт  Айнщайн“  децата  получават  на  всеки  две  седмици  оценка  за работата си в
+* часовете по математика. Създайте програма с функции, с помощта на които оценките на ученик Х да бъдат
+* записани в масив от 10 елемента (съответстващи на 20-те седмици от срока) и да се анализира успеваемостта му.
+* 
+*	Функция за въвеждане на оценките - да допускат само стойности между 2 и 6;
+*	С функция да се проверява дали ученикът има двойки през срока и ако да, да изведе колко на брой са те;
+*	Функция  първа  двойка  –  ако  ученикът  има двойки,  да  се  изведе  през  коя  седмица  е получена първата двойка.
+*	Функция последна двойка – ако ученикът има двойки, да се изведе през коя седмица е получена последната двойка. 
+*/
+
+const int GRADES_COUNT = 10;
+
+int getWeekOfLastPoorGrade(int grades[]);
+int getWeekOfFirstPoorGrade(int grades[]);
+int getPoorGradesCount(int grades[]);
+void inputArray(int numbers[]);
+bool validateGrade(double grade);
+
+int main()
+{
+	setlocale(LC_ALL, "BG");
+
+	int grades[GRADES_COUNT];
+	inputArray(grades);
+
+	cout << "Брой двойки: " << getPoorGradesCount(grades) << endl;
+	cout << "Първа двойка: седмица " << getWeekOfFirstPoorGrade(grades) << endl;
+	cout << "Последна двойка: седмица " << getWeekOfLastPoorGrade(grades);
+}
+
+int getWeekOfLastPoorGrade(int grades[])
+{
+	for (int i = GRADES_COUNT - 1; i >= 0; i--)
+	{
+		if (grades[i] < 2.50) return (i + 1) * 2;
+	}
+	return -1;
+}
+
+int getWeekOfFirstPoorGrade(int grades[])
+{
+	for (int i = 0; i < GRADES_COUNT; i++)
+	{
+		if (grades[i] < 2.50) return (i + 1) * 2;
+	}
+	return -1;
+}
+
+int getPoorGradesCount(int grades[])
+{
+	int result = 0;
+	for (int i = 0; i < GRADES_COUNT; i++)
+	{
+		if (grades[i] < 2.50) result++;
+	}
+	return result;
+}
+
+void inputArray(int numbers[])
+{
+	for (int i = 0; i < GRADES_COUNT; i++)
+	{
+		cin >> numbers[i];
+	}
+}
+
+bool validateGrade(double grade)
+{
+	return grade >= 2. && grade <= 6.;
+}
