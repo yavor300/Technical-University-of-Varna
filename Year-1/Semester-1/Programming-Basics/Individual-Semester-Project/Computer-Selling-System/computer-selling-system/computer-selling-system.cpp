1736de9c07d0195c8f6c785773f5ef31bfad0fda
yavor300 <yavor300@gmail.com>
2021-12-02 10:02:49 +0200

    [PB-Lab] Started the individual project assignment. Add the required structures.

---
+++ Year-1/Semester-1/Programming-Basics/Individual-Semester-Project/Computer-Selling-System/computer-selling-system/computer-selling-system.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Required structures of the program
+*/
+struct Processor
+{
+    string manufacturer;
+    string model;
+    double frequency;
+    int cores;
+};
+
+struct ComputerConfiguration
+{
+    string id;
+    string brand;
+    string model;
+    Processor processor;
+    double ram;
+    double price;
+    string status;
+};
+
+/*
+* Constants
+*/
+const int MAX_NUMBER_OF_CONFIGURATIONS = 100;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
