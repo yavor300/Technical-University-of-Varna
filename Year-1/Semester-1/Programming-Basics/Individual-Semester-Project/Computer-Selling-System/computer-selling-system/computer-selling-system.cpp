7f5aba77b786370fab61e5bb7629aa1e3b478311
yavor300 <yavor300@gmail.com>
2021-12-04 11:17:48 +0200

    [PB-Individual-Project] Added logic for reading processord and computer data. Saving the created configuration in an array

--- Year-1/Semester-1/Programming-Basics/Individual-Semester-Project/Computer-Selling-System/computer-selling-system/computer-selling-system.cpp
+++ Year-1/Semester-1/Programming-Basics/Individual-Semester-Project/Computer-Selling-System/computer-selling-system/computer-selling-system.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
 
 /*
@@ -12,7 +13,7 @@ struct Processor
     int cores;
 };
 
-struct ComputerConfiguration
+struct Computer
 {
     string id;
     string brand;
@@ -34,46 +35,86 @@ const int EXIT_FROM_MENU_CHOICE = 3;
 /*
 * Prototypes
 */
-void process_configration_request(int& present_configurations_count, int configurations[]);
+void process_configration_request(int& present_configurations_count, Computer configurations[]);
 bool is_configurations_count_valid(int configurations_count, int& present_configurations_count);
-void printArray(int configurations[], int& present_configurations_count);
+void read_precessor_data(string& manufacturer, string& model, double& frequency, int& cores);
+void read_computer_data(string& id, string& brand, string& model, double& ram, double& price, string& status);
+Processor create_processor(string& processor_manufacturer, string& processor_model, double& processor_frequency, int& processor_cores);
+Computer create_computer(string& id, string& brand, string& model, double& ram, double& price, string& status, Processor processor);
+
+void printArray(Computer configurations[], int& present_configurations_count);
 
 int main()
 {
+    setlocale(LC_ALL, "BG");
+
     int choice, present_configurations_count(INITIAL_CONFIGURATIONS_COUNT);
-    int configurations[MAX_NUMBER_OF_CONFIGURATIONS];
+    Computer configurations[MAX_NUMBER_OF_CONFIGURATIONS];
 
     do
     {
-        printf("Press %d to add ne configuration.\nPress %d to exit from the program.\n", ADD_NEW_CONFIGURATION_CHOICE, EXIT_FROM_MENU_CHOICE);
+        printf("Въведете %d, за да добавите нова конфигурация.\nВъведете %d, за да спрете програмата.\n", ADD_NEW_CONFIGURATION_CHOICE, EXIT_FROM_MENU_CHOICE);
 
         do
         {
-            printf("Enter a valid command number [%d - %d]: ", ADD_NEW_CONFIGURATION_CHOICE, EXIT_FROM_MENU_CHOICE);
+            printf("Въведете валидна меню опция [%d - %d]: ", ADD_NEW_CONFIGURATION_CHOICE, EXIT_FROM_MENU_CHOICE);
             cin >> choice;
         } while (choice < ADD_NEW_CONFIGURATION_CHOICE || choice > EXIT_FROM_MENU_CHOICE);
 
         switch (choice)
         {
-        case 1: process_configration_request(present_configurations_count, configurations); cin >> choice;
+        case 1: process_configration_request(present_configurations_count, configurations);
         }
     } while (choice != EXIT_FROM_MENU_CHOICE);
 
     printArray(configurations, present_configurations_count);
 }
 
-void process_configration_request(int& present_configurations_count, int configurations[])
+void process_configration_request(int& present_configurations_count, Computer configurations[])
 {
-    printf("Enter configurations count: ");
+    printf("Въведете брой конфигурации: ");
     int configurations_count;
     cin >> configurations_count;
+    cin.ignore();
     if (is_configurations_count_valid(configurations_count, present_configurations_count))
     {
+        string processor_manufacturer, processor_model, computer_id, computer_brand, computer_model, computer_availability_status;
+        double processor_frequency, computer_ram, computer_price;
+        int processor_cores;
+
         for (int i = 0; i < configurations_count; i++)
         {
-            configurations[present_configurations_count++] = present_configurations_count + 1;
+            read_precessor_data(processor_manufacturer, processor_model, processor_frequency, processor_cores);
+            Processor processor = create_processor(processor_manufacturer, processor_model, processor_frequency, processor_cores);
+            cin.ignore();
+            read_computer_data(computer_id, computer_brand, computer_model, computer_ram, computer_price, computer_availability_status);
+            Computer computer = create_computer(computer_id, computer_brand, computer_model, computer_ram, computer_price, computer_availability_status, processor);
+            configurations[present_configurations_count++] = computer;
         }
     }
+}
+
+Processor create_processor(string& processor_manufacturer, string& processor_model, double& processor_frequency, int& processor_cores)
+{
+    Processor processor;
+    processor.manufacturer = processor_manufacturer;
+    processor.model = processor_model;
+    processor.frequency = processor_frequency;
+    processor.cores = processor_cores;
+    return processor;
+}
+
+Computer create_computer(string& id, string& brand, string& model, double& ram, double& price, string& status, Processor processor)
+{
+    Computer computer;
+    computer.id = id;
+    computer.brand = brand;
+    computer.model = model;
+    computer.ram = ram;
+    computer.price = price;
+    computer.status = status;
+    computer.processor = processor;
+    return computer;
 }
 
 bool is_configurations_count_valid(int configurations_count, int& present_configurations_count)
@@ -82,10 +123,39 @@ bool is_configurations_count_valid(int configurations_count, int& present_config
         MAX_NUMBER_OF_CONFIGURATIONS - present_configurations_count >= configurations_count;
 }
 
-void printArray(int configurations[], int& present_configurations_count)
+void read_precessor_data(string& manufacturer, string& model, double& frequency, int& cores)
+{   
+    cout << "Въведете производител на процесора: ";
+    getline(cin, manufacturer);
+    cout << "Въведете модел на процесора: ";
+    getline(cin, model);
+    cout << "Въведете честота на процесора: ";
+    cin >> frequency;
+    cout << "Въведете брой ядра на процесора: ";
+    cin >> cores;
+}
+
+void read_computer_data(string& id, string& brand, string& model, double& ram, double& price, string& status)
+{
+    cout << "Въведете сериен номер на компютъра: ";
+    getline(cin, id);
+    cout << "Въведете марка на компютъра: ";
+    getline(cin, brand);
+    cout << "Въведете модел на компютъра: ";
+    getline(cin, model);
+    cout << "Въведете RAM на компютъра: ";
+    cin >> ram;
+    cout << "Въведете цена на компютъра: ";
+    cin >> price;
+    cin.ignore();
+    cout << "Въведете наличен статус на компютъра: ";
+    getline(cin, status);
+}
+
+void printArray(Computer configurations[], int& present_configurations_count)
 {
     for (int i = 0; i < present_configurations_count; i++)
     {  
-        cout << configurations[i] << " ";
+        cout << configurations[i].brand << " ";
     }
 }
