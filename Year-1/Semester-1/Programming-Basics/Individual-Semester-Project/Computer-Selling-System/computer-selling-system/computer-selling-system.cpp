01edc24bfb2eda41114848b0f49deb43e107b635
yavor300 <yavor300@gmail.com>
2021-12-04 20:18:23 +0200

    [PB-Individual-Project] Added numbers input validation

--- Year-1/Semester-1/Programming-Basics/Individual-Semester-Project/Computer-Selling-System/computer-selling-system/computer-selling-system.cpp
+++ Year-1/Semester-1/Programming-Basics/Individual-Semester-Project/Computer-Selling-System/computer-selling-system/computer-selling-system.cpp
@@ -32,20 +32,24 @@ const int INITIAL_CONFIGURATIONS_COUNT = 0;
 const int ADD_NEW_CONFIGURATION_CHOICE = 1;
 const int PRINT_ALL_CONFIGURATIONS_CHOICE = 2;
 const int PRINT_SORTED_CONFIGURATIONS_BY_PROCESSOR_FREQUENCY_DESC_CHOICE = 3;
-const int EXIT_FROM_MENU_CHOICE = 4;
+const int PRINT_CONFIGURATIONS_BY_BRAND_CHOICE = 4;
+const int EXIT_FROM_MENU_CHOICE = 5;
 
 /*
 * Prototypes
 */
 void process_add_configuration_request(int& present_configurations_count, Computer configurations[]);
-bool is_configurations_count_valid(int configurations_count, int& present_configurations_count);
+bool is_configurations_count_valid(int& configurations_count, int& present_configurations_count);
 void read_precessor_data(string& manufacturer, string& model, double& frequency, int& cores);
 void read_computer_data(string& id, string& brand, string& model, double& ram, double& price, string& status);
 Processor create_processor(string& processor_manufacturer, string& processor_model, double& processor_frequency, int& processor_cores);
 Computer create_computer(string& id, string& brand, string& model, double& ram, double& price, string& status, Processor processor);
 void print_configurations(Computer configurations[], int& present_configurations_count, int& configurations_count_to_print);
 void sort_configurations_by_processor_frequency_desc(Computer configurations[], int& present_configurations_count, Computer sorted_configurations[]);
-void read_integer_value(string message, int& value);
+void print_configurations_by_brand(Computer configurations[], int& present_configurations_count, string brand);
+void read_valid_integer_value(int& value);
+void read_valid_double_value(double& value);
+void process_print_configurations_by_brand_request(Computer configurations[], int& present_configurations_count);
 
 int main()
 {
@@ -56,32 +60,46 @@ int main()
 
     do
     {
-        printf("Въведете %d, за да добавите нова конфигурация.\nВъведете %d, за да изведете всички конфигурации.\nВъведете %d, за да изведете желан брой конфигурации с най-голяма тактова честота на процесора.\nВъведете %d, за да спрете програмата.\n",
-            ADD_NEW_CONFIGURATION_CHOICE, PRINT_ALL_CONFIGURATIONS_CHOICE, PRINT_SORTED_CONFIGURATIONS_BY_PROCESSOR_FREQUENCY_DESC_CHOICE, EXIT_FROM_MENU_CHOICE);
+        printf("Въведете %d, за да добавите нова конфигурация.\nВъведете %d, за да изведете всички конфигурации.\nВъведете %d, за да изведете желан брой конфигурации с най-голяма тактова честота на процесора.\nВъведете %d, за да изведете конфигурации от избрана марка.\nВъведете %d, за да спрете програмата.\n",
+            ADD_NEW_CONFIGURATION_CHOICE, PRINT_ALL_CONFIGURATIONS_CHOICE, PRINT_SORTED_CONFIGURATIONS_BY_PROCESSOR_FREQUENCY_DESC_CHOICE, PRINT_CONFIGURATIONS_BY_BRAND_CHOICE, EXIT_FROM_MENU_CHOICE);
         do
         {
             printf("Въведете валидна меню опция [%d - %d]: ", ADD_NEW_CONFIGURATION_CHOICE, EXIT_FROM_MENU_CHOICE);
-            cin >> choice;
+            read_valid_integer_value(choice);
         } while (choice < ADD_NEW_CONFIGURATION_CHOICE || choice > EXIT_FROM_MENU_CHOICE);
 
         switch (choice)
         {
         case ADD_NEW_CONFIGURATION_CHOICE:
+            cout << endl;
             process_add_configuration_request(present_configurations_count, configurations);
             break;
+
         case PRINT_ALL_CONFIGURATIONS_CHOICE:
+            cout << endl;
             print_configurations(configurations, present_configurations_count, present_configurations_count);
             break;
+
+        case PRINT_CONFIGURATIONS_BY_BRAND_CHOICE:
+            cout << endl;
+            cin.ignore();
+            process_print_configurations_by_brand_request(configurations, present_configurations_count);
+            break;
+
         case PRINT_SORTED_CONFIGURATIONS_BY_PROCESSOR_FREQUENCY_DESC_CHOICE:
+            cout << endl;
+
             Computer sorted_configurations[MAX_NUMBER_OF_CONFIGURATIONS];
             int configurations_to_print;
 
             sort_configurations_by_processor_frequency_desc(configurations, present_configurations_count, sorted_configurations);
-            read_integer_value("Въведете брой конфигурации за извеждане: ", configurations_to_print);
+            cout << "Въведете брой конфигурации за извеждане: ";
+            read_valid_integer_value(configurations_to_print);
             print_configurations(sorted_configurations, present_configurations_count, configurations_to_print);
 
             break;
         }
+        cout << endl;
     } while (choice != EXIT_FROM_MENU_CHOICE);
 }
 
@@ -89,7 +107,7 @@ void process_add_configuration_request(int& present_configurations_count, Comput
 {
     printf("Въведете брой конфигурации: ");
     int configurations_count;
-    cin >> configurations_count;
+    read_valid_integer_value(configurations_count);
     cin.ignore();
     if (is_configurations_count_valid(configurations_count, present_configurations_count))
     {
@@ -110,9 +128,22 @@ void process_add_configuration_request(int& present_configurations_count, Comput
 
             configurations[present_configurations_count++] = computer;
 
-            cout << endl << "КОНФИГУРАЦИЯТА Е ДОБАВЕНА УСПЕШНО!" << endl << endl;
+            cout << endl << "КОНФИГУРАЦИЯТА Е ДОБАВЕНА УСПЕШНО!" << endl;
         }
     }
+    else {
+        cout << "Въведеният брой конфигурации не е валиден.";
+        int remaining_slots = MAX_NUMBER_OF_CONFIGURATIONS - present_configurations_count;
+        if (remaining_slots < configurations_count) cout << " Няма място за толкова конфигурации." << endl;
+        else printf("Броят конфигурации трябва да е в интервала: [%d - %d]\n", 1, remaining_slots);
+    }
+}
+
+void process_print_configurations_by_brand_request(Computer configurations[], int& present_configurations_count) {
+    cout << "Въведете марка, за която искате да намерите конфигурации: ";
+    string brand;
+    getline(cin, brand);
+    print_configurations_by_brand(configurations, present_configurations_count, brand);
 }
 
 Processor create_processor(string& processor_manufacturer, string& processor_model, double& processor_frequency, int& processor_cores)
@@ -138,7 +169,7 @@ Computer create_computer(string& id, string& brand, string& model, double& ram, 
     return computer;
 }
 
-bool is_configurations_count_valid(int configurations_count, int& present_configurations_count)
+bool is_configurations_count_valid(int& configurations_count, int& present_configurations_count)
 {
     return configurations_count > 0 &&
         MAX_NUMBER_OF_CONFIGURATIONS - present_configurations_count >= configurations_count;
@@ -151,7 +182,7 @@ void read_precessor_data(string& manufacturer, string& model, double& frequency,
     cout << "Въведете модел на процесора: ";
     getline(cin, model);
     cout << "Въведете честота на процесора: ";
-    cin >> frequency;
+    read_valid_double_value(frequency);
     cout << "Въведете брой ядра на процесора: ";
     cin >> cores;
 }
@@ -181,7 +212,7 @@ void print_configurations(Computer configurations[], int& present_configurations
         return;
     }
 
-    cout << endl << "--- ИНФОРМАЦИЯ ЗА ВСИЧКИ КОНФИГУРАЦИИ ---" << endl;
+    cout << "--- ИНФОРМАЦИЯ ЗА " << configurations_count_to_print << " КОНФИГУРАЦИИ--- " << endl;
     if (present_configurations_count == 0)
     {
         cout << "Няма запазени конфигурации." << endl;
@@ -194,6 +225,23 @@ void print_configurations(Computer configurations[], int& present_configurations
         printf("\nКонфигурация номер: %d\nСериен номер: %s\nМарка: %s\nМодел: %s\nRAM памет: %.2f\nПроцесор:\n\tПроизводител: %s\n\tМодел: %s\n\tТактова честота: %.2f\n\tБрой ядра: %d\nЦена: %.2f лв.\nСтатус: %s\n",
             i + 1, computer.id.c_str(), computer.brand.c_str(), computer.model.c_str(), computer.ram, processor.manufacturer.c_str(), processor.model.c_str(), processor.frequency, processor.cores, computer.price, computer.status.c_str());
     }
+}
+
+void print_configurations_by_brand(Computer configurations[], int& present_configurations_count, string brand)
+{
+    bool is_brand_existing = false;
+    for (int i = 0; i < present_configurations_count; i++)
+    {
+        Computer computer = configurations[i];
+        if (computer.brand.compare(brand) == 0)
+        {
+            Processor processor = computer.processor;
+            printf("\nКонфигурация номер: %d\nСериен номер: %s\nМарка: %s\nМодел: %s\nRAM памет: %.2f\nПроцесор:\n\tПроизводител: %s\n\tМодел: %s\n\tТактова честота: %.2f\n\tБрой ядра: %d\nЦена: %.2f лв.\nСтатус: %s\n",
+                i + 1, computer.id.c_str(), computer.brand.c_str(), computer.model.c_str(), computer.ram, processor.manufacturer.c_str(), processor.model.c_str(), processor.frequency, processor.cores, computer.price, computer.status.c_str());
+            is_brand_existing = true;
+        }
+    }
+    if (!is_brand_existing) printf("\nКонфигурации с марката %s не са намерени.\n", brand.c_str());
 }
 
 void sort_configurations_by_processor_frequency_desc(Computer configurations[], int& present_configurations_count, Computer sorted_configurations[])
@@ -217,8 +265,31 @@ void sort_configurations_by_processor_frequency_desc(Computer configurations[], 
     }
 }
 
-void read_integer_value(string message, int& value)
+void read_valid_integer_value(int& value)
 {
-    cout << message;
-    cin >> value;
+    while (true) {
+        if (cin >> value) {
+            break;
+        }
+        else {
+            cout << "Въведете валидна числена стойност! ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
 }
+
+void read_valid_double_value(double& value)
+{
+    while (true) {
+        if (cin >> value) {
+            break;
+        }
+        else {
+            cout << "Въведете валидна числена стойност! ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+}
+
