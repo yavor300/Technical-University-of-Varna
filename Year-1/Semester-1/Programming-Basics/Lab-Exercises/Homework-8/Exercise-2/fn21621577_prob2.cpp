3a514200d460079e77c6737e3592e7790dd1a367
yavor300 <yavor300@gmail.com>
2021-12-05 18:00:20 +0200

    [PB-Lab] Added the solution of the 2nd exercise from Homework-8

---
+++ Year-1/Semester-1/Programming-Basics/Lab-Exercises/Homework-8/Exercise-2/fn21621577_prob2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+* Задача 2
+*
+* Създайте програма с функция, която при въведен символен низ (латински букви)
+* извежда броя на гласните букви в него.
+*/
+int count_vocals(string text);
+int main()
+{
+	setlocale(LC_ALL, "BG");
+
+	string text;
+	getline(cin, text);
+	printf("Броя на гласните е: %d", count_vocals(text));
+}
+
+int count_vocals(string text)
+{
+	int result = 0;
+	for (int i = 0; i < text.length(); i++)
+	{
+		char symbol = tolower(text[i]);
+
+		if (symbol == 'a' || symbol == 'e' || symbol == 'i' || symbol == 'o' || symbol == 'u') result++;
+	}
+	return result;
+}
