cde09d565e0aca6d075d6ad744e13a48cec43b9d
yavor300 <yavor300@gmail.com>
2022-01-20 16:30:17 +0200

    [PB-Lectures] Added Sample-2 from In-Class-Samples-2. Calculating power with recursion.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-2/Sample-2/Sample-2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int power(int x, int n);
+
+int main()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "3 to the power " << i
+            << " is " << power(3, i) << endl;
+    }
+}
+
+int power(int x, int n)
+{
+    if (n < 0)
+    {
+        cout << "Illegal argument to power.\n";
+        exit(1);
+    }
+    if (n > 0) return power(x, n - 1) * x;
+    else return 1;
+}
