94a80021282a0b8a0f35fad2d79c7b2410427ac8
yavor300 <yavor300@gmail.com>
2022-01-20 16:01:37 +0200

    [PB-Lectures] Added Sample-1 from In-Class-Samples-2. Recursion.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-2/Sample-1/Sample-1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+void write_vertical(int n);
+
+int main()
+{
+    int n;
+    cin >> n;
+    write_vertical(n);
+}
+
+void write_vertical(int n) {
+    if (n < 10) cout << n << endl;
+    else
+    {
+        write_vertical(n / 10);
+        cout << n % 10 << endl;
+    }
+}
