9c8e2359b8dafff2a83e5ff7b31d4b26d7d18854
yavor300 <yavor300@gmail.com>
2022-01-22 21:30:03 +0200

    [PB-Lectures] Added Sample-3 from In-Class-Samples-4. Usage of get() and put()

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-4/Sample-3/Sample-3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+const char file_name[9] = "data.txt";
+
+void main()
+{
+	cout << "Enter a line of input and I will echo it:\n";
+	
+	char symbol;
+	do
+	{
+		cin.get(symbol);
+		cout.put(symbol);
+	} while (symbol != '\n');
+	
+	cout << "That's all for this demonstration.";
+}
