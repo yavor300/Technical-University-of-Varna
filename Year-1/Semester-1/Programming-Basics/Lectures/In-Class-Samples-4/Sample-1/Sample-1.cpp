cbac1bde749d85120381348dca6d64f594b59dfa
yavor300 <yavor300@gmail.com>
2022-01-22 21:06:59 +0200

    [PB-Lectures] Exteded Sample-1 from In-Class-Samples-4. Added const values for the files names.

--- Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-4/Sample-1/Sample-1.cpp
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-4/Sample-1/Sample-1.cpp
@@ -1,20 +1,23 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 using namespace std;
+
+const char file_name_input[11] = "infile.dat";
+const char file_name_output[12] = "outfile.dat";
 
 void main()
 {
     ifstream in_stream;
     ofstream out_stream;
 
-    in_stream.open("infile.dat");
+    in_stream.open(file_name_input);
     if (in_stream.fail())
     {
         cout << "Input file opening failed.\n";
         exit(1);
     }
 
-    out_stream.open("outfile.dat");
+    out_stream.open(file_name_output);
     if (out_stream.fail())
     {
         cout << "Output file opening failed.\n";
