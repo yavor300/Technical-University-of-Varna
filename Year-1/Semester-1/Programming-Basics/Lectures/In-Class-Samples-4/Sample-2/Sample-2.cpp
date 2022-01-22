a92148092e574c8d40ba2615fe50e9590eec063d
yavor300 <yavor300@gmail.com>
2022-01-22 21:17:31 +0200

    [PB-Lectures] Added Sample-2 from In-Class-Samples-4. Appending to file

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-4/Sample-2/Sample-2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+const char file_name[9] = "data.txt";
+
+void main()
+{
+    cout << "Opening data.txt for appending.\n";
+    ofstream fout;
+    
+    fout.open(file_name, ios::app);
+    if (fout.fail())
+    {
+        cout << "Input file opening failed.\n";
+        exit(1);
+    }
+
+    fout << "5 6 pick up sticks.\n7 8 ain't C++ great!\n";
+
+    fout.close();
+    cout << "End of appending to file.\n";
+}
