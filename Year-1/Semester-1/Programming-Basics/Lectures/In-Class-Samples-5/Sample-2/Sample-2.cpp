72d350878fd8ece0d0a9166ceb82fe4ffda793ab
yavor300 <yavor300@gmail.com>
2022-01-24 13:39:30 +0200

    [PB-Lectures] Added Sample-2 from In-Class-Samples-5. Accessing array elements using pointers.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-5/Sample-2/Sample-2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	int arr[5] = { 10, 20, 30, 40, 50 };
+	int *pointer;
+	pointer = arr;
+	
+	for (int i = 0; i < 5; i++)
+	{
+		cout << endl << arr[i] << '\t' << pointer[i];
+	}
+	
+	for (int i = 0; i < 5; i++)
+	{
+		cout << endl << *(arr + i) << '\t' << *(pointer + i);
+	}
+}
+
