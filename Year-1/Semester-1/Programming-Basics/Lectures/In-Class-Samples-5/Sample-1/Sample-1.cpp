d7cd51a0444b48aa1f56e9ca5c3be1bdfbc00f62
yavor300 <yavor300@gmail.com>
2022-01-24 11:57:51 +0200

    [PB-Lectures] Added Sample-1 from In-Class-Samples-5. Sample usage of pointers.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-5/Sample-1/Sample-1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	int *p1, *p2;
+
+	p1 = new int;
+	*p1 = 42;
+	p2 = p1;
+	cout << "*p1 == " << *p1 << endl;
+	cout << "*p2 == " << *p2 << endl;
+
+	*p2 = 53;
+	cout << "*p1 == " << *p1 << endl;
+	cout << "*p2 == " << *p2 << endl;
+
+	p1 = new int;
+	*p1 = 88;
+	cout << "*p1 == " << *p1 << endl;
+	cout << "*p2 == " << *p2 << endl;
+}
+
