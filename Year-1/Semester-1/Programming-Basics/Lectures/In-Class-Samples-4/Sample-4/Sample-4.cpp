76f96f2a904b0127f05b4ea38ee40523cf08f81b
yavor300 <yavor300@gmail.com>
2022-01-24 09:16:49 +0200

    [PB-Lectures] Added Sample-4 from In-Class-Samples-4. Editing a file content.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-4/Sample-4/Sample-4.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+const char file_name[9] = "data.txt";
+
+void add_plus_plus(ifstream& in_stream, ofstream& out_stream);
+
+void main()
+{
+	ifstream fin;
+	ofstream fout;
+
+	cout << "Begin editing files.\n";
+	fin.open("cad.dat");
+	if (fin.fail())
+	{
+		cout << "Input file opening failed.\n";
+		exit(1);
+	}
+
+	fout.open("cplusad.dat");
+	if (fout.fail())
+	{
+		cout << "Output file opening failed.\n";
+		exit(1);
+	}
+
+	add_plus_plus(fin, fout);
+	
+	fin.close();
+	fout.close();
+	cout << "End of editing files.\n";
+}
+
+void add_plus_plus(ifstream& in_stream, ofstream& out_stream)
+{
+	char next;
+	in_stream.get(next);
+	while (!in_stream.eof())
+	{
+		if (next == 'C') out_stream << "C++";
+		else out_stream << next;
+
+		in_stream.get(next);
+	}
+}
