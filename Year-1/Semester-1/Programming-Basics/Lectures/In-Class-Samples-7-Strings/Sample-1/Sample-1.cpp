c9c15aa0d6dc9152ab2b758a52ee6c5458d9e46b
yavor300 <yavor300@gmail.com>
2022-01-25 16:53:31 +0200

    [PB-Lectures] Added Sample-1 from In-Class-Samples-7-Strings. Declaring C strings.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-7-Strings/Sample-1/Sample-1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	char my_message_1[20] = "Hello, World!";
+	char my_message_2[] = "Hello, World!";
+	char my_message_3[6] = { 'H', 'e', 'l', 'l', 'o', '\0' };
+	char my_message_4[] = { 'H', 'e', 'l', 'l', 'o', '\0' };
+
+	cout << my_message_1 << endl << my_message_2 << endl
+		<< my_message_3 << endl << my_message_4;
+}
