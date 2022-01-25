30d7a8b1329079ec899a483bb33ce4dd156deb74
yavor300 <yavor300@gmail.com>
2022-01-25 09:13:08 +0200

    [PB-Lectures] Added Sample-1 from In-Class-Samples-6-Structures. Implemented sample bank account structure.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-6-Structures/Sample-1/Sample-1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+struct CDAccount
+{
+	double balance;
+	double interest_rate;
+	int term;
+};
+
+void get_data(CDAccount& account);
+
+void main()
+{
+	CDAccount account;
+	get_data(account);
+
+	double rate_fraction, interest;
+	rate_fraction = account.interest_rate / 100.0;
+	interest = account.balance * rate_fraction * (account.term / 12.0);
+	account.balance = account.balance + interest;
+
+	cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.precision(2);
+	cout << "When your CD matures in "
+		<< account.term << " months,\n"
+		<< "it will have a balance of $"
+		<< account.balance << endl;
+
+}
+
+void get_data(CDAccount& account)
+{
+	cout << "Enter account balance: $";
+	cin >> account.balance;
+	cout << "Enter account interest rate: ";
+	cin >> account.interest_rate;
+	cout << "Enter the number of months until maturity\n"
+		<< "(must be 12 or fewer months): ";
+	cin >> account.term;
+}
