b9f10b5448a0cd4464cf7776e0d7f33ffc7ddac5
yavor300 <yavor300@gmail.com>
2022-01-25 17:32:55 +0200

    [PB-Lectures] Extended Sample-1 from In-Class-Samples-7-Strings. Using functions strlen() and sizeof() to see the difference. Changing all symbols of a string using for loop.

--- Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-7-Strings/Sample-1/Sample-1.cpp
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-7-Strings/Sample-1/Sample-1.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <string>
+#include <cstring>
 using namespace std;
 
 int main()
@@ -9,5 +11,15 @@ int main()
 	char my_message_4[] = { 'H', 'e', 'l', 'l', 'o', '\0' };
 
 	cout << my_message_1 << endl << my_message_2 << endl
-		<< my_message_3 << endl << my_message_4;
+		<< my_message_3 << endl << my_message_4 << endl;
+	
+	cout << my_message_4 << " has length of " << strlen(my_message_3) << endl;
+	cout << my_message_4 << " has size of " << sizeof(my_message_4) << endl;
+
+	for (int i = 0; i < strlen(my_message_4); i++)
+	{
+		my_message_4[i] = 'X';
+	}
+		
+	cout << my_message_4;
 }
