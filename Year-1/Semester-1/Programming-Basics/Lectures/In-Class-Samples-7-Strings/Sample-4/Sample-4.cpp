a2695d234942571b9da368b218ea5ab7fcbb071f
yavor300 <yavor300@gmail.com>
2022-01-26 13:53:07 +0200

    Added Sample-4 from In-Class-Samples-7-Strings. Processes with the string class.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-7-Strings/Sample-4/Sample-4.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+using namespace std;
+
+void main()
+{
+	string first_name, last_name;
+	cout << "Enter your first and last name:\n";
+	cin >> first_name >> last_name;
+
+	cout << "Your last name is spelled:\n";
+	int i = 0;
+	for (i = 0; i < last_name.length(); i++)
+	{
+		cout << last_name[i] << " ";
+		last_name[i] = '-';
+	}
+	cout << endl;
+	for (i = 0; i < last_name.length(); i++)
+	{
+		cout << last_name.at(i) << " ";
+	}
+	cout << endl;
+	cout << "Good day " << first_name << endl;
+}
