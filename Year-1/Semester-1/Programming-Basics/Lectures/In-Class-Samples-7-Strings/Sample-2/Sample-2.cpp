402ba17aca1562e4b198d5d439ecb67f63ede061
yavor300 <yavor300@gmail.com>
2022-01-26 13:35:48 +0200

    Added Sample-2 from In-Class-Samples-7-Strings. Using getline() to read strings and atoi() to convert string to an integer.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-7-Strings/Sample-2/Sample-2.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	char a[80];
+	cin.getline(a, 80);
+	cout << a << "END OF INPUT\n";
+	
+	char string_number[2];
+	cin.getline(string_number, 2); // 12 -> 1
+	int x = atoi(string_number);
+	cout << x * 2 << endl;
+}
