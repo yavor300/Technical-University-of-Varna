aef9e3397b342938dde698b4cc532feab18a1806
yavor300 <yavor300@gmail.com>
2022-01-27 08:48:00 +0200

    Added Sample-1 from In-Class-Samples-8-Arrays. Finding the max element within an integer array.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-8-Arrays/Sample-1/Sample-1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+const int ARRAY_LENGTH = 5;
+
+void main()
+{
+    int i, score[ARRAY_LENGTH], max;
+    
+    cout << "Enter " << ARRAY_LENGTH << " scores:\n";
+    cin >> score[0];
+    max = score[0];
+
+    for (i = 1; i < ARRAY_LENGTH; i++)
+    {
+        cin >> score[i];
+        if (score[i] > max) max = score[i];
+    }
+
+    cout << "The highest score is " << max << endl
+        << "The scores and their\n"
+        << "differences from the highest are:\n";
+
+    for (i = 0; i < ARRAY_LENGTH; i++)
+    {
+        cout << score[i] << " off by " << (max - score[i]) << endl;
+    }
+}
