697137a028af7d31526a59d938ca5ae8dfa106b0
yavor300 <yavor300@gmail.com>
2022-01-29 12:31:13 +0200

    [PB-Lectures] Added Sample-1 from In-Class-Samples-9-Functions. Using fnction to determine the total cost of products.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-9-Functions/Sample-1/Sample-1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+const double TAX_RATE = 0.05;
+
+double total_cost(int number_par, double price_par);
+
+void main()
+{
+    double price, bill;
+    int number;
+
+    cout << "Enter the number of items purchased: ";
+    cin >> number;
+    cout << "Enter the price per item $";
+    cin >> price;
+
+    bill = total_cost(number, price);
+
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+    cout << number << " items at "
+        << "$" << price << " each.\n"
+        << "Final bill, including tax, is $" << bill
+        << endl;
+}
+
+double total_cost(int number_par, double price_par)
+{
+    return (price_par * number_par) * (1 + TAX_RATE);
+}
