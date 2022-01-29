88f6da552ec7a9621b5c917c3574eedef27a1b4c
yavor300 <yavor300@gmail.com>
2022-01-29 12:45:07 +0200

    [PB-Lectures] Added Sample-2 from In-Class-Samples-9-Functions. Comparing pass_by_value vs pass_by_reference.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-9-Functions/Sample-2/Sample-2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int sum_pass_by_value(int a, int b) {
+    return a++ + b;
+}
+
+int sum_pass_by_reference(int& a, int b) {
+    return a++ + b;
+}
+
+void main()
+{
+    int a = 5, b = 6;
+    int result_by_value = sum_pass_by_value(a, b);
+    cout << result_by_value << " " << a << endl;
+    int result_by_reference = sum_pass_by_reference(a, b);
+    cout << result_by_reference << " " << a;
+}
