d7b2bae42583e59f1b08eaa7e99b54e8a4df79fe
yavor300 <yavor300@gmail.com>
2022-01-29 19:40:50 +0200

    [PB-Lectures] Added Sample-2 from In-Class-Samples-11-Input-Output. printf() demo with scanf() to read integer number.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-11-Input-Output/Sample-2/Sample-2.cpp
@@ -0,0 +1,12 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	int* number = new int;
+	printf("Enter number: ");
+	scanf("%d", number);
+	printf("You enrered: %d", *number);
+}
