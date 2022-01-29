8a2c9215db9f9ccfc930131008039c2c81a94735
yavor300 <yavor300@gmail.com>
2022-01-29 22:48:53 +0200

    [PB-Lectures] Added Sample-3 from In-Class-Samples-11-Input-Output. Demo with some ios:: flags.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-11-Input-Output/Sample-3/Sample-3.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double price = 78.50;
+	cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.precision(2);
+	cout << price << endl;
+	cout.unsetf(ios::fixed);
+	cout << price << endl;
+	cout << "Start Now" << endl;
+	cout.width(4);
+	cout << 7 << endl;
+	cout << "Start" << setw(4) << 10
+		<< setw(4) << 20 << setw(6) << 30 << endl;
+}
