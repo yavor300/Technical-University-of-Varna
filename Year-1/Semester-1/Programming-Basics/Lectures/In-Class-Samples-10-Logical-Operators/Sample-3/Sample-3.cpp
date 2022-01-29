e4deb06b56d52b24448de73f05141eaacf11a235
yavor300 <yavor300@gmail.com>
2022-01-29 17:38:17 +0200

    [PB-Lectures] Added Sample-3 from In-Class-Samples-10-Logical-Operators. Finding total sum of values using while loop.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-10-Logical-Operators/Sample-3/Sample-3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	int number_of_items, count,
+		calories_for_item, total_calories;
+
+	cout << "How many items did you eat today? ";
+	cin >> number_of_items;
+
+	total_calories = 0;
+	count = 1;
+	cout << "Enter the number of calories in each of the\n"
+		<< number_of_items << " items eaten:\n";
+
+	while (count++ <= number_of_items)
+	{
+		cin >> calories_for_item;
+		total_calories += calories_for_item;
+	}
+
+	cout << "Total calories eaten today = "
+		<< total_calories << endl;
+}
