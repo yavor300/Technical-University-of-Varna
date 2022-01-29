e6e0a08fa590ba4155e127c5262ef7f0b0064b22
yavor300 <yavor300@gmail.com>
2022-01-29 12:51:49 +0200

    [PB-Lectures] Added Sample-3 from In-Class-Samples-9-Functions. Overloading of functions.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-9-Functions/Sample-3/Sample-3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+double ave(double n1, double n2);
+double ave(double n1, double n2, double n3);
+
+void main()
+{
+    cout << "The average of 2.0, 2.5, and 3.0 is "
+        << ave(2.0, 2.5, 3.0) << endl;
+
+    cout << "The average of 4.5, and 5.5 is "
+        << ave(4.5, 5.5) << endl;
+}
+
+double ave(double n1, double n2)
+{
+    return ((n1 + n2) / 2);
+}
+
+double ave(double n1, double n2, double n3)
+{
+    return ((n1 + n2 + n3) / 3);
+}
