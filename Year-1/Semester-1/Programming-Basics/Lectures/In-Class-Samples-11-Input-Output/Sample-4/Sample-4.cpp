e607afdd157832dfa58e21f02df9e6bc1d85c1e2
yavor300 <yavor300@gmail.com>
2022-01-30 10:40:25 +0200

    [PB-Lectures] Added Sample-4 from In-Class-Samples-11-Input-Output. Demo with get() and put().

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-11-Input-Output/Sample-4/Sample-4.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter line of input and I will echo it:\n";
+	char symbol;
+	do
+	{
+		cin.get(symbol);
+		cout.put(symbol);
+	} while (symbol != '\n');
+}
