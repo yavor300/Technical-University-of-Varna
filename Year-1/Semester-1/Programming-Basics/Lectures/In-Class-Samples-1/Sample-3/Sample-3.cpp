c2afada995f0dcbbcc0d4ede48ef8707b3f7f7d2
yavor300 <yavor300@gmail.com>
2022-01-30 13:15:18 +0200

    [PB-Lectures] Added Sample-3 from In-Class-Samples-1-Introduction. Input/Output of char values.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-1/Sample-3/Sample-3.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	char symbol1, symbol2, symbol3 = ' ';
+	cout << "Enter two initials, without any periods:\n";
+	cin >> symbol1 >> symbol2;
+	cout << "The two initials are:\n" << symbol1 << symbol2 << endl;
+	cout << "Once more with a space:\n" << symbol1 << symbol3 << symbol2 << endl;
+	cout << "That's all.";
+}
