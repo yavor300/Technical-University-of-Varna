a69ff049e1dfa63eef1192e4744d1fd0d838a2c4
yavor300 <yavor300@gmail.com>
2022-01-30 13:10:50 +0200

    [PB-Lectures] Added Sample-2 from In-Class-Samples-1-Introduction. Using operators for input and output. Calculating multiplication of int values.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-1/Sample-2/Sample-2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	int number_of_pods, peas_per_pod, total_peas;
+
+	cout << "Press return after entering a number.\n";
+	cout << "Enter the number of pods:\n";
+	cin >> number_of_pods;
+	cout << "Enter the number of peas in a pod:\n";
+	cin >> peas_per_pod;
+
+	total_peas = number_of_pods * peas_per_pod;
+
+	cout << "If you have " << number_of_pods << " pea pods\n" << "and "
+		<< peas_per_pod << " peas in each pod, then\n"
+		<< "you have " << total_peas << " peas in all the pods.\n";
+}
