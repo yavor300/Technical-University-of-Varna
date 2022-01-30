b2e16d5a1821114fa0c034fcd7ea014579fbcdc3
yavor300 <yavor300@gmail.com>
2022-01-30 13:25:53 +0200

    [PB-Lectures] Added Sample-4 from In-Class-Samples-1-Introduction. Input/Output and concat of strings.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-1/Sample-4/Sample-4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void main()
+{
+	string middle_name, pet_name;
+	string alter_ego_name;
+
+	cout << "Enter your middle name and then the name of your pet.\n";
+	cin >> middle_name >> pet_name;
+
+	alter_ego_name = pet_name + " " + middle_name;
+
+	cout << "The name of your alter ego is: " << alter_ego_name << "." << endl;
+}
