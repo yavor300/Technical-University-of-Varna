0fd78c332628abb4fb795f4effbd437a058d6c34
yavor300 <yavor300@gmail.com>
2022-01-30 11:01:16 +0200

    [PB-Lectures] Added Sample-5 from In-Class-Samples-11-Input-Output. Demo with functions from <conio.h>, <stdio.h> and <ctype.h>.

---
+++ Year-1/Semester-1/Programming-Basics/Lectures/In-Class-Samples-11-Input-Output/Sample-5/Sample-5.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <ctype.h>
+using namespace std;
+
+int main()
+{
+	/*char symbol_without_echo;
+	symbol_without_echo = _getch();
+	_putch(symbol_without_echo);*/
+
+	/*char symbol_with_echo;
+	symbol_with_echo = _getche();
+	cout << symbol_with_echo;*/
+
+	char symbol_with_getchar;
+	symbol_with_getchar = getchar();
+	putchar(symbol_with_getchar);
+	putchar('\n');
+	putchar(toupper(symbol_with_getchar));
+	putchar('\n');
+	putchar(tolower(symbol_with_getchar));
+	putchar('\n');
+	cout << (isupper(symbol_with_getchar) == 0 ? "false" : "true") << endl;
+	cout << (isalpha(symbol_with_getchar) == 0 ? "false" : "true") << endl;
+	cout << (isdigit(symbol_with_getchar) == 0 ? "false" : "true") << endl;
+	cout << (isspace(symbol_with_getchar) == 0 ? "false" : "true") << endl;
+}
