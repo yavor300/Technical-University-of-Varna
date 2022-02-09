fcb381ec7e02cd16335889712db8724a68c97cc2
yavor300 <yavor300@gmail.com>
2022-02-09 09:39:39 +0200

    [S.A.A] Added Sample-2 from In-Class-Samples-1. Implemeted ackermann function using recursion.

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lectures/In-Class-Samples-1/Sample-2/Sample-2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int ackermann(int m, int n);
+
+void main()
+{
+    cout << ackermann(2, 1);
+}
+
+int ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return ackermann(m - 1, 1);
+    return ackermann(m - 1, ackermann(m, n - 1));
+}
