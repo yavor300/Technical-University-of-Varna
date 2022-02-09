353b88b73dad4b809afaeb3b58dc0c6672031cba
yavor300 <yavor300@gmail.com>
2022-02-09 09:25:57 +0200

    [S.A.A] Added Sample-1 from In-Class-Samples-1. Implement Euclidean algorithm using recursion.

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lectures/In-Class-Samples-1/Sample-1/Sample-1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int gcd(int a, int b);
+
+void main()
+{
+    cout << gcd(36, 60);
+    
+}
+
+int gcd(int a, int b)
+{
+    if (a == 0) return b;
+    return gcd(b % a, a);
+}
