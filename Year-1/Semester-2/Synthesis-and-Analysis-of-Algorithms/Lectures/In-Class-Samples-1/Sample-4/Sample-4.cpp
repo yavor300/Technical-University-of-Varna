3ae8300ec1ab17a184887721899f3c29ff8d3037
yavor300 <yavor300@gmail.com>
2022-02-11 09:23:46 +0200

    [S.A.A] Added Sample-4 from In-Class-Samples-1. Implemented Fibonacci recursion.

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lectures/In-Class-Samples-1/Sample-4/Sample-4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int fib(int n);
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fib(n);
+}
+
+int fib(int n)
+{
+    if (n <= 1) return n;
+    return fib(n - 1) + fib(n - 2);
+}
