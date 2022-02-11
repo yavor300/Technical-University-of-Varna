a3ba6a157c71bc9e380f0e3568d760bcb1a2fe38
yavor300 <yavor300@gmail.com>
2022-02-11 09:42:13 +0200

    [S.A.A] Added Sample-4 from In-Class-Samples-1. Implemented Fibonacci using dynamic optimisiation.

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lectures/In-Class-Samples-1/Sample-5/Sample-5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+const int N = 9;
+
+int fib();
+
+int main()
+{
+    cout << fib();
+}
+
+int fib()
+{
+    int f[N + 2];
+    f[0] = 0;
+    f[1] = 1;
+
+    for (int i = 2; i <= N; i++)
+    {
+        f[i] = f[i - 1] + f[i - 2];
+    }
+
+    return f[N];
+}
