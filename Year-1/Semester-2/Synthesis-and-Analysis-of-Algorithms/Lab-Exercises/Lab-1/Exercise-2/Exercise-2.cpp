f7f9cb40d93a898214188199eb123e6a09d6f990
yavor300 <yavor300@gmail.com>
2022-02-24 21:32:08 +0200

    [S.A.A-Lab] Added Exercise-2 from Lab-1. Printing matrix with fiven pattern

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lab-Exercises/Lab-1/Exercise-2/Exercise-2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+const int N = 5;
+
+void fill_in_matrix(int matrix[][N]);
+void print_matrix(const int matrix[][N]);
+
+int main()
+{
+    int matrix[N][N];
+	fill_in_matrix(matrix);
+	print_matrix(matrix);
+}
+
+void fill_in_matrix(int matrix[][N])
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			matrix[i][j] = i + 1;
+		}
+	}
+}
+
+void print_matrix(const int matrix[][N])
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			printf("%d ", matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
