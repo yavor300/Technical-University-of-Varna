2a6ea2fc966eb4dac0a7cd87d22cbe8a34765f92
yavor300 <yavor300@gmail.com>
2022-03-11 09:37:20 +0200

    [S.A.A-Lab] Add solution the of Exercise-1 from Lab-3

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lab-Exercises/Lab-3/Exercise-1/Exercise-1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 1
+* 
+* Рекурсивно търсене на MAX елемент в целочислен масив
+*/
+int max(int numbers[], int start_index, int end_index)
+{
+    if (start_index == end_index) return numbers[start_index];
+
+    int average = (start_index + end_index) / 2;
+    int max_left = max(numbers, start_index, average);
+    int max_right = max(numbers, average + 1, end_index);
+
+    if (max_left > max_right) return max_left;
+    else return max_right;
+}
+
+int main()
+{
+    int arr[] = { 1, 2, 3, 4, 13, 6, 7, 8 };
+    cout << "Max: " << max(arr, 0, 7);
+}
