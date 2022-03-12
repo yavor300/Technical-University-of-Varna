ae3f75a73c891f565d22b6f2dbbdba423dce0951
yavor300 <yavor300@gmail.com>
2022-03-12 16:16:13 +0200

    [S.A.A-Lab] Added Exercise-4 from Lab-3

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lab-Exercises/Lab-3/Exercise-4/Exercise-4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 4
+* 
+* Динамична реализация на програма за изчисляване на числата на Фибоначи
+*/
+
+const int n = 10;
+
+int fib(int i)
+{
+    static int known_fib[n];
+    if (known_fib[i] != 0) return known_fib[i];
+
+    int t = i;
+    if (i < 0) return 0;
+    if (i > 1) t = fib(i - 1) + fib(i - 2);
+
+    return known_fib[i] = t;
+}
+
+int main()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        cout << fib(i) << " ";
+    }
+}
+
