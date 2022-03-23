76d3b7df85b2bbe00e4053dd1f4eb147cddc791d
yavor300 <yavor300@gmail.com>
2022-03-23 21:43:56 +0200

    [S.A.A-Lectures] Added custom stack implementation

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lectures/In-Class-Exercises-4/Exercise-1/Exercise-1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+struct CustomStack
+{
+	int key;
+	CustomStack* next;
+};
+CustomStack* start = NULL;
+
+void push(int n)
+{
+	CustomStack* p = start;
+	start = new CustomStack;
+	start->key = n;
+	start->next = p;
+}
+
+int pop(int& n)
+{
+	if (start)
+	{
+		n = start->key;
+		CustomStack* p = start;
+		start = start->next;
+		delete p;
+		return 1;
+	}
+	return 0;
+}
+
+int main()
+{
+	int num;
+	while (cin >> num)
+	{
+		push(num);
+	}
+	while (pop(num))
+	{
+		cout << num << " ";
+	}
+}
