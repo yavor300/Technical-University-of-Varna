0856dd99fb1b391c708eb832a5ec906eadb135c3
yavor300 <yavor300@gmail.com>
2022-03-24 21:13:36 +0200

    [S.A.A-Lectures] Added custom static stack implementation

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lectures/In-Class-Exercises-4/Exercise-2/Exercise-2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+const int s_size = 10;
+int st[s_size];
+int i = 0;
+
+void push(int n)
+{
+	st[i++] = n;
+}
+
+int pop()
+{
+	return st[--i];
+}
+
+int main()
+{
+	int num;
+	while (cin >> num)
+	{
+		push(num);
+	}
+	while (i > 0)
+	{
+		cout << pop() << " ";
+	}
+}
