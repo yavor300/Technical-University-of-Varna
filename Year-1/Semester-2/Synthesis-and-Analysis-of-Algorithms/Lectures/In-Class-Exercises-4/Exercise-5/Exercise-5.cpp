e23e4e4a6f9b5f32004200fb9a3af2eda792a60f
yavor300 <yavor300@gmail.com>
2022-03-26 16:55:53 +0200

    [S.A.A-Lectures] Added custom static queue implementation

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lectures/In-Class-Exercises-4/Exercise-5/Exercise-5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+const int queue_size = 10;
+int queue[queue_size];
+int first = 0, last = 0;
+
+void push(int element)
+{
+    queue[last++] = element;
+}
+
+int pop()
+{
+    return queue[first++];
+}
+
+int main()
+{
+    int num;
+    while (cin >> num && last < queue_size)
+    {
+        push(num);
+    }
+    while (first < last)
+    {
+        cout << pop() << " ";
+    }
+}
