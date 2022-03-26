58b22552c528baf403dd2f0c297b96aa5c707f03
yavor300 <yavor300@gmail.com>
2022-03-26 21:52:52 +0200

    [S.A.A-Lab] Added the solution of the 6th lab exercise

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lab-Exercises/Lab-4/Exercise-6/Exercise-6.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 6
+*
+* Сито на Ератостен с динамично заделяне на памет
+*/
+
+void main()
+{
+    int i, j, n;
+    char c;
+
+	do
+	{
+		cout << "Input n:";
+		cin >> n;
+		int* a = new int[n];
+		if (a == NULL)
+		{
+			cout << "Insufficient memory!";
+		}
+		else
+		{
+			for (i = 2; i < n; i++)
+			{
+				*(a + i) = 1;
+			}
+			for (i = 2; i < n; i++)
+			{
+				if (*(a + i))
+				{
+					for (j = i; i * j < n; j++)
+					{
+						*(a + i * j) = 0;
+					}
+				}
+			}
+			for (i = 2; i < n; i++)
+			{
+				if (*(a + i)) cout << i << ' ';
+			}
+		}
+		delete[]a;
+		cout << "\nAnother n? (Y/N) ";
+		cin >> c;
+	} while (c == 'Y' || c == 'y');
+}
