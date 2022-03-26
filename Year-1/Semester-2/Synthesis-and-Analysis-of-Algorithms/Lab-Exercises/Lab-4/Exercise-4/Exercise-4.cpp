66a623609fbdef31207a088e540378a5de5f25ed
yavor300 <yavor300@gmail.com>
2022-03-26 21:23:36 +0200

    [S.A.A-Lab] Added the solution of the 4th lab exercise

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lab-Exercises/Lab-4/Exercise-4/Exercise-4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 4
+*
+* Връзка между масиви и указатели
+*/
+
+void main()
+{
+    int* pa, a[3] = { 10, 20, 30 };
+    pa = a;
+    cout << *pa << "\t" << *(pa + 1) << "\t" << *(pa + 2) << endl;
+    cout << *a << "\t" << *(a + 1) << "\t" << *(a + 2) << endl;
+    cout << a[0] << "\t" << a[1] << "\t" << a[2] << endl;
+}
