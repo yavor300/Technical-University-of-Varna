0f5ed36c282b732d5e724d551ac760e3c1cc5ddb
yavor300 <yavor300@gmail.com>
2022-03-26 17:18:45 +0200

    [S.A.A-Lectures] Added custom static queue implementation using circle array

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lectures/In-Class-Exercises-4/Exercise-6/Exercise-6.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+const int queue_size = 15;
+int queue[queue_size], first = 0, last = 0, counter = 0;
+
+void push(int element)
+{
+    queue[last] = element;
+    last = (last + 1) % queue_size;
+}
+
+int pop()
+{
+    int result = queue[first];
+    first = (first + 1) % queue_size;
+    return result;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "BG");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    int num, rep;
+    do
+    {
+        cout << "1 - Добавяне на стойност\n";
+        cout << "2 - Извеждане на стойност\n";
+        cout << "3 - Край на работа\n";
+        cout << "Изберете:";
+        cin >> rep;
+        switch (rep)
+        {
+        case 1:
+            if (counter < queue_size)
+            {
+                cout << "Въведете число:";
+                cin >> num;
+                push(num);
+                counter++;
+            }
+            else
+            {
+                cout << "Буферът е препълнен!\n";
+            }
+            break;
+            
+        case 2:
+            if (counter)
+            {
+                cout << "num=" << pop() << '\n';
+                counter--;
+            }
+            else
+            {
+                cout << "Буферът е празен!\n";
+            }
+            break;
+        }
+    } while (rep != 3);
+}
