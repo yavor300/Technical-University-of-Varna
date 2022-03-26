044d29dfadb53c2c786dc0e2780d58f58310a025
yavor300 <yavor300@gmail.com>
2022-03-26 21:14:22 +0200

    [S.A.A-Lab] Added the solution of the second lab exercise

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lab-Exercises/Lab-4/Exercise-2/Exercise-2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 2
+* 
+* Аритметични операции с указатели
+*/
+int main()
+{
+    int q, * pq;
+    pq = &q;
+    cout << "pq=" << pq << endl;
+    pq++;
+    cout << "pq=" << pq << endl;
+
+    float qf, * qpf;
+    qpf = &qf;
+    cout << "qpf=" << qpf << endl;
+    qpf++;
+    cout << "qpf=" << qpf << endl;
+
+    double qd, * pqd;
+    pqd = &qd;
+    cout << "pqd=" << pqd << endl;
+    pqd++;
+    cout << "pqd=" << pqd << endl;
+}
+
