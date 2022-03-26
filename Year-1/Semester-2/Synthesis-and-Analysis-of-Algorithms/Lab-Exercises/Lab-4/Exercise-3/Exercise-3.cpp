09289683c9ba0bbdda7aa83e4bd7495eef8e8fda
yavor300 <yavor300@gmail.com>
2022-03-26 21:19:42 +0200

    [S.A.A-Lab] Added the solution of the third lab exercise

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lab-Exercises/Lab-4/Exercise-3/Exercise-3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 3
+*
+* Инкрементиране и декрементиране към обекта,
+* сочен от указателя
+*/
+int main()
+{
+    int q, * pq;
+    pq = &q;
+    *pq = 20;
+    (*pq)++;
+    cout << "q=" << q << endl;
+    cout << "*pq=" << *pq << endl;
+    *pq += 1;
+    cout << "q=" << q << endl;
+    cout << "*pq=" << *pq << endl;
+    ++* pq;
+    cout << "q=" << q << endl;
+    cout << "*pq=" << *pq << endl;
+}
