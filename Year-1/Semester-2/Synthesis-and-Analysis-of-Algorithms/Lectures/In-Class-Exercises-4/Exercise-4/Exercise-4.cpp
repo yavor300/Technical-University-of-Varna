bfae15e250f4a71a47955815642d56fe9b4556ac
yavor300 <yavor300@gmail.com>
2022-03-26 16:44:00 +0200

    [S.A.A-Lectures] Added custom dynamic queue implementation

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lectures/In-Class-Exercises-4/Exercise-4/Exercise-4.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+struct CustomQueue {
+    int value;
+    CustomQueue* next;
+} *first = NULL, *last = NULL;
+
+void push(int element)
+{
+    CustomQueue* p = last;
+    last = new CustomQueue;
+    last->value = element;
+    last->next = NULL;
+    if (p != NULL) p->next = last;
+    if (first == NULL) first = last;
+}
+
+int pop(int& element)
+{
+    if (first)
+    {
+        element = first->value;
+        CustomQueue* p = first;
+        first = first->next;
+        delete p;
+        return 1;
+    }
+    return 0;
+}
+
+int main()
+{
+    int num;
+    while (cin >> num)
+    {
+        push(num);
+    }
+    while (pop(num))
+    {
+        cout << num << " ";
+    }
+}
