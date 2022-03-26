2befe8df78280b8b5fd7059c2a845a23651c1128
yavor300 <yavor300@gmail.com>
2022-03-26 22:03:02 +0200

    [S.A.A-Lab] Added demo for creating multidimensonal array with the usage of double pointer

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lab-Exercises/Lab-4/Exercise-7/Exercise-7.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 7
+*
+* Създаване на назъбена матрица с употребата на двоен указател
+*/
+
+void main()
+{
+	int** par2;
+	par2 = new int*[4];
+
+	par2[0] = new int[2]{ 1, 2 };
+	par2[1] = new int[3]{ 4, 5, 6 };
+
+	cout << par2[0][0] << endl;
+	cout << par2[0][1] << endl;
+	cout << par2[1][0] << endl;
+	cout << par2[1][1] << endl;
+	cout << par2[1][2] << endl;
+
+	delete[]par2;
+}
