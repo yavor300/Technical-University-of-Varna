6cb8588c1da2abb1bc21e63a1aafdf4dda2e24cc
yavor300 <yavor300@gmail.com>
2022-03-26 21:37:40 +0200

    [S.A.A-Lab] Added the solution of the 5th lab exercise

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lab-Exercises/Lab-4/Exercise-5/Exercise-5.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 5
+*
+* Сито на Ератостен –намиране на прости числа 
+*/
+
+#define K 1000
+
+void main()
+{
+    int i, j, n, a[K];
+    cout << "n=";
+    cin >> n;
+    for (i = 2; i < n; i++)
+    {
+        a[i] = 1;
+    }
+    for (i = 2; i < n; i++)
+    {
+        if (a[i])
+        {
+            for (j=i; i * j < n; j++)
+            {
+                a[i * j] = 0;
+            }
+        }
+    }
+    for (i = 2; i < n; i++)
+    {
+        if (a[i]) cout << i << ' ';
+    }
+}
