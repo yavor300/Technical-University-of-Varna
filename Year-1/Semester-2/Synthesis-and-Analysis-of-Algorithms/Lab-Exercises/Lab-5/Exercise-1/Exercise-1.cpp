80b4295610a3090539a5c1f5bbee2d3197078c04
yavor300 <yavor300@gmail.com>
2022-03-31 21:32:14 +0300

    [S.A.A-Lab] Added the solution of the exercise given in the 6th lab

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lab-Exercises/Lab-5/Exercise-1/Exercise-1.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* Задача 1
+* Програмно да се реализира динамичен стек S от цели положителни числа,
+* който да се преобразува в два други стека –P и Q, съдържащи съответно
+* четните и нечетните стойности на S. Съдържанието на двата стека да се изведе на екрана.
+*/
+
+struct CustomStack
+{
+	int key;
+	CustomStack* next;
+};
+
+void push(CustomStack*& stack, int n)
+{
+	CustomStack* p = stack;
+	stack = new CustomStack;
+	stack->key = n;
+	stack->next = p;
+}
+
+int pop(CustomStack*& stack, int& n)
+{
+	if (stack)
+	{
+		n = stack->key;
+		CustomStack* p = stack;
+		stack = stack->next;
+		delete p;
+		return 1;
+	}
+	return 0;
+}
+
+void main()
+{
+	CustomStack* main = NULL;
+	CustomStack* even = NULL;
+	CustomStack* odd = NULL;
+
+	int num;
+
+	while (cin >> num)
+		push(main, num);
+
+	while (pop(main, num))
+	{
+		if (num % 2 == 0) push(even, num);
+		else push(odd, num);
+	}
+
+	while (pop(even, num))
+		cout << num << " ";
+	while (pop(odd, num))
+		cout << num << " ";
+}
