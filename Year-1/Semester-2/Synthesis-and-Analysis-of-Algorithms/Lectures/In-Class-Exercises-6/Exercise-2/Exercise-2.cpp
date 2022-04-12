0b64b208e50f6d3a21c1c242fdc2512e8df35619
yavor300 <yavor300@gmail.com>
2022-04-12 10:21:10 +0300

    [S.A.A] Added the solution of the 2nd exercise given in lecture #7

---
+++ Year-1/Semester-2/Synthesis-and-Analysis-of-Algorithms/Lectures/In-Class-Exercises-6/Exercise-2/Exercise-2.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+/*
+* Задача 2
+* 
+* Моделиране на играта "Броеница" с единично-свързан
+* кръгов списък
+*/
+
+struct Node
+{
+    int value;
+    Node* next;
+} *start = NULL;
+
+void add(int value)
+{
+    Node* p = start;
+    start = new Node;
+    start->value = value;
+    start->next = p;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "BG");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    int i, n, m;
+    cout << "Въведете броя на децата n: ";
+    cin >> n;
+    
+    for (int i = n; i > 0; i--) add(i);
+    
+    Node* p = start;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    p->next = start;
+
+    cout << "\nВъведете m: ";
+    cin >> m;
+
+    p = start;
+    Node* q = NULL;
+    do
+    {
+        for (int i = 1; i < m; i++)
+        {
+            q = p;
+            p = p->next;
+        }
+        q->next = p->next;
+        cout << "\nНомер за изтриване " << p->value;
+        delete(p);
+        p = q->next;
+    } while (p != p->next);
+    cout << "\nОстава дете с номер " << p->value;
+}
