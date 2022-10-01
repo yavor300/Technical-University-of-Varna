d2d3583d24a41e76b0a6c27cae185ff61bc49557
yavor300 <yavor300@gmail.com>
2022-10-01 23:00:15 +0300

    [O.O.P] (Week 1) Add the solution of the second exercise.

---
+++ Year-2/Semester-1/Object-Oriented-Programming-Part-01/Lab-Exerceises/Lab-01/src/solutions/Exercise02.java
@@ -0,0 +1,24 @@
+package solutions;
+
+
+/**
+ * Задача 2
+ * Създайте масив {5, 6, 7, 9} с количеството на касети в дрогерия и масив
+ * {2.5, 3.6, 8.9, 7.5} с цените на една 1 касета.
+ * Изведете общата сума на всички касети.
+ */
+public class Exercise02 {
+
+  public static void main(String[] args) {
+
+    int[] count = {5, 6, 7, 9};
+    double[] prices = {2.5, 3.6, 8.9, 7.5};
+    double total = 0;
+
+    for (int i = 0; i < 4; i++) {
+      total += count[i] * prices[i];
+    }
+
+    System.out.printf("Total: %.2f", total);
+  }
+}
