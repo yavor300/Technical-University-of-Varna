6a8b454ccc0683268a7c24f6f4d965f7a0804306
yavor300 <yavor300@gmail.com>
2022-10-01 23:04:42 +0300

    [O.O.P] (Week 1) Add the solution of the first exercise.

---
+++ Year-2/Semester-1/Object-Oriented-Programming-Part-01/Lab-Exerceises/Lab-01/src/solutions/Exercise01.java
@@ -0,0 +1,15 @@
+package solutions;
+
+/**
+ * Задача 1
+ * Изведете нечетните числа до 300.
+ */
+public class Exercise01 {
+
+  public static void main(String[] args) {
+
+    for (int i = 1; i < 300; i += 2) {
+      System.out.println(i);
+    }
+  }
+}
