d604218d47941f9d23ceedd12a2c54f9fd1b73bd
yavor300 <yavor300@gmail.com>
2022-10-08 20:38:34 +0300

    [O.O.P] (Week 2) Add the solution of the first homework exercise.

---
+++ Year-2/Semester-1/Object-Oriented-Programming-Part-01/Homework/Homework-02/solutions/src/exercise-01/Item.java
@@ -0,0 +1,55 @@
+public class Item {
+  private String type;
+  private Integer quantity;
+  private Double price;
+  private Integer expirationDays;
+
+  public Item(String type, Integer quantity, Double price, Integer expirationDays) {
+    this.type = type;
+    this.quantity = quantity;
+    this.price = price;
+    this.expirationDays = expirationDays;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+
+  public Double getPrice() {
+    return price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public Integer getExpirationDays() {
+    return expirationDays;
+  }
+
+  public void setExpirationDays(Integer expirationDays) {
+    this.expirationDays = expirationDays;
+  }
+
+  @Override
+  public String toString() {
+    return "Item{" +
+            "type='" + type + '\'' +
+            ", quantity=" + quantity +
+            ", price=" + price +
+            ", expirationDays=" + expirationDays +
+            '}';
+  }
+}
---
+++ Year-2/Semester-1/Object-Oriented-Programming-Part-01/Homework/Homework-02/solutions/src/exercise-01/Main.java
@@ -0,0 +1,33 @@
+/**
+ * Задача 1
+ *
+ * Дефинирайте клас Item, който описва стока с атрибути тип, количество,
+ * единична цена и годност (брой дни). Като поведение използвайте методи за четене/запис,
+ * метод за текстово описание. Създайте масив от 10 обекта.
+ * Намерете и изведете средната цена.
+ */
+public class Main {
+  public static void main(String[] args) {
+    Item[] items = {
+            new Item("Product1", 10, 5.99, 3),
+            new Item("Product2", 10, 2.99, 30),
+            new Item("Product3", 15, 3.00, 50),
+            new Item("Product4", 15, 3.10, 60),
+            new Item("Product5", 15, 6.00, 30),
+            new Item("Product6", 15, 3.00, 30),
+            new Item("Product7", 15, 7.30, 30),
+            new Item("Product8", 15, 3.00, 30),
+            new Item("Product9", 15, 3.20, 30),
+            new Item("Product10", 15, 3.00, 30),
+    };
+
+    double totalPrice = 0;
+    int totalQuantities = 0;
+
+    for (Item item : items) {
+      totalPrice += item.getPrice() * item.getQuantity();
+      totalQuantities += item.getQuantity();
+    }
+    System.out.printf("Average price: %.2f", totalPrice / totalQuantities);
+  }
+}
