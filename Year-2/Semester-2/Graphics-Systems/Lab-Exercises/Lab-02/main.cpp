7a03d4bc48c214c211995e9995853188320c7158
yavor300 <yavor300@gmail.com>
2023-03-14 23:31:49 +0200

    [Graphics-Systems] (Week 2) Add program that creates and fills a circle with color.

--- Year-2/Semester-2/Graphics-Systems/Lab-Exercises/Lab-02/main.cpp
+++ Year-2/Semester-2/Graphics-Systems/Lab-Exercises/Lab-02/main.cpp
@@ -3,10 +3,27 @@
 
 using namespace std;
 
+
+void draw_circle() {
+  initwindow(800, 800);
+  circle(200, 200, 100);
+  getch();
+}
+
+void draw_circle_with_fill() {
+  initwindow(600, 600);
+  setlinestyle(0, 0, 3);
+  setcolor(15);
+  circle(100, 100, 50);
+  setfillstyle(1, 12);
+  floodfill(100, 100, 15);
+  getch();
+}
+
 int main()
 {
-    initwindow(800, 800);
-    circle(200, 200, 100);
-    getch();
-    return 0;
+  //draw_circle();
+  draw_circle_with_fill();
+  return 0;
 }
+
