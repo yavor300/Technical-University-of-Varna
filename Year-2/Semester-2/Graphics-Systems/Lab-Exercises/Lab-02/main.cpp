8fda99108bfcaee49957c7cc94f29a4c3ce78558
yavor300 <yavor300@gmail.com>
2023-03-16 00:33:57 +0200

    [Graphics-Systems] (Week 2) Add graphic with four crossed half circles.

--- Year-2/Semester-2/Graphics-Systems/Lab-Exercises/Lab-02/main.cpp
+++ Year-2/Semester-2/Graphics-Systems/Lab-Exercises/Lab-02/main.cpp
@@ -23,7 +23,26 @@ void draw_circle_with_fill() {
 int main()
 {
   //draw_circle();
-  draw_circle_with_fill();
+  //draw_circle_with_fill();
+
+  initwindow(500, 500);
+
+  // Изчертаване на окръжност, дъга от окръжност, част от кръг.
+  arc(400,200,135,315,141);
+  arc(200,400,315,135,141);
+  arc(200,200,225,45,141);
+  arc(400,400,45,225,141);
+
+  setlinestyle(0, 0, 2);
+  setfillstyle(1, 2);
+  //floodfill(300,200,15);
+  putpixel(300,200,15);
+  settextjustify(0, 0);
+  outtextxy(300, 200, "ab");
+
+
+  getch();
   return 0;
 }
+
 
