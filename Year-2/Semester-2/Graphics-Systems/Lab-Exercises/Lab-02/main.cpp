76d2623fbb6a2f7fba1bcd09ee39c8c01f548eed
yavor300 <yavor300@gmail.com>
2023-03-19 16:20:44 +0200

    [Graphics-Systems] (Week 2) Add graphic with four crossed half circles. Filled with colors and text boxes.

--- Year-2/Semester-2/Graphics-Systems/Lab-Exercises/Lab-02/main.cpp
+++ Year-2/Semester-2/Graphics-Systems/Lab-Exercises/Lab-02/main.cpp
@@ -5,41 +5,53 @@ using namespace std;
 
 
 void draw_circle() {
-  initwindow(800, 800);
-  circle(200, 200, 100);
+  initwindow(800,800);
+  circle(200,200,100);
   getch();
 }
 
 void draw_circle_with_fill() {
-  initwindow(600, 600);
-  setlinestyle(0, 0, 3);
+  initwindow(600,600);
+  setlinestyle(0,0,3);
   setcolor(15);
-  circle(100, 100, 50);
-  setfillstyle(1, 12);
-  floodfill(0, 0, 15);
+  circle(100,100,50);
+  setfillstyle(1,12);
+  floodfill(0,0,15);
   getch();
 }
 
 int main()
 {
-  //draw_circle();
-  //draw_circle_with_fill();
+  initwindow(900,900);
 
-  initwindow(500, 500);
+  setlinestyle(0,0,3);
+  setcolor(WHITE);
 
-  // Изчертаване на окръжност, дъга от окръжност, част от кръг.
   arc(400,200,135,315,141);
   arc(200,400,315,135,141);
   arc(200,200,225,45,141);
   arc(400,400,45,225,141);
 
-  setlinestyle(0, 0, 2);
-  setfillstyle(1, 2);
-  //floodfill(300,200,15);
-  putpixel(300,200,15);
-  settextjustify(0, 0);
-  outtextxy(300, 200, "ab");
+  setfillstyle(1,GREEN);
+  floodfill(300,200,WHITE);
+  putpixel(300,200,WHITE);
+  settextjustify(0,0);
+  outtextxy(300,200,"ab");
 
+  setfillstyle(1,BLUE);
+  floodfill(400,300,WHITE);
+  putpixel(400,300,WHITE);
+  outtextxy(400,300,"ab");
+
+  setfillstyle(1,RED);
+  floodfill(300,400,WHITE);
+  putpixel(300,400,WHITE);
+  outtextxy(300,400,"ab");
+
+  setfillstyle(1,LIGHTBLUE);
+  floodfill(200,300,WHITE);
+  putpixel(200,300,WHITE);
+  outtextxy(200,300,"ab");
 
   getch();
   return 0;
