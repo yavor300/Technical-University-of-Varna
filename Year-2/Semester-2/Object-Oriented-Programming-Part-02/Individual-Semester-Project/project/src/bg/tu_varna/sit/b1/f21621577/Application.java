e43bf0b9aafba0403bb999e95510f3871b79fa40
yavor300 <yavor300@gmail.com>
2023-03-20 00:18:46 +0200

    [O.O.P-Individual-Semester-Project] (Week 5) Add Application logic to test the reader and writer classes.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/Application.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/Application.java
@@ -1,8 +1,37 @@
 package bg.tu_varna.sit.b1.f21621577;
 
-public class Main {
+import bg.tu_varna.sit.b1.f21621577.table.reader.TableReader;
+import bg.tu_varna.sit.b1.f21621577.table.repository.TableRepository;
+import bg.tu_varna.sit.b1.f21621577.table.writer.TableWriter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static bg.tu_varna.sit.b1.f21621577.config.Config.DEFAULT_RESOURCES_DIRECTORY;
+
+public class Application {
 
   public static void main(String[] args) {
-    // write your code here
+
+    File file = new File(DEFAULT_RESOURCES_DIRECTORY + "data.txt");
+    TableReader tableReader;
+    try {
+       tableReader = new TableReader(file);
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+
+    String[][] cells;
+    try {
+       cells = tableReader.read();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    TableWriter tableWriter = new TableWriter();
+    tableWriter.write(cells);
+
+    System.out.println(TableRepository.getInstance().visualizeData());
   }
 }
