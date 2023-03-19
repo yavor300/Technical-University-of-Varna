2159c3b43866fd92d6864835aabd81408f8c5b96
yavor300 <yavor300@gmail.com>
2023-03-20 00:15:30 +0200

    [O.O.P-Individual-Semester-Project] (Week 5) Add config class for storing common application properties.

---
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/config/Config.java
@@ -0,0 +1,15 @@
+package bg.tu_varna.sit.b1.f21621577.config;
+
+/**
+ * Config class holding all configurable properties for the application.
+ */
+public class Config {
+
+  public static final int ROWS = 100;
+
+  public static final int COLS = 100;
+
+  public static final String CELLS_SEPARATOR = " ";
+
+  public static final String DEFAULT_RESOURCES_DIRECTORY = "src/bg/tu_varna/sit/b1/f21621577/resources/";
+}
