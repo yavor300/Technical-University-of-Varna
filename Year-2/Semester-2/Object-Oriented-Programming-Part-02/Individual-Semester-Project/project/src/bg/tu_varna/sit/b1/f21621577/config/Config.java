10a2b46b5f9ec029c57a899c932627d46ff17af5
yavor300 <yavor300@gmail.com>
2023-03-20 18:02:32 +0200

    [O.O.P-Individual-Semester-Project] (Week 5) Add comma separator when reading the table cells from the file.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/config/Config.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/config/Config.java
@@ -11,5 +11,7 @@ public class Config {
 
   public static final String CELLS_SEPARATOR = " ";
 
+  public static final String CELLS_INPUT_SEPARATOR = ",";
+
   public static final String DEFAULT_RESOURCES_DIRECTORY = "src/bg/tu_varna/sit/b1/f21621577/resources/";
 }
