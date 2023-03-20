3b1f29ae8f9564ab1257ceefcff91a417b153248
yavor300 <yavor300@gmail.com>
2023-03-20 18:07:25 +0200

    [O.O.P-Individual-Semester-Project] (Week 5) Substitute the string representation of the tale with a TableCell class/

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/table/cell/TableCell.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/table/cell/TableCell.java
@@ -1,5 +1,18 @@
 package bg.tu_varna.sit.b1.f21621577.table.cell;
 
 public class TableCell {
-  
+
+  private String data;
+  private final boolean isDigit;
+  private int number;
+
+  public TableCell(String data) {
+    this.data = data;
+    this.isDigit = false;
+  }
+
+  public TableCell(int number) {
+    this.number = number;
+    this.isDigit = true;
+  }
 }
