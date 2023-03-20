0c0d592e5e57e3d875fdbb7c8cb3991cf05adf2e
yavor300 <yavor300@gmail.com>
2023-03-20 18:19:13 +0200

    [O.O.P-Individual-Semester-Project] (Week 5) Substitute the string representation of the table with a TableCell class.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/table/reader/TableReader.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/table/reader/TableReader.java
@@ -1,13 +1,14 @@
 package bg.tu_varna.sit.b1.f21621577.table.reader;
 
+import bg.tu_varna.sit.b1.f21621577.table.cell.TableCell;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import static bg.tu_varna.sit.b1.f21621577.config.Config.COLS;
-import static bg.tu_varna.sit.b1.f21621577.config.Config.ROWS;
+import static bg.tu_varna.sit.b1.f21621577.config.Config.*;
 
 /**
  * Reader class that is needed to read the data from the file
@@ -22,18 +23,22 @@ public class TableReader {
     this.inputReader = new BufferedReader(new FileReader(file));
   }
 
-  public String[][] read() throws IOException {
+  public TableCell[][] read() throws IOException {
 
-    String[][] result = new String[ROWS][COLS];
+    TableCell[][] result = new TableCell[ROWS][COLS];
 
     int row = 0;
     String line;
-
     while ((line = inputReader.readLine()) != null) {
-      String[] cells = line.split(",");
+      String[] cells = line.split(CELLS_INPUT_SEPARATOR);
 
       for (int col = 0; col < cells.length; col++) {
-        result[row][col] = cells[col].trim();
+        try {
+          int number = Integer.parseInt(cells[col]);
+          result[row][col] = new TableCell(number);
+        } catch (NumberFormatException e) {
+          result[row][col] = new TableCell(cells[col]);
+        }
       }
       row++;
     }
