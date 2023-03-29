c2f940af1942aa566199ae11a7187d8fdafac22a
yavor300 <yavor300@gmail.com>
2023-03-30 01:20:13 +0300

    [O.O.P-Individual-Semester-Project] (Week 6) Add documentation for the OpenCommandFactory class and replace array with list for the cli arguments.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/open/OpenCommandFactory.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/open/OpenCommandFactory.java
@@ -3,17 +3,34 @@ package bg.tu_varna.sit.b1.f21621577.command.implementation.open;
 import bg.tu_varna.sit.b1.f21621577.command.base.Command;
 import bg.tu_varna.sit.b1.f21621577.command.base.CommandAbstractFactory;
 
+import java.util.List;
+
+/**
+ * A factory for creating an OpenFileCommand object with a list of arguments.
+ */
 public class OpenCommandFactory implements CommandAbstractFactory {
 
-  private final String[] arguments;
+  /**
+   * The list of arguments to be passed to the OpenFileCommand.
+   */
+  private final List<String> arguments;
 
-  public OpenCommandFactory(String[] arguments) {
+  /**
+   * Constructs a new OpenCommandFactory object with a list of arguments.
+   *
+   * @param arguments the list of arguments to be passed to the OpenFileCommand
+   */
+  public OpenCommandFactory(List<String> arguments) {
     this.arguments = arguments;
   }
 
+  /**
+   * Creates a new OpenFileCommand object with the list of arguments.
+   *
+   * @return the new OpenFileCommand object
+   */
   @Override
   public Command createCommand() {
-
     return new OpenFileCommand(arguments);
   }
 }
