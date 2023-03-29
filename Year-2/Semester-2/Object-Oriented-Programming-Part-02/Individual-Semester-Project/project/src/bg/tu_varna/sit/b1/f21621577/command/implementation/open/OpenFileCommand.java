903b5cf137b9944010aab28e6e49980f084e1e37
yavor300 <yavor300@gmail.com>
2023-03-30 01:21:38 +0300

    [O.O.P-Individual-Semester-Project] (Week 6) Make the OpenFileCommand class to operate with Path instead of File. Add try-with-resources block when calling the TableReader.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/open/OpenFileCommand.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/open/OpenFileCommand.java
@@ -1,17 +1,35 @@
 package bg.tu_varna.sit.b1.f21621577.command.implementation.open;
 
 import bg.tu_varna.sit.b1.f21621577.command.base.Command;
+import bg.tu_varna.sit.b1.f21621577.table.reader.TableReader;
+import bg.tu_varna.sit.b1.f21621577.table.repository.TableRepository;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static bg.tu_varna.sit.b1.f21621577.config.Config.DEFAULT_RESOURCES_DIRECTORY;
 
 public class OpenFileCommand extends Command {
 
-  public OpenFileCommand(String[] arguments) {
+  private Path file;
+
+  public OpenFileCommand() {
+  }
+
+  public OpenFileCommand(List<String> arguments) {
     super(arguments);
+    this.file = Paths.get(DEFAULT_RESOURCES_DIRECTORY, arguments.get(0));
   }
 
   @Override
-  public void execute() throws IOException {
-
+  public boolean execute() throws IOException {
+    try (TableReader tableReader = new TableReader(file)) {
+      TableRepository.getInstance().save(tableReader.read());
+      return true;
+    } catch (IOException e) {
+      return false;
+    }
   }
 }
