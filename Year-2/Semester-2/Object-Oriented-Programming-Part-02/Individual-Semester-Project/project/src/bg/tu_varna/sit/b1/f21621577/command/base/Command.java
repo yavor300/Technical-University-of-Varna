0625240e094cf9fb002a6da8704987d8a3fc3d13
yavor300 <yavor300@gmail.com>
2023-03-30 01:18:17 +0300

    [O.O.P-Individual-Semester-Project] (Week 6) Add documentation for the Command clas and make the abstract method return boolean value.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/base/Command.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/base/Command.java
@@ -1,14 +1,65 @@
 package bg.tu_varna.sit.b1.f21621577.command.base;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import static bg.tu_varna.sit.b1.f21621577.config.Config.DEFAULT_TABLE_FILENAME;
+
+/**
+ * The abstract base class for all command classes.
+ * Commands are used to encapsulate functionality that can be executed on a system.
+ * This class defines a constructor that accepts a list of strings representing the command arguments,
+ * an abstract method "execute" which must be implemented by concrete command classes, and a method to
+ * retrieve the command arguments.
+ */
 public abstract class Command {
 
-  private final String[] arguments;
+  private List<String> arguments;
 
-  public Command(String[] arguments) {
+  public Command() {
+    this.arguments = new ArrayList<>(Collections.singletonList(DEFAULT_TABLE_FILENAME));
+  }
+
+  /**
+   * Constructs a new Command object with the specified array of arguments.
+   *
+   * @param arguments the arguments for the command
+   */
+  public Command(List<String> arguments) {
     this.arguments = arguments;
   }
 
-  public abstract void execute() throws IOException;
+  /**
+   * Executes the command.
+   * This method must be implemented by concrete command classes.
+   *
+   * @return true if the command was executed successfully; false otherwise
+   * @throws IOException if an I/O error occurs during command execution
+   */
+  public abstract boolean execute() throws IOException;
+
+  /**
+   * Returns the arguments for the command.
+   *
+   * @return the arguments for the command as a list of strings
+   */
+  public List<String> getArguments() {
+    return arguments;
+  }
+
+  /**
+   * Sets the arguments for the command.
+   *
+   * @param arguments the arguments for the command as a list of strings
+   */
+  public void setArguments(List<String> arguments) {
+
+    if (arguments == null || arguments.size() == 0) {
+      this.arguments = new ArrayList<>(Collections.singletonList(DEFAULT_TABLE_FILENAME));
+    }
+
+    this.arguments = arguments;
+  }
 }
