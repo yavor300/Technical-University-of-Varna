306aa0dd5545d4bf0e1d3acaa22b0c25d9403634
yavor300 <yavor300@gmail.com>
2023-03-30 23:21:24 +0300

    [O.O.P-Individual-Semester-Project] (Week 6) Add arguments validations to the factort class.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/open/OpenCommandFactory.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/open/OpenCommandFactory.java
@@ -3,7 +3,11 @@ package bg.tu_varna.sit.b1.f21621577.command.implementation.open;
 import bg.tu_varna.sit.b1.f21621577.command.base.Command;
 import bg.tu_varna.sit.b1.f21621577.command.base.CommandAbstractFactory;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static bg.tu_varna.sit.b1.f21621577.config.Config.DEFAULT_TABLE_FILENAME;
 
 /**
  * A factory for creating an OpenFileCommand object with a list of arguments.
@@ -13,7 +17,7 @@ public class OpenCommandFactory implements CommandAbstractFactory {
   /**
    * The list of arguments to be passed to the OpenFileCommand.
    */
-  private final List<String> arguments;
+  private List<String> arguments;
 
   /**
    * Constructs a new OpenCommandFactory object with a list of arguments.
@@ -21,7 +25,7 @@ public class OpenCommandFactory implements CommandAbstractFactory {
    * @param arguments the list of arguments to be passed to the OpenFileCommand
    */
   public OpenCommandFactory(List<String> arguments) {
-    this.arguments = arguments;
+    setArguments(arguments);
   }
 
   /**
@@ -32,5 +36,20 @@ public class OpenCommandFactory implements CommandAbstractFactory {
   @Override
   public Command createCommand() {
     return new OpenFileCommand(arguments);
+  }
+
+  /**
+   * Sets the list of arguments for the command. If the argument list is null or empty,
+   * the default table filename will be used. Otherwise, the provided argument list will be used.
+   *
+   * @param arguments the list of arguments to set for the command, or null/empty to use the default filename
+   */
+  public void setArguments(List<String> arguments) {
+
+    if (arguments == null || arguments.size() == 0) {
+      this.arguments = new ArrayList<>(Collections.singletonList(DEFAULT_TABLE_FILENAME));
+    } else {
+      this.arguments = arguments;
+    }
   }
 }
