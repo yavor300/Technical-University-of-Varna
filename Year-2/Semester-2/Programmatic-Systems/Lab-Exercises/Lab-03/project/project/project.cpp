a86741cc409dc25b10176132194907ba93785b25
yavor300 <yavor300@gmail.com>
2023-04-03 10:14:06 +0300

    [Programmatic-Systems] (Week 6) Format the solution of Lab-03 and add comments.

--- Year-2/Semester-2/Programmatic-Systems/Lab-Exercises/Lab-03/project/project/project.cpp
+++ Year-2/Semester-2/Programmatic-Systems/Lab-Exercises/Lab-03/project/project/project.cpp
@@ -2,86 +2,80 @@
 //
 
 #include "stdafx.h"
+
 #include "project.h"
 
 #define MAX_LOADSTRING 100
 
 // Global Variables:
-HINSTANCE hInst;								// current instance
-TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
-TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+HINSTANCE hInst; // current instance
+TCHAR szTitle[MAX_LOADSTRING]; // The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING]; // the main window class name
 
 // Forward declarations of functions included in this code module:
-ATOM				MyRegisterClass(HINSTANCE hInstance);
-BOOL				InitInstance(HINSTANCE, int);
-LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK	TaskOne(HWND, UINT, WPARAM, LPARAM);
+ATOM MyRegisterClass(HINSTANCE hInstance);
+BOOL InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK TaskOne(HWND, UINT, WPARAM, LPARAM); // Callback дефиниция за DialogBox
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
-											 _In_opt_ HINSTANCE hPrevInstance,
-											 _In_ LPTSTR    lpCmdLine,
-											 _In_ int       nCmdShow)
-{
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
+  _In_opt_ HINSTANCE hPrevInstance,
+  _In_ LPTSTR lpCmdLine,
+  _In_ int nCmdShow) {
+  UNREFERENCED_PARAMETER(hPrevInstance);
+  UNREFERENCED_PARAMETER(lpCmdLine);
 
-	// TODO: Place code here.
-	MSG msg;
-	HACCEL hAccelTable;
+  // TODO: Place code here.
+  MSG msg;
+  HACCEL hAccelTable;
 
-	// Initialize global strings
-	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-	LoadString(hInstance, IDC_PROJECT, szWindowClass, MAX_LOADSTRING);
-	MyRegisterClass(hInstance);
+  // Initialize global strings
+  LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+  LoadString(hInstance, IDC_PROJECT, szWindowClass, MAX_LOADSTRING);
+  MyRegisterClass(hInstance);
 
-	// Perform application initialization:
-	if (!InitInstance (hInstance, nCmdShow))
-	{
-		return FALSE;
-	}
+  // Perform application initialization:
+  if (!InitInstance(hInstance, nCmdShow)) {
+    return FALSE;
+  }
 
-	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_PROJECT));
+  hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_PROJECT));
 
-	// Main message loop:
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-	}
+  // Main message loop:
+  while (GetMessage( & msg, NULL, 0, 0)) {
+    if (!TranslateAccelerator(msg.hwnd, hAccelTable, & msg)) {
+      TranslateMessage( & msg);
+      DispatchMessage( & msg);
+    }
+  }
 
-	return (int) msg.wParam;
+  return (int) msg.wParam;
 }
-
-
 
 //
 //  FUNCTION: MyRegisterClass()
 //
 //  PURPOSE: Registers the window class.
 //
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-	WNDCLASSEX wcex;
+ATOM MyRegisterClass(HINSTANCE hInstance) {
+  WNDCLASSEX wcex;
 
-	wcex.cbSize = sizeof(WNDCLASSEX);
+  wcex.cbSize = sizeof(WNDCLASSEX);
 
-	wcex.style			= CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc	= WndProc;
-	wcex.cbClsExtra		= 0;
-	wcex.cbWndExtra		= 0;
-	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PROJECT));
-	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_PROJECT);
-	wcex.lpszClassName	= szWindowClass;
-	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+  wcex.style = CS_HREDRAW | CS_VREDRAW;
+  wcex.lpfnWndProc = WndProc;
+  wcex.cbClsExtra = 0;
+  wcex.cbWndExtra = 0;
+  wcex.hInstance = hInstance;
+  wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PROJECT));
+  wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+  wcex.lpszMenuName = MAKEINTRESOURCE(IDC_PROJECT);
+  wcex.lpszClassName = szWindowClass;
+  wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-	return RegisterClassEx(&wcex);
+  return RegisterClassEx( & wcex);
 }
 
 //
@@ -94,24 +88,22 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //        In this function, we save the instance handle in a global variable and
 //        create and display the main program window.
 //
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-	HWND hWnd;
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
+  HWND hWnd;
 
-	hInst = hInstance; // Store instance handle in our global variable
+  hInst = hInstance; // Store instance handle in our global variable
 
-	hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+  hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
 
-	if (!hWnd)
-	{
-		return FALSE;
-	}
+  if (!hWnd) {
+    return FALSE;
+  }
 
-	ShowWindow(hWnd, nCmdShow);
-	UpdateWindow(hWnd);
+  ShowWindow(hWnd, nCmdShow);
+  UpdateWindow(hWnd);
 
-	return TRUE;
+  return TRUE;
 }
 
 //
@@ -124,127 +116,98 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY	- post a quit message and return
 //
 //
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	int wmId, wmEvent;
-	PAINTSTRUCT ps;
-	HDC hdc;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  int wmId, wmEvent;
+  PAINTSTRUCT ps;
+  HDC hdc;
 
-	switch (message)
-	{
-	case WM_COMMAND:
-		wmId    = LOWORD(wParam);
-		wmEvent = HIWORD(wParam);
-		// Parse the menu selections:
-		switch (wmId)
-		{
-		case IDM_ABOUT:
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-			break;
-		case IDM_EXIT:
-			DestroyWindow(hWnd);
-			break;
-		case ID_T12:
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, TaskOne);
-			break;
-		case ID_T11:
-			switch (MessageBox(hWnd,"Message Box", "MSG",MB_YESNOCANCEL | MB_ICONERROR)) {
-			case IDYES:
-				MessageBox(hWnd, "Yes", "MSG", MB_OK);
-				break;
-			case IDNO:
-				MessageBox(hWnd, "No", "MSG", MB_OK);
-				break;
-			case IDCANCEL:
-				MessageBox(hWnd, "Cancel", "MSG", MB_OK);
-				break;
-			default:
-				break;
-			}
-			break;
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
-		}
-		break;
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		// TODO: Add any drawing code here...
-		EndPaint(hWnd, &ps);
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return 0;
+  switch (message) {
+  case WM_COMMAND:
+    wmId = LOWORD(wParam);
+    wmEvent = HIWORD(wParam);
+    // Parse the menu selections:
+    switch (wmId) {
+    case IDM_ABOUT:
+      DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About); // Визуализиране на DialogBox
+      break;
+    case IDM_EXIT:
+      DestroyWindow(hWnd);
+      break;
+    case ID_T12:
+      DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, TaskOne); // Визуализиране на DialogBox
+      break;
+    case ID_T11:
+      MessageBox(hWnd, "Message Box", "T11", MB_OK); // Визуализиране на MessageBox
+      break;
+    default:
+      return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    break;
+  case WM_PAINT:
+    hdc = BeginPaint(hWnd, & ps);
+    // TODO: Add any drawing code here...
+    EndPaint(hWnd, & ps);
+    break;
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    break;
+  default:
+    return DefWindowProc(hWnd, message, wParam, lParam);
+  }
+  return 0;
 }
 
 // Message handler for task one box.
-INT_PTR CALLBACK TaskOne(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	UNREFERENCED_PARAMETER(lParam);
-	switch (message)
-	{
-	case WM_INITDIALOG:
-		SetDlgItemText(hDlg,IDC_EDIT1,"Hello, World!");
-		return (INT_PTR)TRUE;
+INT_PTR CALLBACK TaskOne(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+  UNREFERENCED_PARAMETER(lParam);
+  switch (message) {
+  case WM_INITDIALOG:
+    SetDlgItemText(hDlg, IDC_EDIT1, "Hello, World!"); // Задаване на текст предварително в EditControl
+    return (INT_PTR) TRUE;
 
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-		{
-			EndDialog(hDlg, LOWORD(wParam));
-			return (INT_PTR)TRUE;
-		}
-		else if (LOWORD(wParam) == IDC_BUTTON2)
-		{
-			char textFromFirstBox[100];
-			GetDlgItemText(hDlg, IDC_EDIT1, textFromFirstBox, 100);
-			SetDlgItemText(hDlg, IDC_EDIT3, textFromFirstBox);
-		}
-		else if (LOWORD(wParam) == IDC_BUTTON3)
-		{
-			char textFromSecondBox[100];
-			GetDlgItemText(hDlg, IDC_EDIT3, textFromSecondBox, 100);
-			SetDlgItemText(hDlg, IDC_EDIT1, textFromSecondBox);
-		}
-		else if (LOWORD(wParam) == IDC_PRESS) {
-			switch (MessageBox(hDlg,"Message Box", "MSG",MB_YESNOCANCEL | MB_ICONERROR)) {
-			case IDYES:
-				MessageBox(hDlg, "Yes", "MSG", MB_OK);
-				break;
-			case IDNO:
-				MessageBox(hDlg, "No", "MSG", MB_OK);
-				break;
-			case IDCANCEL:
-				MessageBox(hDlg, "Cancel", "MSG", MB_OK);
-				break;
-			default:
-				break;
-			}
-		}
+  case WM_COMMAND:
+    if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
+      EndDialog(hDlg, LOWORD(wParam));
+      return (INT_PTR) TRUE;
+    } else if (LOWORD(wParam) == IDC_BUTTON2) {
+      char textFromFirstBox[100]; // String за съхранение на текст от EditControl
+      GetDlgItemText(hDlg, IDC_EDIT1, textFromFirstBox, 100); // Четене на и записване на текста в String
+      SetDlgItemText(hDlg, IDC_EDIT3, textFromFirstBox); // Задаване на текст в EditControl
+    } else if (LOWORD(wParam) == IDC_BUTTON3) {
+      char textFromSecondBox[100];
+      GetDlgItemText(hDlg, IDC_EDIT3, textFromSecondBox, 100);
+      SetDlgItemText(hDlg, IDC_EDIT1, textFromSecondBox);
+    } else if (LOWORD(wParam) == IDC_PRESS) {
+      switch (MessageBox(hDlg, "Message Box", "MSG", MB_OKCANCEL | MB_ICONQUESTION)) { // Проверка за натиснат бутон от MessageBox
+      case IDOK:
+        MessageBox(hDlg, "Ok", "Message Box", MB_OK); // Показване на нов MessageBox ако потребителят е натиснал OK
+        break;
+      case IDCANCEL:
+        MessageBox(hDlg, "Cancel", "Message Box", MB_OK);
+        break;
+      default:
+        break;
+      }
+    }
 
-		break;
-	}
-	return (INT_PTR)FALSE;
+    break;
+  }
+  return (INT_PTR) FALSE;
 }
 
 // Message handler for about box.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	UNREFERENCED_PARAMETER(lParam);
-	switch (message)
-	{
-	case WM_INITDIALOG:
-		return (INT_PTR)TRUE;
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+  UNREFERENCED_PARAMETER(lParam);
+  switch (message) {
+  case WM_INITDIALOG:
+    return (INT_PTR) TRUE;
 
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-		{
-			EndDialog(hDlg, LOWORD(wParam));
-			return (INT_PTR)TRUE;
-		}
-		break;
-	}
-	return (INT_PTR)FALSE;
+  case WM_COMMAND:
+    if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
+      EndDialog(hDlg, LOWORD(wParam));
+      return (INT_PTR) TRUE;
+    }
+    break;
+  }
+  return (INT_PTR) FALSE;
 }
