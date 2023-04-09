2c4bb975b631c56d49b04ec359745cdca516ffa7
yavor300 <yavor300@gmail.com>
2023-04-10 00:03:39 +0300

    [O.O.P-Individual-Semester-Project] (Week7) Use .csv file instead of .txt.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/config/Config.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/config/Config.java
@@ -15,7 +15,7 @@ public class Config {
 
   public static final String DEFAULT_RESOURCES_DIRECTORY = "src/bg/tu_varna/sit/b1/f21621577/resources/";
 
-  public static final String DEFAULT_TABLE_FILENAME = "data.txt";
+  public static final String DEFAULT_TABLE_FILENAME = "data.csv";
 
   public static final String EXIT_PROGRAM_MENU_COMMAND = "exit";
 }
