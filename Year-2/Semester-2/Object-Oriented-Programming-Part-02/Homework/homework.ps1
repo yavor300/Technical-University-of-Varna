f55b997912c28e27ca8f0c23e28712727075f9eb
yavor300 <yavor300@gmail.com>
2023-04-10 00:06:36 +0300

    [O.O.P] (Week7) Add powershell script that moves all homework java files to one common folder.

---
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Homework/homework.ps1
@@ -0,0 +1,11 @@
+# Set the source and destination directories
+$src_dir = "E:\Technical-University-of-Varna\Year-2\Semester-2\Object-Oriented-Programming-Part-02\Homework\Homework-04"
+$dst_dir = "E:\Technical-University-of-Varna\Year-2\Semester-2\Object-Oriented-Programming-Part-02\Homework\Homework-04-To-Zip"
+
+# Create the destination directory if it doesn't exist
+New-Item -ItemType Directory -Force $dst_dir | Out-Null
+
+Get-ChildItem -Path $src_dir -Include *.java -Recurse -File | ForEach-Object {
+    $destinationFile = Join-Path $dst_dir $_.Name
+    Copy-Item -Path $_.FullName -Destination $destinationFile -Force
+}
