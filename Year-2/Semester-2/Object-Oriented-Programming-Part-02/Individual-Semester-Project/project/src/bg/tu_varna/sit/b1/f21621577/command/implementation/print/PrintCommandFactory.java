cf12f9738ddb4f9c04df5f4f60cdfbfdeb673fa8
yavor300 <yavor300@gmail.com>
2023-04-15 12:41:00 +0300

    [O.O.P-Individual-Semester-Project] (Week 9) Remove the arguments field from the open command.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/print/PrintCommandFactory.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/print/PrintCommandFactory.java
@@ -3,26 +3,10 @@ package bg.tu_varna.sit.b1.f21621577.command.implementation.print;
 import bg.tu_varna.sit.b1.f21621577.command.base.Command;
 import bg.tu_varna.sit.b1.f21621577.command.base.CommandAbstractFactory;
 
-import java.util.List;
-
 /**
  * A factory for creating an PrintFileCommand object with a list of arguments.
  */
 public class PrintCommandFactory implements CommandAbstractFactory {
-
-  /**
-   * The list of arguments to be passed to the PrintFileCommand.
-   */
-  private List<String> arguments;
-
-  /**
-   * Constructs a new PrintFileCommand object with a list of arguments.
-   *
-   * @param arguments the list of arguments to be passed to the PrintFileCommand
-   */
-  public PrintCommandFactory(List<String> arguments) {
-    setArguments(arguments);
-  }
 
   /**
    * Creates a new PrintFileCommand object with the list of arguments.
@@ -31,16 +15,6 @@ public class PrintCommandFactory implements CommandAbstractFactory {
    */
   @Override
   public Command createCommand() {
-    return new PrintFileCommand(arguments);
-  }
-
-
-  /**
-   * TODO That method should be removed by creating an additional base class with arguments.
-   *
-   * @param arguments the list of arguments to be passed to the PrintFileCommand
-   */
-  public void setArguments(List<String> arguments) {
-    this.arguments = arguments;
+    return new PrintFileCommand();
   }
 }
