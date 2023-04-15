9910d820d85ee3cce62e44eab6d1dd416e405a8a
yavor300 <yavor300@gmail.com>
2023-04-15 12:38:15 +0300

    [O.O.P-Individual-Semester-Project] (Week 9) Add JavaDoc to the  CommandFactory class.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/CommandFactory.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/CommandFactory.java
@@ -3,8 +3,17 @@ package bg.tu_varna.sit.b1.f21621577.command.implementation;
 import bg.tu_varna.sit.b1.f21621577.command.base.Command;
 import bg.tu_varna.sit.b1.f21621577.command.base.CommandAbstractFactory;
 
+/**
+ * A factory class for creating commands based on the provided abstract factory.
+ */
 public class CommandFactory {
 
+  /**
+   * Returns a new command instance created by the provided abstract factory.
+   *
+   * @param factory the abstract factory to use for creating the command
+   * @return the newly created command instance
+   */
   public static Command getCommand(CommandAbstractFactory factory) {
     return factory.createCommand();
   }
