d6790b753348cea8250c4a0cd0b7e8ec28184c77
yavor300 <yavor300@gmail.com>
2023-04-15 12:43:58 +0300

    [O.O.P-Individual-Semester-Project] (Week 9) Create new abstract class for commands with arguments.

---
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/base/ArgumentCommand.java
@@ -0,0 +1,34 @@
+package bg.tu_varna.sit.b1.f21621577.command.base;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static bg.tu_varna.sit.b1.f21621577.config.Config.DEFAULT_TABLE_FILENAME;
+
+public abstract class ArgumentCommand implements Command {
+
+  private final List<String> arguments;
+
+  public ArgumentCommand() {
+    this.arguments = new ArrayList<>(Collections.singletonList(DEFAULT_TABLE_FILENAME));
+  }
+
+  /**
+   * Constructs a new ArgumentCommand object with the specified array of arguments.
+   *
+   * @param arguments the arguments for the command
+   */
+  public ArgumentCommand(List<String> arguments) {
+    this.arguments = arguments;
+  }
+
+  /**
+   * Returns the arguments for the command.
+   *
+   * @return the arguments for the command as a list of strings
+   */
+  public List<String> getArguments() {
+    return arguments;
+  }
+}
