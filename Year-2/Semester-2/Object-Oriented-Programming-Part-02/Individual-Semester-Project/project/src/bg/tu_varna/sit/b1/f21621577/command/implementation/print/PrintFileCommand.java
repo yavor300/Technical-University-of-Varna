0948a7388cf606db0a59f6d15079670bc9442e19
yavor300 <yavor300@gmail.com>
2023-04-15 12:02:48 +0300

    [O.O.P-Individual-Semester-Project] (Week 9) Improve the format of printing the table. Split the functionality to functions.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/print/PrintFileCommand.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/print/PrintFileCommand.java
@@ -1,7 +1,6 @@
 package bg.tu_varna.sit.b1.f21621577.command.implementation.print;
 
 import bg.tu_varna.sit.b1.f21621577.command.base.Command;
-import bg.tu_varna.sit.b1.f21621577.table.cell.CellType;
 import bg.tu_varna.sit.b1.f21621577.table.cell.TableCell;
 import bg.tu_varna.sit.b1.f21621577.table.repository.TableRepository;
 
@@ -14,23 +13,36 @@ public class PrintFileCommand extends Command {
   }
 
   /**
-   * Executes the print table command.
+   * Executes the command to display the contents of the table.
+   *
+   * @return true if the command is executed successfully
    */
   @Override
   public boolean execute() {
 
     TableRepository table = TableRepository.getInstance();
-
     int numRows = table.getNumRows();
     int numCols = table.getNumColumns();
+    int[] colWidths = computeColumnWidths(table, numRows, numCols);
+    printTableData(table, numRows, numCols, colWidths);
 
-    // Compute the maximum length of each column
+    return true;
+  }
+
+  /**
+   * Computes the maximum width for each column in the specified table.
+   *
+   * @param table   the table to compute the column widths for
+   * @param numRows the number of rows in the table
+   * @param numCols the number of columns in the table
+   * @return an array containing the maximum width for each column
+   */
+  private int[] computeColumnWidths(TableRepository table, int numRows, int numCols) {
+
     int[] colWidths = new int[numCols];
-
     for (int j = 0; j < numCols; j++) {
       int maxColWidth = 0;
       for (int i = 0; i < numRows; i++) {
-
         TableCell cell = table.getCell(i, j);
         if (cell != null) {
           String cellValue = cell.getValueAsString();
@@ -42,31 +54,38 @@ public class PrintFileCommand extends Command {
       colWidths[j] = maxColWidth;
     }
 
-    // Print table data
+    return colWidths;
+  }
+
+  /**
+   * Prints the table data to the console, using the provided table, number of rows and columns, and an array of column
+   * widths.
+   *
+   * @param table     the table containing the data to be printed
+   * @param numRows   the number of rows in the table
+   * @param numCols   the number of columns in the table
+   * @param colWidths an array of integers representing the maximum width of each column
+   */
+  private void printTableData(TableRepository table, int numRows, int numCols, int[] colWidths) {
+
     for (int i = 0; i < numRows; i++) {
       System.out.print("| ");
       for (int j = 0; j < numCols; j++) {
         TableCell cell = table.getCell(i, j);
+        int colWidth = colWidths[j];
         if (cell != null) {
           String cellValue = cell.getValueAsString();
-          int colWidth = colWidths[j];
           if (j == 0) {
-            // Float the first column of numbers to the left
             System.out.printf("%-" + colWidth + "s", cellValue);
           } else {
-            // Align the rest of the columns with the numbers to the right
-            if (cell.getType() == CellType.INTEGER || cell.getType() == CellType.FRACTIONAL) {
-              System.out.printf("%" + colWidth + "s", cellValue);
-            } else {
-              System.out.printf("%-" + colWidth + "s", cellValue);
-            }
+            System.out.printf("%" + colWidth + "s", cellValue);
           }
+        } else {
+          System.out.printf("%" + colWidth + "s", "");
         }
         System.out.print(" | ");
       }
       System.out.println();
     }
-
-    return true;
   }
 }
