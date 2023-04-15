9ec2e7f67d3d6b8f425577212edcc1956c250262
yavor300 <yavor300@gmail.com>
2023-04-15 15:20:23 +0300

    [O.O.P-Individual-Semester-Project] (Week 9) Make the execute method return a String value.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/base/Command.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/base/Command.java
@@ -3,17 +3,16 @@ package bg.tu_varna.sit.b1.f21621577.command.base;
 import java.io.IOException;
 
 /**
- * The Command interface represents a single command that can be executed by the application.
- * All concrete command classes must implement this interface.
+ * The Command interface defines the behavior of a command that can be executed by the application.
  */
 public interface Command {
 
   /**
    * Executes the command.
    *
-   * @return {@code true} if the command was executed successfully; {@code false} otherwise
+   * @return a message indicating the result of the command execution
    * @throws IOException if an I/O error occurs during command execution
    */
-  boolean execute() throws IOException;
+  String execute() throws IOException;
 
 }
