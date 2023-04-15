0206a8591866c80318014b9011056ce2f151806a
yavor300 <yavor300@gmail.com>
2023-04-15 12:39:09 +0300

    [O.O.P-Individual-Semester-Project] (Week 9) Add JavaDoc to the OpenFileCommand class.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/open/OpenFileCommand.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/open/OpenFileCommand.java
@@ -1,6 +1,6 @@
 package bg.tu_varna.sit.b1.f21621577.command.implementation.open;
 
-import bg.tu_varna.sit.b1.f21621577.command.base.Command;
+import bg.tu_varna.sit.b1.f21621577.command.base.ArgumentCommand;
 import bg.tu_varna.sit.b1.f21621577.table.reader.TableReader;
 import bg.tu_varna.sit.b1.f21621577.table.repository.TableRepository;
 
@@ -11,20 +11,38 @@ import java.util.List;
 
 import static bg.tu_varna.sit.b1.f21621577.config.Config.DEFAULT_RESOURCES_DIRECTORY;
 
-public class OpenFileCommand extends Command {
+/**
+ * A command class that opens a table file and saves its contents in the table repository.
+ */
+public class OpenFileCommand extends ArgumentCommand {
 
   private Path file;
 
+  /**
+   * Constructs an OpenFileCommand instance.
+   */
   public OpenFileCommand() {
   }
 
+  /**
+   * Constructs an OpenFileCommand instance with a list of arguments.
+   *
+   * @param arguments the list of arguments for the command
+   */
   public OpenFileCommand(List<String> arguments) {
     super(arguments);
     this.file = Paths.get(DEFAULT_RESOURCES_DIRECTORY, arguments.get(0));
   }
 
+  /**
+   * Executes the command.
+   * Opens the table file specified in the arguments, reads its contents and saves them in the table repository.
+   *
+   * @return true if the command was executed successfully; false otherwise
+   */
   @Override
-  public boolean execute() throws IOException {
+  public boolean execute() {
+
     try (TableReader tableReader = new TableReader(file)) {
       TableRepository.getInstance().save(tableReader.read());
       return true;
