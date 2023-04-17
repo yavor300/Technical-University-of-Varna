87a95e2308cf91c9be073fca7e67755166966624
yavor300 <yavor300@gmail.com>
2023-04-17 22:19:15 +0300

    [O.O.P-Individual-Semester-Project] (Week 9) Add TableWriter for writing the data to a file.

---
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/table/writer/TableWriter.java
@@ -0,0 +1,69 @@
+package bg.tu_varna.sit.b1.f21621577.table.writer;
+
+import bg.tu_varna.sit.b1.f21621577.table.cell.CellType;
+import bg.tu_varna.sit.b1.f21621577.table.cell.TableCell;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static bg.tu_varna.sit.b1.f21621577.config.Config.CELLS_OUTPUT_SEPARATOR;
+
+/**
+ * Writer class that is needed to write the table data to a file.
+ */
+public class TableWriter implements AutoCloseable {
+
+  private final BufferedWriter outputWriter;
+
+  /**
+   * Constructs a new instance of {@link TableWriter}.
+   *
+   * @param path the path to the file to write to
+   * @throws IOException if an I/O error occurs
+   */
+  public TableWriter(Path path) throws IOException {
+    this.outputWriter = Files.newBufferedWriter(path);
+  }
+
+  /**
+   * Writes the given table data to the output writer.
+   *
+   * @param tableData the table data to write
+   * @throws IOException if an I/O error occurs
+   */
+  public void write(TableCell[][] tableData) throws IOException {
+
+    for (TableCell[] tableDatum : tableData) {
+      StringBuilder lineBuilder = new StringBuilder();
+
+      for (int col = 0; col < tableDatum.length; col++) {
+        TableCell cell = tableDatum[col];
+        if (cell.getType() == CellType.STRING) {
+          lineBuilder.append("\"").append(cell.getValueAsString()).append("\"");
+        } else {
+          lineBuilder.append(cell.getValueAsString());
+        }
+        if (col < tableDatum.length - 1) {
+          lineBuilder.append(CELLS_OUTPUT_SEPARATOR);
+        }
+      }
+
+      outputWriter.write(lineBuilder.toString());
+      outputWriter.newLine();
+    }
+
+    outputWriter.flush();
+  }
+
+  /**
+   * Closes the output writer.
+   *
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  public void close() throws IOException {
+    outputWriter.close();
+  }
+}
