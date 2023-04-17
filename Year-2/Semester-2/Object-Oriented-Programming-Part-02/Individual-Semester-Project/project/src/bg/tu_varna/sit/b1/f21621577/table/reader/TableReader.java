b17f5b53889e5020323da606d1d41acca65edd3b
yavor300 <yavor300@gmail.com>
2023-04-17 11:52:21 +0300

    [O.O.P-Individual-Semester-Project] (Week 9) Add logic to create a new file if the given one does not exist.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/table/reader/TableReader.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/table/reader/TableReader.java
@@ -12,18 +12,33 @@ import static bg.tu_varna.sit.b1.f21621577.config.Config.COLS;
 import static bg.tu_varna.sit.b1.f21621577.config.Config.ROWS;
 
 /**
- * Reader class that is needed to read the data from the file
- * validate it and prepare it for storing the client input
- * tot the actual table repository.
+ * A reader class that is used to read data from a file, validate it,
+ * and prepare it for storing the client input in the actual table repository.
  */
 public class TableReader implements AutoCloseable {
 
   private final BufferedReader inputReader;
 
+  /**
+   * Constructs a new {@code TableReader} instance with the specified file path. If the file does not exist, a new file
+   * will be created.
+   *
+   * @param path the path of the file to read
+   * @throws IOException if an I/O error occurs while opening the file
+   */
   public TableReader(Path path) throws IOException {
+    if (!Files.exists(path)) {
+      Files.createFile(path);
+    }
     this.inputReader = Files.newBufferedReader(path);
   }
 
+  /**
+   * Reads the data from the file and returns a 2D array of {@code TableCell} objects.
+   *
+   * @return a 2D array of {@code TableCell} objects
+   * @throws IOException if an I/O error occurs while reading the file
+   */
   public TableCell[][] read() throws IOException {
 
     TableCell[][] result = new TableCell[ROWS][COLS];
@@ -45,6 +60,11 @@ public class TableReader implements AutoCloseable {
     return result;
   }
 
+  /**
+   * Closes the input stream.
+   *
+   * @throws IOException if an I/O error occurs while closing the stream
+   */
   @Override
   public void close() throws IOException {
     inputReader.close();
