98b18ecc5dc4f3ec50c75f7d18bc5d9cd5a291ee
yavor300 <yavor300@gmail.com>
2023-05-06 14:12:10 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add complete Javadoc for the EditComamndFactory class.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/edit/EditCommandFactory.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/edit/EditCommandFactory.java
@@ -7,18 +7,26 @@ import java.util.List;
 
 /**
  * A factory for creating an EditCommand object with a list of arguments.
+ * <p>
+ * Фабричен клас за създаване на обект EditCommand със списък от аргументи.
  */
 public class EditCommandFactory implements CommandAbstractFactory {
 
   /**
    * The list of arguments to be passed to the EditCommand.
+   * <p>
+   * Списъкът с аргументи, които трябва да бъдат предадени на EditCommand.
    */
   private final List<String> arguments;
 
   /**
    * Constructs a new EditCommandFactory object with a list of arguments.
+   * <p>
+   * Конструира нов обект EditCommandFactory със списък от аргументи.
    *
    * @param arguments the list of arguments to be passed to the EditCommand
+   *                  <p>
+   *                  списъкът с аргументи, които да бъдат предадени на EditCommand
    */
   public EditCommandFactory(List<String> arguments) {
     this.arguments = arguments;
@@ -26,12 +34,15 @@ public class EditCommandFactory implements CommandAbstractFactory {
 
   /**
    * Creates a new EditCommand object with the list of arguments.
+   * <p>
+   * Създава нов обект EditCommand със списъка с аргументи.
    *
    * @return the new EditCommand object
+   * <p>
+   * новия обект EditCommand
    */
   @Override
   public Command createCommand() {
     return new EditCommand(arguments);
   }
-
 }
