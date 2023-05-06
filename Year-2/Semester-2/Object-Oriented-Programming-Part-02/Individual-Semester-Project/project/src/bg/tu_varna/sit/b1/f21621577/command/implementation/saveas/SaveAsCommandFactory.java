3f3efd19a1192dda5a6bc0aa168a92f6b843f291
yavor300 <yavor300@gmail.com>
2023-05-06 18:43:10 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add complete Javadoc for the SaveAsCommandFactory  class.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/saveas/SaveAsCommandFactory.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/saveas/SaveAsCommandFactory.java
@@ -8,18 +8,26 @@ import java.util.List;
 
 /**
  * A factory for creating an SaveCommand object with a list of arguments.
+ * <p>
+ * Клас-фабрика за създаване на обект SaveCommand със списък от аргументи.
  */
 public class SaveAsCommandFactory implements CommandAbstractFactory {
 
   /**
    * The list of arguments to be passed to the SaveCommand.
+   * <p>
+   * Списъкът с аргументи, които трябва да бъдат предадени на SaveCommand.
    */
   private final List<String> arguments;
 
   /**
    * Constructs a new SaveCommand object with a list of arguments.
+   * <p>
+   * Конструира нов обект SaveCommand със списък от аргументи.
    *
    * @param arguments the list of arguments to be passed to the SaveCommand
+   *                  <p>
+   *                  списъкът с аргументи, които да бъдат предадени на SaveCommand
    */
   public SaveAsCommandFactory(List<String> arguments) {
     this.arguments = arguments;
@@ -27,8 +35,12 @@ public class SaveAsCommandFactory implements CommandAbstractFactory {
 
   /**
    * Creates a new SaveCommand object with the list of arguments.
+   * <p>
+   * Създава нов обект SaveCommand със списъка с аргументи.
    *
    * @return the new SaveCommand object
+   * <p>
+   * новия обект SaveCommand
    */
   @Override
   public Command createCommand() {
