27d71f902c60390e8cbfaa4bee338023c2fa4df2
yavor300 <yavor300@gmail.com>
2023-05-06 12:58:26 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add complete Javadoc for the CloseCommandFactory class.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/close/CloseCommandFactory.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/close/CloseCommandFactory.java
@@ -5,13 +5,19 @@ import bg.tu_varna.sit.b1.f21621577.command.base.CommandAbstractFactory;
 
 /**
  * A factory for creating an CloseCommand object with a list of arguments.
+ * <p>
+ * Фабричен клас за създаване на обект CloseCommand със списък от аргументи.
  */
 public class CloseCommandFactory implements CommandAbstractFactory {
 
   /**
    * Creates a new CloseCommand object.
+   * <p>
+   * Създава нов обект CloseCommand.
    *
    * @return the new CloseCommand object
+   * <p>
+   * новия обект CloseCommand
    */
   @Override
   public Command createCommand() {
