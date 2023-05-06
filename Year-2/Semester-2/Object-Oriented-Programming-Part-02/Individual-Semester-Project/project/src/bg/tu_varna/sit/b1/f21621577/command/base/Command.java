dcda860100042a28ae56c1ea81da1b104507e8cd
yavor300 <yavor300@gmail.com>
2023-05-06 12:39:55 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add complete Javadoc for the Command interface.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/base/Command.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/base/Command.java
@@ -4,14 +4,22 @@ import java.io.IOException;
 
 /**
  * The Command interface defines the behavior of a command that can be executed by the application.
+ * <p>
+ * Командният интерфейс определя поведението на команда, която може да бъде изпълнена от приложението.
  */
 public interface Command {
 
   /**
    * Executes the command.
+   * <p>
+   * Изпълнява командата.
    *
    * @return a message indicating the result of the command execution
+   * <p>
+   * съобщение, показващо резултата от изпълнението на командата
    * @throws IOException if an I/O error occurs during command execution
+   *                     <p>
+   *                     ако възникне I/O грешка по време на изпълнение на командата
    */
   String execute() throws IOException;
 
