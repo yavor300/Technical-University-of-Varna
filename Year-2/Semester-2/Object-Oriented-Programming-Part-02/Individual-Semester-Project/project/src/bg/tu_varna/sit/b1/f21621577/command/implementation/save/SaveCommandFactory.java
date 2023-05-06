f1bb13fb438534276cd6627a50b2342d93a9876b
yavor300 <yavor300@gmail.com>
2023-05-06 18:39:57 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add complete Javadoc for the SaveCommandFactory  class.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/save/SaveCommandFactory.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/save/SaveCommandFactory.java
@@ -5,13 +5,19 @@ import bg.tu_varna.sit.b1.f21621577.command.base.CommandAbstractFactory;
 
 /**
  * A factory for creating an SaveCommand object with a list of arguments.
+ * <p>
+ * Клас-фабрика за създаване на обект SaveCommand със списък от аргументи.
  */
 public class SaveCommandFactory implements CommandAbstractFactory {
 
   /**
    * Creates a new SaveCommand object.
+   * <p>
+   * Създава нов обект SaveCommand.
    *
    * @return the new SaveCommand object
+   * <p>
+   * новия обект SaveCommand
    */
   @Override
   public Command createCommand() {
