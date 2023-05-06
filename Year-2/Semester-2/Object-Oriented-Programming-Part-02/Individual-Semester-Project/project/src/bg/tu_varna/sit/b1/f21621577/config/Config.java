efa8bc7cc60e01e074e94f30f71664d7fa523147
yavor300 <yavor300@gmail.com>
2023-05-06 12:55:05 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add string constants if the table is opened.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/config/Config.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/config/Config.java
@@ -11,10 +11,14 @@ public class Config {
 
   public static final String CELLS_INPUT_SEPARATOR = ",";
 
-  public static final String CELLS_OUTPUT_SEPARATOR = CELLS_INPUT_SEPARATOR;
+  public static final String CELLS_OUTPUT_SEPARATOR = ",";
 
   public static final String DEFAULT_RESOURCES_DIRECTORY = "src/bg/tu_varna/sit/b1/f21621577/resources/";
 
   public static final String DEFAULT_TABLE_FILENAME = "data.csv";
+
+  public static final String TABLE_DATA_CLEARED_MESSAGE = "Table data cleared successfully.";
+
+  public static final String NO_TABLE_OPENED_MESSAGE = "No table is currently opened.";
 
 }
