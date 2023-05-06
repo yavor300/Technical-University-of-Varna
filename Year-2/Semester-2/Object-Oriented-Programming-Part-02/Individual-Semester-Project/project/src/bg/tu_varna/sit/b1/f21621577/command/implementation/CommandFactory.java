1f0c7ea94eff04e57e216ed44726c997881c4d80
yavor300 <yavor300@gmail.com>
2023-05-06 18:45:02 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add complete Javadoc for the CommandFactory  class.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/CommandFactory.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/CommandFactory.java
@@ -5,14 +5,22 @@ import bg.tu_varna.sit.b1.f21621577.command.base.CommandAbstractFactory;
 
 /**
  * A factory class for creating commands based on the provided abstract factory.
+ * <p>
+ * Клас-фабрика за създаване на команди, базирани на предоставената абстрактна фабрика.
  */
 public class CommandFactory {
 
   /**
    * Returns a new command instance created by the provided abstract factory.
+   * <p>
+   * Връща нова инстанция на команда, създадена от предоставената абстрактна фабрика.
    *
    * @param factory the abstract factory to use for creating the command
+   *                <p>
+   *                абстрактната фабрика, която да се използва за създаване на командата
    * @return the newly created command instance
+   * <p>
+   * новосъздадената команда
    */
   public static Command getCommand(CommandAbstractFactory factory) {
     return factory.createCommand();
