16e8fd56baf15bb54124140ff166ace184a7d9b9
yavor300 <yavor300@gmail.com>
2023-05-06 18:03:24 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add complete Javadoc for the PrintCommandFactory class.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/print/PrintCommandFactory.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/print/PrintCommandFactory.java
@@ -5,13 +5,19 @@ import bg.tu_varna.sit.b1.f21621577.command.base.CommandAbstractFactory;
 
 /**
  * A factory for creating an PrintCommand object with a list of arguments.
+ * <p>
+ * Клас-фабрика за създаване на PrintCommand обект със списък от аргументи.
  */
 public class PrintCommandFactory implements CommandAbstractFactory {
 
   /**
    * Creates a new PrintCommand object.
+   * <p>
+   * Създава нов обект PrintCommand.
    *
    * @return the new PrintCommand object
+   * <p>
+   * новия обект PrintCommand
    */
   @Override
   public Command createCommand() {
