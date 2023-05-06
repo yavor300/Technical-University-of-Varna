05e5a78cbb843a8a863a8d6059d4d58281f3dc7f
yavor300 <yavor300@gmail.com>
2023-05-06 16:56:32 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add complete Javadoc for the OpenCommand class.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/open/OpenCommand.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/open/OpenCommand.java
@@ -9,26 +9,26 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-import static bg.tu_varna.sit.b1.f21621577.config.Config.DEFAULT_RESOURCES_DIRECTORY;
+import static bg.tu_varna.sit.b1.f21621577.config.Config.*;
 
 /**
  * A command class that opens a table file and saves its contents in the table repository.
+ * <p>
+ * Команден клас, който отваря файл с таблица и записва съдържанието му в хранилището на таблицата.
  */
 public class OpenCommand extends ArgumentCommand {
 
   private final TableRepository repository = TableRepository.getInstance();
-  private Path file;
-
-  /**
-   * Constructs an OpenCommand instance.
-   */
-  public OpenCommand() {
-  }
+  private final Path file;
 
   /**
    * Constructs an OpenCommand instance with a list of arguments.
+   * <p>
+   * Конструира инстанция на OpenCommand със списък от аргументи.
    *
    * @param arguments the list of arguments for the command
+   *                  <p>
+   *                  списъка с аргументи за командата
    */
   public OpenCommand(List<String> arguments) {
     super(arguments);
@@ -38,8 +38,13 @@ public class OpenCommand extends ArgumentCommand {
   /**
    * Executes the command by opening the table file specified in the arguments, reading its contents,
    * and loading them in the table repository.
+   * <p>
+   * Изпълнява командата, като отваря файла с таблицата, посочен в аргументите, чете съдържанието му,
+   * и го зарежда в хранилището на таблицата.
    *
    * @return a message indicating whether the command was executed successfully or not
+   * <p>
+   * съобщение, показващо дали командата е изпълнена успешно или не
    */
   @Override
   public String execute() {
@@ -47,9 +52,9 @@ public class OpenCommand extends ArgumentCommand {
     try (TableReader tableReader = new TableReader(file)) {
       repository.loadData(tableReader.read());
       repository.setTableFileName(file.getFileName().toString());
-      return "Table opened successfully.";
+      return TABLE_OPENED_SUCCESSFULLY_MESSAGE;
     } catch (IOException | IllegalArgumentException e) {
-      return "Error opening table: " + e.getMessage();
+      return String.format(ERROR_OPENING_TABLE, e.getMessage());
     }
   }
 }
