232da147e2e5b19e2701b452f88e462b1f7a4e09
yavor300 <yavor300@gmail.com>
2023-05-06 20:49:06 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add complete javadoc to the CellType enum.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/table/cell/CellType.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/table/cell/CellType.java
@@ -1,5 +1,17 @@
 package bg.tu_varna.sit.b1.f21621577.table.cell;
 
+/**
+ * An enumeration representing the possible types of a table cell.
+ * <p>
+ * Изброяване, представящо възможните типове клетка от таблица.
+ * <ul>
+ * <li> {@link #INTEGER} - an integer number; цяло число
+ * <li> {@link #FRACTIONAL} - a fractional number; дробно число
+ * <li> {@link #STRING} - a string; низ
+ * <li> {@link #FORMULA} - a formula; формула
+ * <li> {@link #EMPTY} - an empty cell; празна клетка
+ * </ul>
+ */
 public enum CellType {
   INTEGER,
   FRACTIONAL,
