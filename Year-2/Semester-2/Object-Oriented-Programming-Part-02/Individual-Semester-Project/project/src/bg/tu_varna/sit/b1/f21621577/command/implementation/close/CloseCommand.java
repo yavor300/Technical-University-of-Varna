cddfaefd5067098e1da1ca638ea8a04a10d683f4
yavor300 <yavor300@gmail.com>
2023-05-06 12:51:24 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add complete Javadoc for the CloseCommand class.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/close/CloseCommand.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/close/CloseCommand.java
@@ -5,6 +5,8 @@ import bg.tu_varna.sit.b1.f21621577.table.repository.TableRepository;
 
 /**
  * A command that closes the currently opened table by clearing its data from the repository.
+ * <p>
+ * Команда, която затваря текущо отворената таблица, като изчиства нейните данни от хранилището.
  */
 public class CloseCommand implements Command {
 
@@ -12,8 +14,12 @@ public class CloseCommand implements Command {
 
   /**
    * Executes the close command by clearing the data of the currently opened table from the repository.
+   * <p>
+   * Изпълнява командата за затваряне, като изчиства данните от текущо отворената таблица от хранилището.
    *
    * @return a message indicating whether the table data was cleared successfully or if no table was currently opened.
+   * <p>
+   * съобщение, указващо дали данните от таблицата са изчистени успешно или в момента няма отворена таблица.
    */
   @Override
   public String execute() {
