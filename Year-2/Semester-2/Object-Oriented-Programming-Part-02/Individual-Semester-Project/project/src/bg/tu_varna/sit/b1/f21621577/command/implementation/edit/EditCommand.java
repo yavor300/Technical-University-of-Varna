89e8067e9900e3b4b5b9415fb0e86591a32db47c
yavor300 <yavor300@gmail.com>
2023-05-06 14:00:20 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add complete Javadoc for the EditComamnd class. Fix row and col values to start from 1. Use constant instead of magic values.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/edit/EditCommand.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/edit/EditCommand.java
@@ -6,12 +6,20 @@ import bg.tu_varna.sit.b1.f21621577.table.repository.TableRepository;
 
 import java.util.List;
 
+import static bg.tu_varna.sit.b1.f21621577.config.Config.*;
+
 /**
  * An implementation of the {@link ArgumentCommand} interface that represents the "edit" command.
  * This command takes three arguments: a row number, a column number, and a new cell value.
  * It updates the cell value in the specified row and column of the current table.
+ * <p>
+ * Реализация на интерфейса {@link ArgumentCommand}, който представлява командата „редактиране“.
+ * Тази команда приема три аргумента: номер на ред, номер на колона и нова стойност на клетка.
+ * Той актуализира стойността на клетката в посочения ред и колона на текущата таблица.
  */
 public class EditCommand extends ArgumentCommand {
+
+  private static final int ALLOWED_ARGUMENTS_COUNT = 3;
 
   private int row;
   private int col;
@@ -19,10 +27,20 @@ public class EditCommand extends ArgumentCommand {
 
   /**
    * Constructs a new {@code EditCommand} object with the specified array of arguments.
+   * <p>
+   * Конструира нов обект {@code EditCommand} с посочения масив от аргументи.
    *
    * @param arguments the arguments for the command
+   *                  <p>
+   *                  аргументите за командата
    * @throws IllegalArgumentException if the number of arguments is less than 3, or if the row or column number
    *                                  cannot be parsed as an integer
+   *                                  <p>
+   *                                  ако броят на аргументите е по-малък от 3 или ако номерът на реда или колоната
+   *                                  не може да се представи като цяло число
+   * @throws NumberFormatException    if the given cell value cannot be parsed as a double
+   *                                  <p>
+   *                                  ако дадената стойност на клетката не може да бъде представена като число
    */
   public EditCommand(List<String> arguments) {
     super(arguments);
@@ -32,10 +50,14 @@ public class EditCommand extends ArgumentCommand {
 
   /**
    * Updates a cell in the currently opened table at the specified row and column with the given value.
+   * In addition it scales and shrinks the table if necessary.
+   * <p>
+   * Актуализира клетка в текущо отворената таблица в посочения ред и колона с дадената стойност.
+   * В допълнение, командата мащабира и свива таблицата, ако е необходимо.
    *
    * @return a message indicating the result of the command execution, such as success or failure and the updated cell value
-   * @throws IndexOutOfBoundsException if the given row or column is out of bounds of the current table
-   * @throws NumberFormatException     if the given cell value cannot be parsed as a double
+   * <p>
+   * съобщение, указващо резултата от изпълнението на командата, като успех или неуспех и актуализираната стойност на клетката
    */
   @Override
   public String execute() {
@@ -43,7 +65,7 @@ public class EditCommand extends ArgumentCommand {
     TableRepository repository = TableRepository.getInstance();
 
     if (!repository.isTableOpened()) {
-      return "No table is currently opened.";
+      return NO_TABLE_OPENED_MESSAGE;
     }
 
     int numRows = repository.getNumRows();
@@ -61,39 +83,51 @@ public class EditCommand extends ArgumentCommand {
     try {
       newCell = new TableCell(newValue);
     } catch (IllegalArgumentException e) {
-      return "Error editing the cell: " + e.getMessage();
+      return String.format(ERROR_EDITING_CELL_MESSAGE, e.getMessage());
     }
 
     repository.setCell(row, col, newCell);
     repository.shrink();
 
-    return "Cell (" + row + ", " + col + ") updated from \"" + oldCell.getValueAsString() + "\" to \"" + newCell.getValueAsString() + "\"";
+    return String.format(CELL_UPDATED_MESSAGE, row + 1, col + 1, oldCell.getValueAsString(), newCell.getValueAsString());
   }
 
   /**
-   * Validates the arguments for the command.
+   * Validates the arguments passed to the edit command.
+   * <p>
+   * Потвърждава аргументите, предадени на командата за редактиране.
    *
-   * @throws IllegalArgumentException if the number of arguments is less than 3
+   * @throws IllegalArgumentException if there are not enough arguments or too many arguments for the edit command
+   *                                  <p>
+   *                                  ако няма достатъчно аргументи или твърде много аргументи за командата за редактиране
    */
   private void validateArguments() {
-    if (getArguments().size() < 3) {
-      throw new IllegalArgumentException("Not enough arguments. Usage: edit <row> <col> <value>");
+    if (getArguments().size() < ALLOWED_ARGUMENTS_COUNT) {
+      throw new IllegalArgumentException(EDIT_NOT_ENOUGH_ARGS_ERROR_MESSAGE + " " + EDIT_USAGE_ERROR_MESSAGE);
+    }
+    if (getArguments().size() > ALLOWED_ARGUMENTS_COUNT) {
+      throw new IllegalArgumentException(EDIT_TOO_MANY_ARGS_ERROR_MESSAGE + " " + EDIT_USAGE_ERROR_MESSAGE);
     }
   }
 
   /**
    * Parses the arguments for the command.
+   * <p>
+   * Превръща аргументите за командата към целочислени стойности.
    *
    * @throws IllegalArgumentException if the row or column number cannot be parsed as an integer
+   *                                  <p>
+   *                                  ако номерът на реда или колоната не може да бъде превърнат в цяло число
    */
   private void parseArguments() {
+
     try {
-      this.row = Integer.parseInt(getArguments().get(0));
-      this.col = Integer.parseInt(getArguments().get(1));
+      this.row = Integer.parseInt(getArguments().get(0)) - 1;
+      this.col = Integer.parseInt(getArguments().get(1)) - 1;
     } catch (NumberFormatException e) {
-      throw new IllegalArgumentException("Invalid row or column number.");
+      throw new IllegalArgumentException(INVALID_ROW_COL_NUMBER);
     }
+
     this.newValue = getArguments().get(2);
   }
-
 }
