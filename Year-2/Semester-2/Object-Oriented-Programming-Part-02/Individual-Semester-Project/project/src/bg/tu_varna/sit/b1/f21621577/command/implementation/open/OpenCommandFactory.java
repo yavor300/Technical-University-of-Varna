c0670f40a5b8fe7a67c88d64e231223f8ca34949
yavor300 <yavor300@gmail.com>
2023-05-06 17:25:53 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add complete Javadoc for the OpenCommandFactory class.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/open/OpenCommandFactory.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/implementation/open/OpenCommandFactory.java
@@ -11,18 +11,26 @@ import static bg.tu_varna.sit.b1.f21621577.config.Config.DEFAULT_TABLE_FILENAME;
 
 /**
  * A factory for creating an OpenCommand object with a list of arguments.
+ * <p>
+ * Клас-фабрика за създаване на OpenCommand обект със списък от аргументи.
  */
 public class OpenCommandFactory implements CommandAbstractFactory {
 
   /**
    * The list of arguments to be passed to the OpenCommand.
+   * <p>
+   * Списъкът с аргументи, които трябва да бъдат предадени на OpenCommand.
    */
   private List<String> arguments;
 
   /**
    * Constructs a new OpenCommand object with a list of arguments.
+   * <p>
+   * Конструира нов OpenCommand обект със списък от аргументи.
    *
    * @param arguments the list of arguments to be passed to the OpenCommand
+   *                  <p>
+   *                  списъкът с аргументи, които трябва да бъдат предадени на OpenCommand
    */
   public OpenCommandFactory(List<String> arguments) {
     setArguments(arguments);
@@ -30,6 +38,8 @@ public class OpenCommandFactory implements CommandAbstractFactory {
 
   /**
    * Creates a new OpenCommand object with the list of arguments.
+   * <p>
+   * Създава нов OpenCommand обект със списъка с аргументи.
    *
    * @return the new OpenCommand object
    */
@@ -41,8 +51,13 @@ public class OpenCommandFactory implements CommandAbstractFactory {
   /**
    * Sets the list of arguments for the command. If the argument list is null or empty,
    * the default table filename will be used. Otherwise, the provided argument list will be used.
+   * <p>
+   * Задава списъка с аргументи за командата. Ако списъкът с аргументи е нулев или празен,
+   * ще се използва името на файла на таблицата по подразбиране. В противен случай ще се използва предоставеният списък с аргументи
    *
    * @param arguments the list of arguments to set for the command, or null/empty to use the default filename
+   *                  <p>
+   *                  списъкът с аргументи, които да зададете за командата, или null/empty, за да използвате името на файла по подразбиране
    */
   public void setArguments(List<String> arguments) {
 
