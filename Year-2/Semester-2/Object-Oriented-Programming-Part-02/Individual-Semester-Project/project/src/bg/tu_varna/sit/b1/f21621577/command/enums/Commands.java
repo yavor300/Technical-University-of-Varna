ef1365dbe8d082757616331519e96a96bb8329b6
yavor300 <yavor300@gmail.com>
2023-05-06 12:47:53 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add complete Javadoc for the Commands enum.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/enums/Commands.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/command/enums/Commands.java
@@ -1,6 +1,11 @@
 package bg.tu_varna.sit.b1.f21621577.command.enums;
 
-public enum  Commands {
+/**
+ * An enumeration representing the available commands in the application.
+ * <p>
+ * Енумерация, представяща наличните команди в приложението.
+ */
+public enum Commands {
 
   OPEN("open"),
   PRINT("print"),
@@ -13,10 +18,28 @@ public enum  Commands {
 
   private final String value;
 
+  /**
+   * Constructs a Commands object with the specified value.
+   * <p>
+   * Конструира обект Commands със зададената стойност.
+   *
+   * @param value the string value of the command
+   *              <p>
+   *              стойността на низа на командата
+   */
   Commands(String value) {
     this.value = value;
   }
 
+  /**
+   * Returns the string value of the command.
+   * <p>
+   * Връща низовата стойност на командата.
+   *
+   * @return the string value of the command
+   * <p>
+   * низовата стойност на командата.
+   */
   public String getValue() {
     return value;
   }
