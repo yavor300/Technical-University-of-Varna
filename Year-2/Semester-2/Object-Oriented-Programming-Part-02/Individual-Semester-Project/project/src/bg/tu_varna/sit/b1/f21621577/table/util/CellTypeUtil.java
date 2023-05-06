9394ec0fbe817fd5696822d7b8f578891b08f0c9
yavor300 <yavor300@gmail.com>
2023-05-06 20:57:39 +0300

    [O.O.P-Individual-Semester-Project] (Week 11) Add complete javadoc to the CellTypeUtil class.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/table/util/CellTypeUtil.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/table/util/CellTypeUtil.java
@@ -3,13 +3,24 @@ package bg.tu_varna.sit.b1.f21621577.table.util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * A utility class that provides methods for working with cell data types.
+ * <p>
+ * Клас помощник, който предоставя методи за работа с типовете данни на клетки.
+ */
 public class CellTypeUtil {
 
   /**
    * Returns true if the specified data value is a fractional number, false otherwise.
+   * <p>
+   * Връща true, ако зададената стойност на данните е дробно число, false в противен случай.
    *
    * @param data the data value to check
+   *             <p>
+   *             стойността на данните за проверка
    * @return true if the data value is a fractional number, false otherwise
+   * <p>
+   * true, ако стойността на данните е дробно число, false в противен случай
    */
   public static boolean isFractionalNumber(String data) {
     Pattern fractionalPattern = Pattern.compile("^[+-]?\\d+\\.\\d+$");
@@ -19,9 +30,15 @@ public class CellTypeUtil {
 
   /**
    * Returns true if the specified data value is an integer, false otherwise.
+   * <p>
+   * Връща true, ако зададената стойност на данните е цяло число, false в противен случай.
    *
    * @param data the data value to check
+   *             <p>
+   *             стойността на данните за проверка
    * @return true if the data value is an integer, false otherwise
+   * <p>
+   * true, ако стойността на данните е цяло число, false в противен случай
    */
   public static boolean isInteger(String data) {
     Pattern intPattern = Pattern.compile("^[+-]?\\d+$");
