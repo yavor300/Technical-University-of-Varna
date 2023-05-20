5a8fbbc037ae942faf23b7223fffa1bdc4e31c48
yavor300 <yavor300@gmail.com>
2023-05-20 17:09:28 +0300

    [O.O.P-Individual-Semester-Project] (Week 13) Remove the formula subpackage.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/calculator/FormulaCalculator.java
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/src/bg/tu_varna/sit/b1/f21621577/calculator/FormulaCalculator.java
@@ -1,4 +1,4 @@
-package bg.tu_varna.sit.b1.f21621577.calculator.formula;
+package bg.tu_varna.sit.b1.f21621577.calculator;
 
 import bg.tu_varna.sit.b1.f21621577.table.cell.CellType;
 import bg.tu_varna.sit.b1.f21621577.table.cell.TableCell;
