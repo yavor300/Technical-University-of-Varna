f526c0e47bda85b4879595f699a17a25d89e90cc
yavor300 <yavor300@gmail.com>
2023-05-21 13:12:52 +0300

    [O.O.P-Individual-Semester-Project] (Week 13) Check if the project.jar exists before deleting it.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/build.ps1
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/build.ps1
@@ -1,6 +1,10 @@
-Remove-Item -Path program.jar -Force
+$file = "program.jar"
+
+if (Test-Path $file) {
+    Remove-Item $file
+}
 Get-ChildItem -Recurse -Filter "*.java" | ForEach-Object { $_.FullName } > sources.txt
 javac -encoding UTF-8 -d bin @(Get-Content sources.txt)
-jar cmvf META-INF/MANIFEST.MF program.jar -C bin/ .
+jar cmvf META-INF/MANIFEST.MF $file -C bin/ .
 Remove-Item -Path bin -Recurse -Force
 Remove-Item -Path sources.txt -Force
