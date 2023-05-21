935d8d716179e18217568b145843be7d1b635146
yavor300 <yavor300@gmail.com>
2023-05-21 13:42:23 +0300

    [O.O.P-Individual-Semester-Project] (Week 13) Modify the build script to delete old versions.

--- Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/build.ps1
+++ Year-2/Semester-2/Object-Oriented-Programming-Part-02/Individual-Semester-Project/project/build.ps1
@@ -1,12 +1,13 @@
 $versionFile = "VERSION"
 $version = Get-Content $versionFile -Raw
-$file = "program-$version.jar"
+$file_without_version = "program"
+$file_with_version = "program-$version"
 
-if (Test-Path $file) {
-    Remove-Item $file
+if (Test-Path $file_without_version-*) {
+    Remove-Item $file_without_version-*
 }
 Get-ChildItem -Recurse -Filter "*.java" | ForEach-Object { $_.FullName } > sources.txt
 javac -encoding UTF-8 -d bin @(Get-Content sources.txt)
-jar cmvf META-INF/MANIFEST.MF $file -C bin/ .
+jar cmvf META-INF/MANIFEST.MF "$file_with_version.jar" -C bin/ .
 Remove-Item -Path bin -Recurse -Force
 Remove-Item -Path sources.txt -Force
