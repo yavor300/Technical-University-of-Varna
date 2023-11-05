67d8fa0791456ee8c578575ec876d6f64487c62e
yavor300 <yavor300@gmail.com>
2023-11-05 18:42:02 +0200

    [Technology-of-Software-Production] (Week 5) Add the solution of the first exercise from Lab-01.

---
+++ Year-3/Semester-1/Technology-of-Software-Production/Lab-01/project/project/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(33, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Поздравления";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(12, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "RED";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Aqua;
+            this.button3.Location = new System.Drawing.Point(133, 178);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 46);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "BLUE";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Първи проект";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Year-3/Semester-1/Technology-of-Software-Production/Lab-01/project/project/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Black;
+            label1.Text = "Click one of the buttons below \n to see the magic working...";
+            label1.Font = new Font("Arial", 14);
+            label1.ForeColor = Color.White;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("\t Hello students! \n \n \t \t THIS is your FIRST C# PROGRAM ... \n \n \t \t \t Be Happy! :))))))))))");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ChangeFormColor(sender, e);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ChangeFormColor(sender, e);
+        }
+
+        private void ChangeFormColor(object sender, EventArgs e)
+        {
+            if (sender is Button)
+            {
+                this.BackColor = (sender as Button).BackColor;
+                this.Text = sender.ToString();
+            }
+        }
+
+       
+    }
+}
