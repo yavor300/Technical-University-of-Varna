9df1afddda54d21feb4f85d8492030bd4a611e76
yavor300 <yavor300@gmail.com>
2024-03-08 10:36:23 +0200

    [System-Programming] (Week 2) Add solution of the sixth exercise.

---
+++ Year-3/Semester-2/System-Programming/Lab-02/task06.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  pid_t pid;
+  pid = fork();
+  if (pid == -1) {
+    perror("fork");
+    exit(EXIT_FAILURE);
+  } else if (pid == 0) {
+    printf("Child\n");
+    exit(23);
+  } else {
+    sleep(2); // Parent sleeps for 2 seconds
+    system("ps a"); // Parent lists current processes
+    printf("Parent\n");
+    int status;
+    printf("Parent created child with pid %d\n", pid);
+    wait(&status);
+    if (WIFEXITED(status)) {
+      int statusValue = WEXITSTATUS(status);
+      printf("Child exit status value: %d\n", statusValue);
+    }
+  }
+  return 0;
+}
