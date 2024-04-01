7f39dd707763bb6c8f3fbb6c12070f9a04a70ff5
yavor300 <yavor300@gmail.com>
2024-04-02 01:03:26 +0300

    [System-Programming] (Week 2) Enhance seond task solution with logs and system call.

--- Year-3/Semester-2/System-Programming/Lab-02/task06.c
+++ Year-3/Semester-2/System-Programming/Lab-02/task06.c
@@ -4,25 +4,30 @@
 
 int main(void)
 {
+
   pid_t pid;
   pid = fork();
   if (pid == -1) {
     perror("fork");
     exit(EXIT_FAILURE);
   } else if (pid == 0) {
-    printf("Child\n");
+    printf("Child was created with pid %d.\n", pid);
+    printf("Child stopping execution...\n");
     exit(23);
   } else {
-    sleep(2); // Parent sleeps for 2 seconds
-    system("ps a"); // Parent lists current processes
-    printf("Parent\n");
+    sleep(2);
+    printf("Parent checking for zombie processes.\n");
+    system("ps a");
     int status;
-    printf("Parent created child with pid %d\n", pid);
     wait(&status);
     if (WIFEXITED(status)) {
       int statusValue = WEXITSTATUS(status);
-      printf("Child exit status value: %d\n", statusValue);
+      printf("Child exit status value: %d.\n", statusValue);
     }
+    printf("Parent checking for zombie processes after wait() command is executed.\n");
+    system("ps a");
+    exit(0);
   }
+
   return 0;
 }
