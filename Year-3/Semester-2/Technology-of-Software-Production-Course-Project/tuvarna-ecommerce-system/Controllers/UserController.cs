67147a03cc2d691f653c1afa0630edb1805e449b
yavor300 <yavor300@gmail.com>
2024-05-04 14:42:48 +0300

    [Technology-of-Software-Production-Course-Project] (Week 11) Add regiser endpoint.

---
+++ Year-3/Semester-2/Technology-of-Software-Production-Course-Project/tuvarna-ecommerce-system/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tuvarna_ecommerce_system.Exceptions;
+using tuvarna_ecommerce_system.Models.DTOs;
+using tuvarna_ecommerce_system.Service;
+
+namespace tuvarna_ecommerce_system.Controllers
+{
+    [Route("api/v1/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserReadDTO>> Register([FromBody] UserCreateDTO userCreateDTO)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { message = "Validation failed", errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage) });
+            }
+
+            try
+            {
+                var user = await _userService.CreateAsync(userCreateDTO);
+                return CreatedAtAction(nameof(Register), new { id = user.Id }, user);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+            catch (InternalServerErrorException ex)
+            {
+                return StatusCode(500, new { message = ex.Message });
+            }
+        }
+    }
+}
--- Year-3/Semester-2/Technology-of-Software-Production-Course-Project/tuvarna-ecommerce-system/Service/Implementation/UserService.cs
+++ Year-3/Semester-2/Technology-of-Software-Production-Course-Project/tuvarna-ecommerce-system/Service/Implementation/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using tuvarna_ecommerce_system.Exceptions;
 using tuvarna_ecommerce_system.Models.DTOs;
 using tuvarna_ecommerce_system.Models.Entities;
 using tuvarna_ecommerce_system.Models.Entities.Enums;
@@ -11,44 +12,59 @@ namespace tuvarna_ecommerce_system.Service.Implementation
 
         private readonly IUserRepository _userRepository;
         private readonly IPasswordHasher<User> _passwordHasher;
+        private readonly ILogger<ProductService> _logger;
 
-        public UserService(IUserRepository userRepository, IPasswordHasher<User> passwordHasher)
+        public UserService(IUserRepository userRepository, IPasswordHasher<User> passwordHasher, ILogger<ProductService> logger)
         {
             _userRepository = userRepository;
             _passwordHasher = passwordHasher;
+            _logger = logger;
         }
 
         public async Task<UserReadDTO> CreateAsync(UserCreateDTO userCreateDTO)
         {
-            User newUser;
-            switch (Enum.Parse<RoleEnum>(userCreateDTO.Role, true))
+            try
             {
-                case RoleEnum.ADMIN:
-                    newUser = new Administrator();
-                    break;
-                case RoleEnum.CUSTOMER:
-                    newUser = new Customer();
-                    break;
-                case RoleEnum.EMPLOYEE:
-                    newUser = new Employee();
-                    break;
-                default:
-                    throw new ArgumentException("Invalid role specified");
+                User newUser;
+                switch (Enum.Parse<RoleEnum>(userCreateDTO.Role, true))
+                {
+                    case RoleEnum.ADMIN:
+                        newUser = new Administrator();
+                        break;
+                    case RoleEnum.CUSTOMER:
+                        newUser = new Customer();
+                        break;
+                    case RoleEnum.EMPLOYEE:
+                        newUser = new Employee();
+                        break;
+                    default:
+                        throw new ArgumentException("Invalid role specified");
+                }
+
+                newUser.Username = userCreateDTO.Username;
+                newUser.Email = userCreateDTO.Email;
+                newUser.Password = _passwordHasher.HashPassword(newUser, userCreateDTO.Password);
+
+                var createdUser = await _userRepository.CreateAsync(newUser);
+
+                return new UserReadDTO
+                {
+                    Id = createdUser.Id,
+                    Username = createdUser.Username,
+                    Email = createdUser.Email,
+                    Role = createdUser.Role.ToString()
+                };
             }
-
-            newUser.Username = userCreateDTO.Username;
-            newUser.Email = userCreateDTO.Email;
-            newUser.Password = _passwordHasher.HashPassword(newUser, userCreateDTO.Password);
-
-            var createdUser = await _userRepository.CreateAsync(newUser);
-
-            return new UserReadDTO
+            catch (ArgumentException ex)
             {
-                Id = createdUser.Id,
-                Username = createdUser.Username,
-                Email = createdUser.Email,
-                Role = createdUser.Role.ToString()
-            };
+                _logger.LogError(ex, ex.Message);
+                throw;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unexpected error occurred.");
+                throw new InternalServerErrorException("An unexpected error occurred. Please try again later.", ex);
+            }
         }
     }
 }
