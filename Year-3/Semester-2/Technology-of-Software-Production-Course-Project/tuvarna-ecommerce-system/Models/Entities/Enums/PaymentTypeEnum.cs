9f16a96b091b50f3f1ff112882bebb2d4813681c
yavor300 <yavor300@gmail.com>
2024-05-04 10:56:27 +0300

    [Technology-of-Software-Production-Course-Project] (Week 11) Add discount percentage, payment and shipping type when creating new sale.

---
+++ Year-3/Semester-2/Technology-of-Software-Production-Course-Project/tuvarna-ecommerce-system/Models/Entities/Enums/PaymentTypeEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace tuvarna_ecommerce_system.Models.Entities.Enums
+{
+    public enum PaymentTypeEnum
+    {
+        BANK_TRANSFER,
+        CASH_ON_DELIVERY
+    }
+}
---
+++ Year-3/Semester-2/Technology-of-Software-Production-Course-Project/tuvarna-ecommerce-system/Models/Entities/Enums/ShippingTypeEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace tuvarna_ecommerce_system.Models.Entities.Enums
+{
+    public enum ShippingTypeEnum
+    {
+        PAID_ADDRESS,
+        PAID_OFFICE,
+        FREE_ADDRESS,
+        FREE_OFFICE
+    }
+}
