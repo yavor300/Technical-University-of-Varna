624a3089df8621b90d60d47ab5762439b00cd9f4
yavor300 <yavor300@gmail.com>
2024-05-04 10:43:29 +0300

    [Technology-of-Software-Production-Course-Project] (Week 11) Add list of products to the category dto.

--- Year-3/Semester-2/Technology-of-Software-Production-Course-Project/tuvarna-ecommerce-system/Models/DTOs/CategoryReadDTO.cs
+++ Year-3/Semester-2/Technology-of-Software-Production-Course-Project/tuvarna-ecommerce-system/Models/DTOs/CategoryReadDTO.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public string? ImageUrl { get; set; }
+
+        public List<ProductReadDTO> Products { get; set; } = new List<ProductReadDTO>();
     }
 }
