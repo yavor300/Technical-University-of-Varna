19262afec88e3fb3e6376cb05464a24727f5c8a6
yavor300 <yavor300@gmail.com>
2024-10-05 19:18:48 +0300

    [Mobile-Internet-Devices-Programming] (Week 1) Add demo for looping constructs.

--- Year-4/Semester-1/Mobile-Internet-Devices-Programming/Lab-01/src/main/kotlin/LoopingConstructs.kt
+++ Year-4/Semester-1/Mobile-Internet-Devices-Programming/Lab-01/src/main/kotlin/LoopingConstructs.kt
@@ -1,15 +1,37 @@
-import java.util.Random;
-
 fun main(args: Array<String>) {
 
+  val fishes = arrayOf("shark", "salmon", "minnow")
 
-}
+  for (element in fishes) {
+    print("$element ")
+  }
+  println()
 
-fun randomDay() : String {
-  val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-  return week[Random().nextInt(week.size)]
-}
+  for ((index, element) in fishes.withIndex()) {
+    println("Item at $index is $element\n");
+  }
 
-fun swim(day: String, speed: String = "fast") {
-  println("swimming $speed")
+  for (i in 1..5) print(i)
+  println()
+
+  for (i in 5 downTo 1) print(i)
+  println()
+
+  for (i in 3..6 step 2) print(i)
+  println()
+
+  for (i in 'd'..'g') print(i)
+
+  var bubbles = 0
+  while (bubbles < 50) bubbles++;
+  println("$bubbles bubbles in the water\n")
+
+  do {
+    bubbles--;
+  } while (bubbles > 50)
+  println("$bubbles bubbles in the water\n")
+
+  repeat(2) {
+    println("A fish is swimming")
+  }
 }
