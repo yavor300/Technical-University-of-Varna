6a911031fd3a46037dd9595f4c33f1b604d273d1
yavor300 <yavor300@gmail.com>
2024-11-13 01:26:37 +0200

    [Mobile-Internet-Devices-Programming] (Week 7) Add Android hello world project.

---
+++ Year-4/Semester-1/Mobile-Internet-Devices-Programming/Lab-04/app/src/main/java/com/example/lab_04/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.lab_04
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab_04.ui.theme.Lab04Theme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      Lab04Theme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+          )
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+  Text(
+    text = "Hello $name!",
+    modifier = modifier
+  )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  Lab04Theme {
+    Greeting("Android")
+  }
+}
