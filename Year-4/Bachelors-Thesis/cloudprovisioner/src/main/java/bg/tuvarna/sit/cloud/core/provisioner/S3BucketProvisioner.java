3e17c87e6df9ca452d5f24350c1fa8e8bcecb625
yavor300 <yavor300@gmail.com>
2025-04-09 17:32:54 +0300

    [Bachelor-Thesis] Iterate over the steps when creating the resource.

--- Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/provisioner/S3BucketProvisioner.java
+++ Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/provisioner/S3BucketProvisioner.java
@@ -1,16 +1,18 @@
 package bg.tuvarna.sit.cloud.core.provisioner;
 
+import bg.tuvarna.sit.cloud.core.aws.s3.*;
 import bg.tuvarna.sit.cloud.core.config.S3BucketConfig;
 import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.*;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
-import software.amazon.awssdk.services.s3.model.HeadBucketResponse;
 
 import java.net.URI;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class S3BucketProvisioner implements CloudResourceProvisioner<S3BucketConfig> {
@@ -37,20 +39,75 @@ public class S3BucketProvisioner implements CloudResourceProvisioner<S3BucketCon
         .forcePathStyle(true)
         .build()) {
 
-      CreateBucketRequest request = CreateBucketRequest.builder()
-          .bucket(bucketName)
-          .build();
+      CreateBucketRequest.Builder request = CreateBucketRequest.builder()
+          .bucket(bucketName);
+
+      if (config.getAcl() != null && !config.getAcl().isBlank()) {
+        request.acl(BucketCannedACL.fromValue(config.getAcl()));
+        log.info("Setting ACL '{}' for bucket '{}'", config.getAcl(), bucketName);
+      }
 
       log.info("Sending request to create S3 bucket '{}'", bucketName);
-      s3Client.createBucket(request);
+      s3Client.createBucket(request.build());
 
-      HeadBucketRequest headRequest = HeadBucketRequest.builder()
-          .bucket(bucketName)
-          .build();
+      if (config.getTags() != null && !config.getTags().isEmpty()) {
+        List<Tag> tagList = config.getTags().entrySet().stream()
+            .map(e -> Tag.builder().key(e.getKey()).value(e.getValue()).build())
+            .collect(Collectors.toList());
+
+        s3Client.putBucketTagging(PutBucketTaggingRequest.builder()
+            .bucket(bucketName)
+            .tagging(Tagging.builder().tagSet(tagList).build())
+            .build());
+        log.info("Applied tags to bucket '{}': {}", bucketName, config.getTags());
+      }
+
+      if ("enabled".equalsIgnoreCase(config.getVersioning())) {
+        s3Client.putBucketVersioning(PutBucketVersioningRequest.builder()
+            .bucket(bucketName)
+            .versioningConfiguration(VersioningConfiguration.builder()
+                .status(BucketVersioningStatus.ENABLED)
+                .build())
+            .build());
+        log.info("Enabled versioning for bucket '{}'", bucketName);
+      }
+
+      if (config.getEncryption() != null && "SSE-S3".equalsIgnoreCase(config.getEncryption().getType())) {
+        s3Client.putBucketEncryption(PutBucketEncryptionRequest.builder()
+            .bucket(bucketName)
+            .serverSideEncryptionConfiguration(ServerSideEncryptionConfiguration.builder()
+                .rules(ServerSideEncryptionRule.builder()
+                    .applyServerSideEncryptionByDefault(
+                        ServerSideEncryptionByDefault.builder()
+                            .sseAlgorithm(ServerSideEncryption.AES256)
+                            .build())
+                    .build())
+                .build())
+            .build());
+        log.info("Enabled SSE-S3 encryption for bucket '{}'", bucketName);
+      }
+
+      if (config.getPolicy() != null && !config.getPolicy().isBlank()) {
+        s3Client.putBucketPolicy(PutBucketPolicyRequest.builder()
+            .bucket(bucketName)
+            .policy(config.getPolicy())
+            .build());
+        log.info("Applied policy to bucket '{}'", bucketName);
+      }
+
+      List<S3ProvisionStep> steps = List.of(
+          new S3PolicyStep(),
+          new S3VersioningStep(),
+          new S3TaggingStep(),
+          new S3EncryptionStep()
+      );
+
+      steps.stream()
+          .sorted(Comparator.comparingInt(s -> s.getClass().getAnnotation(ProvisionOrder.class).value()))
+          .forEach(step -> step.apply(s3Client, config));
 
       log.debug("Verifying bucket existence with HeadBucket request");
-      HeadBucketResponse headBucketResponse = s3Client.headBucket(headRequest);
-
+      s3Client.headBucket(HeadBucketRequest.builder().bucket(bucketName).build());
       log.info("S3 bucket '{}' exists and is active", bucketName);
 
       String arn = String.format("arn:aws:s3:::%s", bucketName);
