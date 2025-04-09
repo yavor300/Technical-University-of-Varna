8049d672f36cd8212293351dce57bfd728e52013
yavor300 <yavor300@gmail.com>
2025-04-09 17:30:16 +0300

    [Bachelor-Thesis] Extend the S3 config with more features.

--- Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/config/S3BucketConfig.java
+++ Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/config/S3BucketConfig.java
@@ -4,6 +4,8 @@ package bg.tuvarna.sit.cloud.core.config;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Map;
+
 // TODO Extend the class with more features from the S3
 @Setter
 @Getter
@@ -11,4 +13,18 @@ public class S3BucketConfig {
 
   private String name;
   private String region;
+
+  private Map<String, String> tags;
+  private String versioning;
+  private EncryptionConfig encryption;
+  private String acl;
+  private String policy;
+
+
+  @Getter
+  @Setter
+  public static class EncryptionConfig {
+    private String type;
+    private String kmsKeyId;
+  }
 }
