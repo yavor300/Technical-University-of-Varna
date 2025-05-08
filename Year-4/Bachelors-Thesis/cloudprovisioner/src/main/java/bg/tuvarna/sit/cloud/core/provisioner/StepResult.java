11558396ae8495689d33ff0fd9146769f3a62f3e
yavor300 <yavor300@gmail.com>
2025-05-09 01:25:50 +0300

    [Bachelor-Thesis] Override equals and hashcode to track for state changes.

--- Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/provisioner/StepResult.java
+++ Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/provisioner/StepResult.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -23,6 +24,33 @@ public class StepResult<K extends Enum<K>> {
 
   public static <K extends Enum<K>> Builder<K> builder() {
     return new Builder<>();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    StepResult<?> that = (StepResult<?>) o;
+
+    if (!Objects.equals(stepName, that.stepName)) return false;
+    if (outputs.size() != that.outputs.size()) return false;
+
+    for (Map.Entry<K, Object> entry : outputs.entrySet()) {
+      Object thatValue = that.outputs.get(entry.getKey());
+      if (!Objects.equals(entry.getValue(), thatValue)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+
+    return Objects.hash(stepName, outputs);
   }
 
   public static class Builder<K extends Enum<K>> {
