d7752b983b42bf01b0b8d350a722eaf6ab8206a1
yavor300 <yavor300@gmail.com>
2025-05-09 01:26:43 +0300

    [Bachelor-Thesis] Override equals and hashcode to track for state changes.

--- Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/provisioner/CloudProvisionStep.java
+++ Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/provisioner/CloudProvisionStep.java
@@ -1,8 +1,12 @@
 package bg.tuvarna.sit.cloud.core.provisioner;
 
+import bg.tuvarna.sit.cloud.exception.CloudProvisioningStepException;
+
 public interface CloudProvisionStep<TClient, TConfig, K extends Enum<K>> {
 
-    StepResult<K> apply(TClient client, TConfig config);
+    StepResult<K> apply(TClient client, TConfig config) throws CloudProvisioningStepException;
 
     StepResult<K> generateDesiredState(TConfig config);
+
+    StepResult<K> getCurrentState(TClient client, TConfig config);
 }
