f338cdde419c95aa070a954b6eae7b78948a63d8
yavor300 <yavor300@gmail.com>
2025-05-10 21:53:59 +0300

    [Bachelor-Thesis] Add s3StepExecutor instance to the Guice context.

--- Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/config/CloudProvisionerModule.java
+++ Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/config/CloudProvisionerModule.java
@@ -1,13 +1,24 @@
 package bg.tuvarna.sit.cloud.config;
 
 import bg.tuvarna.sit.cloud.core.aws.s3.S3BucketConfig;
+import bg.tuvarna.sit.cloud.core.aws.s3.S3Output;
 import bg.tuvarna.sit.cloud.core.aws.s3.S3ProvisioningContext;
 import bg.tuvarna.sit.cloud.core.aws.s3.client.S3SafeClient;
+import bg.tuvarna.sit.cloud.core.aws.s3.step.S3AclStep;
+import bg.tuvarna.sit.cloud.core.aws.s3.step.S3BucketCreateStep;
+import bg.tuvarna.sit.cloud.core.aws.s3.step.S3EncryptionStep;
+import bg.tuvarna.sit.cloud.core.aws.s3.step.S3OwnershipControlsStep;
+import bg.tuvarna.sit.cloud.core.aws.s3.step.S3PolicyStep;
+import bg.tuvarna.sit.cloud.core.aws.s3.step.S3TaggingStep;
+import bg.tuvarna.sit.cloud.core.aws.s3.step.S3VersioningStep;
+import bg.tuvarna.sit.cloud.core.provisioner.CloudStepExecutor;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import lombok.AllArgsConstructor;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.services.s3.S3Client;
+
+import java.util.List;
 
 @AllArgsConstructor
 public class CloudProvisionerModule extends AbstractModule {
@@ -31,4 +42,28 @@ public class CloudProvisionerModule extends AbstractModule {
         .forcePathStyle(true)
         .build());
   }
+
+  @Provides
+  CloudStepExecutor<S3Output> s3StepExecutor(
+      S3BucketCreateStep createStep,
+      S3PolicyStep policyStep,
+      S3VersioningStep versioningStep,
+      S3TaggingStep taggingStep,
+      S3EncryptionStep encryptionStep,
+      S3AclStep aclStep,
+      S3OwnershipControlsStep ownershipControlsStep
+  ) {
+    return new CloudStepExecutor<>(
+        List.of(
+            createStep,
+            policyStep,
+            versioningStep,
+            taggingStep,
+            encryptionStep,
+            aclStep,
+            ownershipControlsStep
+        )
+    );
+  }
+
 }
