0169c9915f2236ba8cf269dd78abf9d760262a76
yavor300 <yavor300@gmail.com>
2025-05-10 21:55:26 +0300

    [Bachelor-Thesis] Override equals for ProvisinedTags object.

--- Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/aws/s3/ProvisionedTags.java
+++ Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/aws/s3/ProvisionedTags.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Map;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 public class ProvisionedTags {
@@ -13,5 +14,17 @@ public class ProvisionedTags {
   @JsonAnyGetter
   public Map<String, String> getTags() {
     return tags;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof ProvisionedTags that)) return false;
+    return Objects.equals(this.tags, that.tags);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(tags);
   }
 }
