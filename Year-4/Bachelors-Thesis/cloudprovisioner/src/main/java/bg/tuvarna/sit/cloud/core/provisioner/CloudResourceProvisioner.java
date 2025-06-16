e4cfc470c9a238d9234f930c5f54b5200eaf942c
yavor300 <yavor300@gmail.com>
2025-06-16 20:03:06 +0300

    [Bachelor-Thesis] Use correct generic for abstraction in the params.

--- Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/provisioner/CloudResourceProvisioner.java
+++ Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/provisioner/CloudResourceProvisioner.java
@@ -1,12 +1,11 @@
 package bg.tuvarna.sit.cloud.core.provisioner;
 
-import bg.tuvarna.sit.cloud.core.aws.s3.S3Output;
 import bg.tuvarna.sit.cloud.exception.CloudProvisioningTerminationException;
 
 import java.util.List;
 
 public interface CloudResourceProvisioner<K extends Enum<K>> {
 
-  CloudProvisionerSuccessfulResponse<K> provision(List<CloudProvisionStep<S3Output>> resources) throws
-      CloudProvisioningTerminationException;
+  CloudProvisionerSuccessfulResponse<K> provision(List<CloudProvisionStep<K>> resources)
+      throws CloudProvisioningTerminationException;
 }
--- Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/provisioner/CloudResourceReverter.java
+++ Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/provisioner/CloudResourceReverter.java
@@ -1,12 +1,11 @@
 package bg.tuvarna.sit.cloud.core.provisioner;
 
-import bg.tuvarna.sit.cloud.core.aws.s3.S3Output;
 import bg.tuvarna.sit.cloud.exception.CloudProvisioningTerminationException;
 
 import java.util.List;
 
 public interface CloudResourceReverter<K extends Enum<K>> {
 
-  CloudProvisionerSuccessfulResponse<K> revert(List<CloudProvisionStep<S3Output>> resources, List<StepResult<K>> previous)
+  CloudProvisionerSuccessfulResponse<K> revert(List<CloudProvisionStep<K>> resources, List<StepResult<K>> previous)
       throws CloudProvisioningTerminationException;
 }
