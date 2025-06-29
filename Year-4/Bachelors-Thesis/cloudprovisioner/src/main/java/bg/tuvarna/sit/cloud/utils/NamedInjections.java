4b5f340613e381489412baf158db26606a35c111
yavor300 <yavor300@gmail.com>
2025-06-30 02:15:06 +0300

    [Bachelor-Thesis] Add all names for injected components.

---
+++ Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/utils/NamedInjections.java
@@ -0,0 +1,19 @@
+package bg.tuvarna.sit.cloud.utils;
+
+public class NamedInjections {
+
+  public static final String JSON_MAPPER = "jsonMapper";
+
+  public static final String YAML_MAPPER = "yamlMapper";
+
+  public static final String DEFAULT_PRETTY_PRINTER = "defaultPrettyPrinter";
+
+  public static final String JSON_MAPPER_SINGLE_VALUE_ARRAY = "jsonMapperAcceptSingleValueAsArray";
+
+  public static final String BUNDLES = "bundles";
+
+  public static final String S3_STEPS = "s3Steps";
+
+  public static final String EKS_STEPS = "eksSteps";
+
+}
