187089fb57b578fdff7e6badb5e02e3c45a3714e
yavor300 <yavor300@gmail.com>
2025-06-30 02:11:41 +0300

    [Bachelor-Thesis] Add more common fields to the base configuration class.

--- Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/provisioner/BaseCloudResourceConfiguration.java
+++ Year-4/Bachelors-Thesis/cloudprovisioner/src/main/java/bg/tuvarna/sit/cloud/core/provisioner/BaseCloudResourceConfiguration.java
@@ -5,8 +5,29 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public abstract class BaseCloudResourceConfiguration implements IdentifiableConfig {
+public abstract class BaseCloudResourceConfiguration implements IdentifiableConfig, DestroyProtection {
 
-    private String id;
-    private boolean toDelete = false;
+  protected static final String DEFAULT_EMPTY_STRING = "";
+
+  // TODO [Implementation:Low] @Required?
+  private String id;
+  private String name;
+  private boolean enableReconciliation = true;
+  private Boolean preventDestroy = true;
+  private boolean toDelete = false;
+  private RetryConfiguration retry = new RetryConfiguration();
+
+  @Override
+  public Boolean preventDestroy() {
+
+    return preventDestroy;
+  }
+
+  @Getter
+  public static class RetryConfiguration {
+
+    int max = 3;
+    long backoffMs = 1000;
+  }
+
 }
